e1802d1373a707351f5f193b6a7275ef6accb937
Krzysztof Ostrowski <ostrowski@google.com>
2018-09-20 15:11:14 -0700

    Adds the top-level __init__.py
    PiperOrigin-RevId: 213890797

--- tensorflow_federated/python/__init__.py
+++ tensorflow_federated/python/__init__.py
@@ -11,3 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""TensorFlow Federated library."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Used by doc generation script.
+_allowed_symbols = []
