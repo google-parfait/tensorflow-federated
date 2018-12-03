55af89300a1541cb6b5344cde254f685e5d7cc78
Krzysztof Ostrowski <ostrowski@google.com>
2018-12-03 09:53:35 -0800

    Documents a number of upcoming intrinsics in impl/intrinsic_defs.
    PiperOrigin-RevId: 223812595

--- tensorflow_federated/python/core/impl/intrinsic_defs.py
+++ tensorflow_federated/python/core/impl/intrinsic_defs.py
@@ -80,6 +80,133 @@ class IntrinsicDef(object):
 # placement_literals.py.
 
 
+# TODO(b/113112885): Define the generic equivalents of all operators below,
+# i.e., intrinsics that support arbitrary placements, to allow the federated
+# operators to be decomposed into expressions that might involve one or more
+# layers of intermediate aggregators. The type signatures of these generic
+# intrinsics are tentatively defined as follows:
+#
+# - Compute an aggregate using the 4-part aggregation operator interface:
+#
+#     generic_aggregate: <{T}@p,U,(<U,T>->U),(<U,U>->U),(U->R),q> -> R@q
+#
+# - Compute an unweighted average:
+#
+#     generic_average: <{T}@p,q> -> T@q
+#
+# - Broadcast an item:
+#
+#     generic_broadcast: <T@p,q> -> T@q
+#
+# - Materialize a federated value as a set of sequences at another placement,
+#   with the participants at 'q' collecting from disjoint subsets of 'p' that
+#   jointly cover all of 'p'.
+#
+#     generic_partial_collect: <{T}@p,q> -> {T*}@q
+#
+# - Materialize a federated value as a single sequence:
+#
+#     generic_collect: <{T}@p,q> -> T*@q
+#
+# - Pointwise mapping of constituents of a federated value:
+#
+#     generic_map: <{T}@p,(T->U)> -> {U}@p
+#
+# - Perform one-stage set reduction of a federated value with a given operator,
+#   with the participants at 'q' reducing over disjoint subsets of 'p' that
+#   jointly cover all of 'p'.
+#
+#     generic_partial_reduce: <{T}@p,U,(<U,T>->U),q> -> {U}@q
+#
+# - Perform complete set reduction of a federated value with a given operator:
+#
+#     generic_reduce: <{T}@p,U,(<U,T>->U),q> -> U@q
+#
+# - Select and agree on a single member consistuent of a federated value (this
+#   is technically need to project {T}@SERVER to T@SERVER in a manner that is
+#   formally consistent; a technicality that we do not expect to surface in the
+#   user API).
+#
+#     generic_only: {T}@p -> T@p
+#
+# - Compute a partial sum of a value (for values with numeric constituents):
+#
+#     generic_partial_sum: <{T}@p,q> -> {T}@q
+#
+# - Compute a simple sum of a value (for values with numeric constituents):
+#
+#     generic_sum: <{T}@p,q> -> T@q
+#
+# - Compute an average weighted by a numeric non-complex scalar:
+#
+#     generic_weighted_average: <{T}@p,{U}@p,q> -> T@q
+#
+# - Transform a pair of federated values into a federated pair (technicality we
+#   expect to bury through implicit convertions, TBD).
+#
+#     generic_zip: <{T}@p,{U}@p> -> {<T,U>}@p
+
+
+# Computes an aggregate of client items (the first, {T}@CLIENTS-typed parameter)
+# using a multi-stage process in which client items are first partially
+# aggregated at an intermediate layer, then the partial aggregates are further
+# combined, and finally projected into the result. This multi-stage process is
+# parameterized by a four-part aggregation interface that consists of the
+# following:
+# a) The 'zero' in the algebra used at the initial stage (partial aggregation),
+#    This is the second, U-typed parameter.
+# b) The operator that accumulates T-typed client items into the U-typed partial
+#    aggregates. This is the third, (<U,T>->U)-typed parameter.
+# c) The operator that combines pairs of U-typed partial aggregates. This is the
+#    fourth, (<U,U>->U)-typed parameter.
+# d) The operator that projects the top-level aggregate into the final result.
+#    This is the fifth, (U->R)-typed parameter.
+#
+# Conceptually, given a new literal INTERMEDIATE_AGGREGATORS in a single-layer
+# aggregation architecture, one could define this intrinsic in terms of generic
+# intrinsics defined above, as follows.
+#
+# @federated_computation
+# def federated_aggregate(x, zero, accumulate, merge, report):
+#   a = generic_partial_reduce(x, zero, accumulate, INTERMEDIATE_AGGREGATORS)
+#   b = generic_reduce(a, zero, merge, SERVER)
+#   c = generic_map(b, report)
+#   return c
+#
+# Actual implementations might vary.
+#
+# Type signature: <{T}@CLIENTS,U,(U,T->U),(U,U->U),(U->R)> -> R@SERVER
+FEDERATED_AGGREGATE = IntrinsicDef(
+    'FEDERATED_AGGREGATE',
+    'federated_aggregate',
+    types.FunctionType(
+        [types.FederatedType(types.AbstractType('T'), placements.CLIENTS),
+         types.AbstractType('U'),
+         types.FunctionType(
+             [types.AbstractType('U'), types.AbstractType('T')],
+             types.AbstractType('U')),
+         types.FunctionType(
+             [types.AbstractType('U'), types.AbstractType('U')],
+             types.AbstractType('U')),
+         types.FunctionType(types.AbstractType('U'), types.AbstractType('R'))],
+        types.FederatedType(types.AbstractType('R'), placements.SERVER, True)))
+
+
+# Computes a simple (equally weighted) average of client items. Only supported
+# for numeric tensor types, or composite structures made up of numeric types.
+#
+# Type signature: {T}@CLIENTS -> T@SERVER
+FEDERATED_AVERAGE = IntrinsicDef(
+    'FEDERATED_AVERAGE',
+    'federated_average',
+    types.FunctionType(
+        types.FederatedType(types.AbstractType('T'), placements.CLIENTS),
+        types.FederatedType(types.AbstractType('T'), placements.SERVER, True)))
+
+
+# Broadcasts a server item to all clients.
+#
+# Type signature: T@SERVER -> T@CLIENTS
 FEDERATED_BROADCAST = IntrinsicDef(
     'FEDERATED_BROADCAST',
     'federated_broadcast',
@@ -88,6 +215,24 @@ FEDERATED_BROADCAST = IntrinsicDef(
         types.FederatedType(types.AbstractType('T'), placements.CLIENTS, True)))
 
 
+# Materializes client items as a sequence on the server.
+#
+# Type signature: {T}@CLIENTS -> T*@SERVER
+FEDERATED_COLLECT = IntrinsicDef(
+    'FEDERATED_COLLECT',
+    'federated_collect',
+    types.FunctionType(
+        types.FederatedType(types.AbstractType('T'), placements.CLIENTS),
+        types.FederatedType(
+            types.SequenceType(types.AbstractType('T')),
+            placements.SERVER,
+            True)))
+
+
+# Maps member constituents of a client value pointwise uising a given mapping
+# function that operates independently on each client.
+#
+# Type signature: <{T}@CLIENTS,(T->U)> -> {U}@CLIENTS
 FEDERATED_MAP = IntrinsicDef(
     'FEDERATED_MAP',
     'federated_map',
@@ -97,6 +242,16 @@ FEDERATED_MAP = IntrinsicDef(
         types.FederatedType(types.AbstractType('U'), placements.CLIENTS)))
 
 
+# Reduces a set of member constituents of a client value using a given 'zero' in
+# the algebra (i.e., the result of reducing an empty set) and a given reduction
+# operator with the signature U,T->U that incorporates a single T-typed element
+# into a U-typed result of partial reduction. In the special case of T = U, this
+# corresponds to the classical notion of reduction of a set using a commutative
+# associative binary operator. The generalized reduction operator (with T != U)
+# must yield the same results when repeatedly applied on sequences of elements
+# in any order.
+#
+# Type signature: <{T}@CLIENTS,U,(<U,T>->U)> -> U@SERVER
 FEDERATED_REDUCE = IntrinsicDef(
     'FEDERATED_REDUCE',
     'federated_reduce',
@@ -109,6 +264,10 @@ FEDERATED_REDUCE = IntrinsicDef(
         types.FederatedType(types.AbstractType('U'), placements.SERVER, True)))
 
 
+# Computes the sum of client values on the server. Only supported for numeric
+# types, or nested structures made up of numeric types.
+#
+# Type signature: {T}@CLIENTS -> T@SERVER
 FEDERATED_SUM = IntrinsicDef(
     'FEDERATED_SUM',
     'federated_sum',
@@ -117,6 +276,28 @@ FEDERATED_SUM = IntrinsicDef(
         types.FederatedType(types.AbstractType('T'), placements.SERVER, True)))
 
 
+# Computes a weighted average of client items. Only supported for numeric tensor
+# types, or composite structures made up of numeric types. Weights must be
+# simple scalar numeric (integer or floating point, not complex) tensor types.
+# The types of weights and values must be compatible, i.e., multiplying and
+# dividing member constituents of the value by weights should yield results of
+# the same type as the type of these member constituents being weighted. Thus,
+# for example, one may not supply values containing tf.int32 tensors, as the
+# result of weighting such values is of a floating-point type. Casting must be
+# injected, where appropriate, by the compiler before invoking this intrinsic.
+#
+# Type signature: <{T}@CLIENTS,{U}@CLIENTS> -> T@SERVER
+FEDERATED_WEIGHTED_AVERAGE = IntrinsicDef(
+    'FEDERATED_WEIGHTED_AVERAGE',
+    'federated_weighted_average',
+    types.FunctionType(
+        [types.FederatedType(types.AbstractType('T'), placements.CLIENTS),
+         types.FederatedType(types.AbstractType('U'), placements.CLIENTS)],
+        types.FederatedType(types.AbstractType('T'), placements.SERVER, True)))
+
+# Zips a tuple of two federated types into a federated tuple.
+#
+# Type signature: <{T}@CLIENTS,{U}@CLIENTS> -> {<T,U>}@CLIENTS
 FEDERATED_ZIP = IntrinsicDef(
     'FEDERATED_ZIP',
     'federated_zip',
