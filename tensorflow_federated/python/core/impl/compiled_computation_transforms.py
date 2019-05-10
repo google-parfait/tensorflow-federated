35c3476b51c3c75de7eba7509de00d28378d3e4c
Keith Rush <krush@google.com>
2019-05-10 12:45:34 -0700

    Implements pad_graph_inputs_to_match_type, padding the type signature of a CompiledComputation to accept more parameters. Also implements wrap_graph_parameter_as_tuple as a preprocessing step to simplify assumptions for pad_graph_inputs_to_match_type.
    PiperOrigin-RevId: 247659080

--- tensorflow_federated/python/core/impl/compiled_computation_transforms.py
+++ tensorflow_federated/python/core/impl/compiled_computation_transforms.py
@@ -20,11 +20,15 @@ from __future__ import print_function
 
 from six.moves import range
 
+import tensorflow as tf
+
 from tensorflow_federated.proto.v0 import computation_pb2 as pb
 from tensorflow_federated.python.common_libs import anonymous_tuple
 from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.common_libs import serialization_utils
 from tensorflow_federated.python.core.api import computation_types
 from tensorflow_federated.python.core.impl import computation_building_blocks
+from tensorflow_federated.python.core.impl import graph_utils
 from tensorflow_federated.python.core.impl import type_serialization
 
 
@@ -215,3 +219,178 @@ def permute_graph_inputs(comp, input_permutation):
                   element=new_parameter_bindings)),
           result=proto.tensorflow.result))
   return computation_building_blocks.CompiledComputation(permuted_proto)
+
+
+def wraph_graph_parameter_as_tuple(comp):
+  """Wraps the parameter of `comp` in a tuple binding.
+
+  `wraph_graph_parameter_as_tuple` is intended as a preprocessing step
+  to `pad_graph_inputs_to_match_type`, so that `pad_graph_inputs_to_match_type`
+  can
+  make the assumption that its argument `comp` always has a tuple binding,
+  instead of dealing with the possibility of an unwrapped tensor or sequence
+  binding.
+
+  Args:
+    comp: Instance of `computation_building_blocks.CompiledComputation` whose
+      parameter we wish to wrap in a tuple binding.
+
+  Returns:
+    A transformed version of comp representing exactly the same computation,
+    but accepting a tuple containing one element--the parameter of `comp`.
+
+  Raises:
+    TypeError: If `comp` is not a
+      `computation_building_blocks.CompiledComputation`.
+  """
+  py_typecheck.check_type(comp, computation_building_blocks.CompiledComputation)
+  proto = comp.proto
+  proto_type = type_serialization.deserialize_type(proto.type)
+
+  parameter_binding = [proto.tensorflow.parameter]
+  parameter_type_list = [proto_type.parameter]
+  new_parameter_binding = pb.TensorFlow.Binding(
+      tuple=pb.TensorFlow.NamedTupleBinding(element=parameter_binding))
+
+  new_function_type = computation_types.FunctionType(parameter_type_list,
+                                                     proto_type.result)
+  serialized_type = type_serialization.serialize_type(new_function_type)
+
+  input_padded_proto = pb.Computation(
+      type=serialized_type,
+      tensorflow=pb.TensorFlow(
+          graph_def=proto.tensorflow.graph_def,
+          initialize_op=proto.tensorflow.initialize_op,
+          parameter=new_parameter_binding,
+          result=proto.tensorflow.result))
+
+  return computation_building_blocks.CompiledComputation(input_padded_proto)
+
+
+def pad_graph_inputs_to_match_type(comp, type_signature):
+  r"""Pads the parameter bindings of `comp` to match `type_signature`.
+
+  The padded parameters here are in effect dummy bindings--they are not
+  plugged in elsewhere in `comp`. This pattern is necessary to transform TFF
+  expressions of the form:
+
+                            Lambda(arg)
+                                |
+                              Call
+                             /     \
+          CompiledComputation       Tuple
+                                      |
+                                  Selection[i]
+                                      |
+                                    Ref(arg)
+
+  into the form:
+
+                          CompiledComputation
+
+  in the case where arg in the above picture represents an n-tuple, where n > 1.
+
+  Notice that some type manipulation must take place to execute the
+  transformation outlined above, or anything similar to it, since the Lambda
+  we are looking to replace accepts a parameter of an n-tuple, whereas the
+  `CompiledComputation` represented above accepts only a 1-tuple.
+  `pad_graph_inputs_to_match_type` is intended as an intermediate transform in
+  the transformation outlined above, since there may also need to be some
+  parameter permutation via `permute_graph_inputs`.
+
+  Notice also that the existing parameter bindings of `comp` must match the
+  first elements of `type_signature`. This is to ensure that we are attempting
+  to pad only compatible `CompiledComputation`s to a given type signature.
+
+  Args:
+    comp: Instance of `computation_building_blocks.CompiledComputation`
+      representing the graph whose inputs we want to pad to match
+      `type_signature`.
+    type_signature: Instance of `computation_types.NamedTupleType` representing
+      the type signature we wish to pad `comp` to accept as a parameter.
+
+  Returns:
+    A transformed version of `comp`, instance of
+    `computation_building_blocks.CompiledComputation` which takes an argument
+    of type `type_signature` and executes the same logic as `comp`. In
+    particular, this transformed version will have the same return type as
+    the original `comp`.
+
+  Raises:
+    TypeError: If the proto underlying `comp` has a parameter type which
+      is not of `NamedTupleType`, the `type_signature` argument is not of type
+      `NamedTupleType`, or there is a type mismatch between the declared
+      parameters of `comp` and the requested `type_signature`.
+    ValueError: If the requested `type_signature` is shorter than the
+      parameter type signature declared by `comp`.
+  """
+  py_typecheck.check_type(type_signature, computation_types.NamedTupleType)
+  py_typecheck.check_type(comp, computation_building_blocks.CompiledComputation)
+  proto = comp.proto
+  graph_def = proto.tensorflow.graph_def
+  graph_parameter_binding = proto.tensorflow.parameter
+  proto_type = type_serialization.deserialize_type(proto.type)
+  binding_oneof = graph_parameter_binding.WhichOneof('binding')
+  if binding_oneof != 'tuple':
+    raise TypeError(
+        'Can only pad inputs of a CompiledComputation with parameter type '
+        'tuple; you have attempted to pad a CompiledComputation '
+        'with parameter type {}'.format(binding_oneof))
+  # This line provides protection against an improperly serialized proto
+  py_typecheck.check_type(proto_type.parameter,
+                          computation_types.NamedTupleType)
+  parameter_bindings = [x for x in graph_parameter_binding.tuple.element]
+  parameter_type_elements = anonymous_tuple.to_elements(proto_type.parameter)
+  type_signature_elements = anonymous_tuple.to_elements(type_signature)
+  if len(parameter_bindings) > len(type_signature):
+    raise ValueError('We can only pad graph input bindings, never mask them. '
+                     'This means that a proposed type signature passed to '
+                     '`pad_graph_inputs_to_match_type` must have more elements '
+                     'than the existing type signature of the compiled '
+                     'computation. You have proposed a type signature of '
+                     'length {} be assigned to a computation with parameter '
+                     'type signature of length {}.'.format(
+                         len(type_signature), len(parameter_bindings)))
+  if any(x != type_signature_elements[idx]
+         for idx, x in enumerate(parameter_type_elements)):
+    raise TypeError(
+        'The existing elements of the parameter type signature '
+        'of the compiled computation in `pad_graph_inputs_to_match_type` '
+        'must match the beginning of the proposed new type signature; '
+        'you have proposed a parameter type of {} for a computation '
+        'with existing parameter type {}.'.format(type_signature,
+                                                  proto_type.parameter))
+  g = tf.Graph()
+  with g.as_default():
+    tf.graph_util.import_graph_def(
+        serialization_utils.unpack_graph_def(graph_def), name='')
+
+  elems_to_stamp = anonymous_tuple.to_elements(
+      type_signature)[len(parameter_bindings):]
+  for name, type_spec in elems_to_stamp:
+    if name is None:
+      stamp_name = 'name'
+    else:
+      stamp_name = name
+    _, stamped_binding = graph_utils.stamp_parameter_in_graph(
+        stamp_name, type_spec, g)
+    parameter_bindings.append(stamped_binding)
+    parameter_type_elements.append((name, type_spec))
+
+  new_parameter_binding = pb.TensorFlow.Binding(
+      tuple=pb.TensorFlow.NamedTupleBinding(element=parameter_bindings))
+  new_graph_def = g.as_graph_def()
+
+  new_function_type = computation_types.FunctionType(parameter_type_elements,
+                                                     proto_type.result)
+  serialized_type = type_serialization.serialize_type(new_function_type)
+
+  input_padded_proto = pb.Computation(
+      type=serialized_type,
+      tensorflow=pb.TensorFlow(
+          graph_def=serialization_utils.pack_graph_def(new_graph_def),
+          initialize_op=proto.tensorflow.initialize_op,
+          parameter=new_parameter_binding,
+          result=proto.tensorflow.result))
+
+  return computation_building_blocks.CompiledComputation(input_padded_proto)
