6f1a2303dfbeb09f86cbba432e3ea768dcc21aa5
Keith Rush <krush@google.com>
2019-05-13 16:52:26 -0700

    Remove dataset.output_*, silencing deprecation warning.
    PiperOrigin-RevId: 248038235

--- tensorflow_federated/python/core/impl/graph_utils.py
+++ tensorflow_federated/python/core/impl/graph_utils.py
@@ -826,7 +826,8 @@ def make_data_set_from_elements(graph, elements, element_type):
           singleton_ds = _make(elements[i:i + 1])
           ds = singleton_ds if ds is None else ds.concatenate(singleton_ds)
     ds_element_type = type_utils.tf_dtypes_and_shapes_to_type(
-        ds.output_types, ds.output_shapes)
+        tf.compat.v1.data.get_output_types(ds),
+        tf.compat.v1.data.get_output_shapes(ds))
     if not type_utils.is_assignable_from(element_type, ds_element_type):
       raise TypeError(
           'Failure during data set construction, expected elements of type {}, '
