9c9d4bcd9c068af1f9a883e608aad1c94aa60ba2
Keith Rush <krush@google.com>
2019-06-05 14:59:20 -0700

    Implements transform_type_postorder.
    PiperOrigin-RevId: 251725768

--- tensorflow_federated/python/core/impl/type_utils.py
+++ tensorflow_federated/python/core/impl/type_utils.py
@@ -1102,3 +1102,71 @@ def is_concrete_instance_of(type_with_concrete_elements,
 
   return are_equivalent_types(concretized_abstract_type,
                               type_with_concrete_elements)
+
+
+def transform_type_postorder(type_signature, transform_fn):
+  """Walks type tree of `type_signature` postorder, calling `transform_fn`.
+
+  Args:
+    type_signature: Instance of `computation_types.Type` to transform
+      recursively.
+    transform_fn: Transformation function to apply to each node in the type tree
+      of `type_signature`. Must be instance of Python function type.
+
+  Returns:
+    A possibly transformed version of `type_signature`, with each node in its
+    tree the result of applying `transform_fn` to the corresponding node in
+    `type_signature`.
+
+  Raises:
+    TypeError: If the types don't match the specification above.
+  """
+  # TODO(b/134525440): Investigate unifying the recursive methods in type_utils,
+  # rather than proliferating them.
+  # TODO(b/134595038): Revisit the change here to add a mutated flag.
+  py_typecheck.check_type(type_signature, computation_types.Type)
+  py_typecheck.check_callable(transform_fn)
+  if isinstance(type_signature, computation_types.FederatedType):
+    transformed_member, member_mutated = transform_fn(type_signature.member)
+    if member_mutated:
+      type_signature = computation_types.FederatedType(transformed_member,
+                                                       type_signature.placement,
+                                                       type_signature.all_equal)
+    fed_type_signature, type_signature_mutated = transform_fn(type_signature)
+    return fed_type_signature, type_signature_mutated or member_mutated
+  elif isinstance(type_signature, computation_types.SequenceType):
+    transformed_element, element_mutated = transform_fn(type_signature.element)
+    if element_mutated:
+      type_signature = computation_types.SequenceType(transformed_element)
+    seq_type_signature, type_signature_mutated = transform_fn(type_signature)
+    return seq_type_signature, type_signature_mutated or element_mutated
+  elif isinstance(type_signature, computation_types.FunctionType):
+    transformed_param, param_mutated = transform_fn(type_signature.parameter)
+    transformed_result, result_mutated = transform_fn(type_signature.result)
+    if param_mutated or result_mutated:
+      type_signature = computation_types.FunctionType(transformed_param,
+                                                      transformed_result)
+    fn_type_signature, fn_mutated = transform_fn(type_signature)
+    return fn_type_signature, fn_mutated or param_mutated or result_mutated
+  elif isinstance(type_signature, computation_types.NamedTupleType):
+    elems = []
+    elems_mutated = False
+    for element in anonymous_tuple.to_elements(type_signature):
+      transformed_element, element_mutated = transform_fn(element[1])
+      elems_mutated = elems_mutated or element_mutated
+      elems.append((element[0], transformed_element))
+    if elems_mutated:
+      if isinstance(type_signature,
+                    computation_types.NamedTupleTypeWithPyContainerType):
+        type_signature = computation_types.NamedTupleTypeWithPyContainerType(
+            elems,
+            computation_types.NamedTupleTypeWithPyContainerType
+            .get_container_type(type_signature))
+      else:
+        type_signature = computation_types.NamedTupleType(elems)
+    tuple_type_signature, tuple_mutated = transform_fn(type_signature)
+    return tuple_type_signature, elems_mutated or tuple_mutated
+  elif isinstance(type_signature,
+                  (computation_types.AbstractType, computation_types.TensorType,
+                   computation_types.PlacementType)):
+    return transform_fn(type_signature)
--- tensorflow_federated/python/core/impl/type_utils_test.py
+++ tensorflow_federated/python/core/impl/type_utils_test.py
@@ -1379,5 +1379,213 @@ class IsConcreteInstanceOf(test.TestCase):
     self.assertFalse(type_utils.is_concrete_instance_of(t2, t1))
 
 
+def _convert_tensor_to_float(type_spec):
+  if isinstance(type_spec, computation_types.TensorType):
+    return computation_types.TensorType(tf.float32, shape=type_spec.shape), True
+  return type_spec, False
+
+
+def _convert_abstract_type_to_tensor(type_spec):
+  if isinstance(type_spec, computation_types.AbstractType):
+    return computation_types.TensorType(tf.float32), True
+  return type_spec, False
+
+
+def _convert_placement_type_to_tensor(type_spec):
+  if isinstance(type_spec, computation_types.PlacementType):
+    return computation_types.TensorType(tf.float32), True
+  return type_spec, False
+
+
+def _convert_function_to_tensor(type_spec):
+  if isinstance(type_spec, computation_types.FunctionType):
+    return computation_types.TensorType(tf.float32), True
+  return type_spec, False
+
+
+def _convert_federated_to_tensor(type_spec):
+  if isinstance(type_spec, computation_types.FederatedType):
+    return computation_types.TensorType(tf.float32), True
+  return type_spec, False
+
+
+def _convert_sequence_to_tensor(type_spec):
+  if isinstance(type_spec, computation_types.SequenceType):
+    return computation_types.TensorType(tf.float32), True
+  return type_spec, False
+
+
+def _convert_tuple_to_tensor(type_spec):
+  if isinstance(type_spec, computation_types.NamedTupleType):
+    return computation_types.TensorType(tf.float32), True
+  return type_spec, False
+
+
+class TransformTypePostorderTest(tf.test.TestCase):
+
+  def test_raises_on_none_type(self):
+    with self.assertRaises(TypeError):
+      type_utils.transform_type_postorder(None, lambda x: x)
+
+  def test_raises_on_none_function(self):
+    with self.assertRaises(TypeError):
+      type_utils.transform_type_postorder(
+          computation_types.to_type(tf.int32), None)
+
+  def test_raises_on_non_type_first_arg(self):
+    with self.assertRaises(TypeError):
+      type_utils.transform_type_postorder(tf.int32, None)
+
+  def test_transforms_tensor(self):
+    orig_type = computation_types.to_type(tf.int32)
+    expected_type = computation_types.to_type(tf.float32)
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_tensor_to_float)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+  def test_transforms_federated_type(self):
+    orig_type = computation_types.FederatedType(tf.int32, placements.CLIENTS)
+    expected_type = computation_types.FederatedType(tf.float32,
+                                                    placements.CLIENTS)
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_tensor_to_float)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+  def test_updates_mutated_bit_at_federated(self):
+    orig_type = computation_types.FederatedType(tf.int32, placements.CLIENTS)
+    _, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_federated_to_tensor)
+    self.assertTrue(mutated)
+
+  def test_transforms_sequence(self):
+    orig_type = computation_types.SequenceType(tf.int32)
+    expected_type = computation_types.SequenceType(tf.float32)
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_tensor_to_float)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+  def test_updates_mutated_bit_at_sequence(self):
+    orig_type = computation_types.SequenceType(tf.int32)
+    _, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_sequence_to_tensor)
+    self.assertTrue(mutated)
+
+  def test_transforms_function(self):
+    orig_type = computation_types.FunctionType(tf.int32, tf.int64)
+    expected_type = computation_types.FunctionType(tf.float32, tf.float32)
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_tensor_to_float)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+  def test_updates_mutated_bit_at_function(self):
+    orig_type = computation_types.FunctionType(tf.int32, tf.int64)
+    _, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_function_to_tensor)
+    self.assertTrue(mutated)
+
+  def test_transforms_unnamed_tuple_type_preserving_tuple_container(self):
+    orig_type = computation_types.NamedTupleTypeWithPyContainerType(
+        [tf.int32, tf.float64], tuple)
+    expected_type = computation_types.NamedTupleTypeWithPyContainerType(
+        [tf.float32, tf.float32], tuple)
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_tensor_to_float)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+  def test_transforms_unnamed_tuple_type(self):
+    orig_type = computation_types.to_type([tf.int32, tf.float64])
+    expected_type = computation_types.to_type([tf.float32, tf.float32])
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_tensor_to_float)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+  def test_updates_mutated_bit_at_tuple(self):
+    orig_type = computation_types.to_type([tf.int32, tf.float64])
+    _, mutated = type_utils.transform_type_postorder(orig_type,
+                                                     _convert_tuple_to_tensor)
+    self.assertTrue(mutated)
+
+  def test_transforms_named_tuple_type(self):
+    orig_type = computation_types.to_type([('a', tf.int32), ('b', tf.float64)])
+    expected_type = computation_types.to_type([('a', tf.float32),
+                                               ('b', tf.float32)])
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_tensor_to_float)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+  def test_transforms_named_tuple_type_preserving_tuple_container(self):
+    orig_type = computation_types.NamedTupleTypeWithPyContainerType(
+        [('a', tf.int32), ('b', tf.float64)], dict)
+    expected_type = computation_types.NamedTupleTypeWithPyContainerType(
+        [('a', tf.float32), ('b', tf.float32)], dict)
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_tensor_to_float)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+  def test_transforms_abstract_type(self):
+    orig_type = computation_types.AbstractType('T')
+    expected_type = computation_types.to_type(tf.float32)
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_placement_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+  def test_transforms_placement_type(self):
+    orig_type = computation_types.PlacementType()
+    expected_type = computation_types.to_type(tf.float32)
+    result_type, mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_placement_type_to_tensor)
+    noop_type, not_mutated = type_utils.transform_type_postorder(
+        orig_type, _convert_abstract_type_to_tensor)
+    self.assertEqual(result_type, expected_type)
+    self.assertEqual(noop_type, orig_type)
+    self.assertTrue(mutated)
+    self.assertFalse(not_mutated)
+
+
 if __name__ == '__main__':
   tf.test.main()
