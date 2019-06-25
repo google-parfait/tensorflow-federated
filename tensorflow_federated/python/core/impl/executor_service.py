1180d294ad29390ef2619c4f48ce5f9ef5d366e8
Krzysztof Ostrowski <ostrowski@google.com>
2019-06-25 09:37:44 -0700

    An incremental step in creating the executor service, adds tensor value creation.
    PiperOrigin-RevId: 254986842

--- tensorflow_federated/python/core/impl/executor_service.py
+++ tensorflow_federated/python/core/impl/executor_service.py
@@ -18,16 +18,26 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import threading
+import uuid
+import grpc
+
 from tensorflow_federated.proto.v0 import executor_pb2
 from tensorflow_federated.proto.v0 import executor_pb2_grpc
+from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.core.impl import executor_base
+from tensorflow_federated.python.core.impl import executor_service_utils
 
 
 class ExecutorService(executor_pb2_grpc.ExecutorServicer):
   """A wrapper around a target executor that makes it into a gRPC service."""
 
-  # pylint: disable=useless-super-delegation
-  def __init__(self, *args, **kwargs):
+  def __init__(self, executor, *args, **kwargs):
+    py_typecheck.check_type(executor, executor_base.Executor)
     super(ExecutorService, self).__init__(*args, **kwargs)
+    self._executor = executor
+    self._lock = threading.Lock()
+    self._values = {}
 
   def CreateValue(self, request, context):
     """Creates a value embedded in the executor.
@@ -39,4 +49,16 @@ class ExecutorService(executor_pb2_grpc.ExecutorServicer):
     Returns:
       An instance of `executor_pb2.CreateValueResponse`.
     """
-    return executor_pb2.CreateValueResponse()
+    py_typecheck.check_type(request, executor_pb2.CreateValueRequest)
+    try:
+      value, value_type = executor_service_utils.deserialize_tensor_value(
+          request.value)
+      value_id = str(uuid.uuid4())
+      future_val = self._executor.create_value(value, value_type)
+      with self._lock:
+        self._values[value_id] = future_val
+      return executor_pb2.CreateValueResponse(
+          value_ref=executor_pb2.ValueRef(id=value_id))
+    except (ValueError, TypeError):
+      context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+      return executor_pb2.CreateValueResponse()
