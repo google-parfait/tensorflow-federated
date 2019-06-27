4bcf331761bde851b9d219fcf4e0f6e7b6ec3b3f
Krzysztof Ostrowski <ostrowski@google.com>
2019-06-27 11:03:22 -0700

    Cleanup in the executor service and added support for the remaining methods in the interface and remaining types of values.
    PiperOrigin-RevId: 255437308

--- tensorflow_federated/python/core/impl/executor_service_test.py
+++ tensorflow_federated/python/core/impl/executor_service_test.py
@@ -18,7 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import asyncio
+import threading
 
 from absl.testing import absltest
 
@@ -30,29 +30,31 @@ import tensorflow as tf
 
 from tensorflow_federated.proto.v0 import executor_pb2
 from tensorflow_federated.proto.v0 import executor_pb2_grpc
+from tensorflow_federated.python.common_libs import py_typecheck
 from tensorflow_federated.python.core.api import computations
 from tensorflow_federated.python.core.impl import eager_executor
+from tensorflow_federated.python.core.impl import executor_base
 from tensorflow_federated.python.core.impl import executor_service
 from tensorflow_federated.python.core.impl import executor_service_utils
+from tensorflow_federated.python.core.impl import executor_value_base
 
 
-class ExecutorServiceTest(absltest.TestCase):
+class TestEnv(object):
+  """A test environment that consists of a single client and backend service."""
 
-  def setUp(self):
-    super(ExecutorServiceTest, self).setUp()
+  def __init__(self, executor):
     port = portpicker.pick_unused_port()
     server_pool = logging_pool.pool(max_workers=1)
     self._server = grpc.server(server_pool)
     self._server.add_insecure_port('[::]:{}'.format(port))
-    self._service = executor_service.ExecutorService(
-        eager_executor.EagerExecutor())
+    self._service = executor_service.ExecutorService(executor)
     executor_pb2_grpc.add_ExecutorServicer_to_server(self._service,
                                                      self._server)
     self._server.start()
     self._channel = grpc.insecure_channel('localhost:{}'.format(port))
     self._stub = executor_pb2_grpc.ExecutorStub(self._channel)
 
-  def tearDown(self):
+  def __del__(self):
     # TODO(b/134543154): Find some way of cleanly disposing of channels that is
     # consistent between Google-internal and OSS stacks.
     try:
@@ -63,42 +65,170 @@ class ExecutorServiceTest(absltest.TestCase):
       del self._stub
       del self._channel
     self._server.stop(None)
-    super(ExecutorServiceTest, self).tearDown()
 
-  def _extract_value_from_service(self, value_id):
-    # pylint: disable=protected-access
-    with self._service._lock:
-      future_val = self._service._values[value_id]
-    # pylint: enable=protected-access
+  @property
+  def stub(self):
+    return self._stub
 
-    value = asyncio.get_event_loop().run_until_complete(future_val)
-    self.assertIsInstance(value, eager_executor.EagerValue)
+  def get_value(self, value_id):
+    response = self._stub.Compute(
+        executor_pb2.ComputeRequest(
+            value_ref=executor_pb2.ValueRef(id=value_id)))
+    py_typecheck.check_type(response, executor_pb2.ComputeResponse)
+    value, _ = executor_service_utils.deserialize_value(response.value)
     return value
 
+
+class ExecutorServiceTest(absltest.TestCase):
+
+  def test_executor_service_slowly_create_tensor_value(self):
+
+    class SlowExecutorValue(executor_value_base.ExecutorValue):
+
+      def __init__(self, v, t):
+        self._v = v
+        self._t = t
+
+      @property
+      def type_signature(self):
+        return self._t
+
+      async def compute(self):
+        return self._v
+
+    class SlowExecutor(executor_base.Executor):
+
+      def __init__(self):
+        self.status = 'idle'
+        self.busy = threading.Event()
+        self.done = threading.Event()
+
+      async def create_value(self, value, type_spec=None):
+        self.status = 'busy'
+        self.busy.set()
+        self.done.wait()
+        self.status = 'done'
+        return SlowExecutorValue(value, type_spec)
+
+      async def create_call(self, comp, arg=None):
+        raise NotImplementedError
+
+      async def create_tuple(self, elements):
+        raise NotImplementedError
+
+      async def create_selection(self, source, index=None, name=None):
+        raise NotImplementedError
+
+    ex = SlowExecutor()
+    env = TestEnv(ex)
+    self.assertEqual(ex.status, 'idle')
+    value_proto, _ = executor_service_utils.serialize_value(10, tf.int32)
+    response = env.stub.CreateValue(
+        executor_pb2.CreateValueRequest(value=value_proto))
+    ex.busy.wait()
+    self.assertEqual(ex.status, 'busy')
+    ex.done.set()
+    value = env.get_value(response.value_ref.id)
+    self.assertEqual(ex.status, 'done')
+    self.assertEqual(value, 10)
+
   def test_executor_service_create_tensor_value(self):
+    env = TestEnv(eager_executor.EagerExecutor())
     value_proto, _ = executor_service_utils.serialize_value(
         tf.constant(10.0).numpy(), tf.float32)
-    request = executor_pb2.CreateValueRequest(value=value_proto)
-    response = self._stub.CreateValue(request)
+    response = env.stub.CreateValue(
+        executor_pb2.CreateValueRequest(value=value_proto))
     self.assertIsInstance(response, executor_pb2.CreateValueResponse)
     value_id = str(response.value_ref.id)
-    value = self._extract_value_from_service(value_id)
-    self.assertEqual(value.internal_representation.numpy(), 10.0)
+    value = env.get_value(value_id)
+    self.assertEqual(value, 10.0)
+    del env
 
-  def test_executor_service_create_computation_value(self):
+  def test_executor_service_create_no_arg_computation_value_and_call(self):
+    env = TestEnv(eager_executor.EagerExecutor())
 
     @computations.tf_computation
     def comp():
       return tf.constant(10)
 
     value_proto, _ = executor_service_utils.serialize_value(comp)
-    request = executor_pb2.CreateValueRequest(value=value_proto)
-    response = self._stub.CreateValue(request)
+    response = env.stub.CreateValue(
+        executor_pb2.CreateValueRequest(value=value_proto))
     self.assertIsInstance(response, executor_pb2.CreateValueResponse)
+    response = env.stub.CreateCall(
+        executor_pb2.CreateCallRequest(function_ref=response.value_ref))
+    self.assertIsInstance(response, executor_pb2.CreateCallResponse)
     value_id = str(response.value_ref.id)
-    value = self._extract_value_from_service(value_id)
-    self.assertTrue(callable(value.internal_representation))
-    self.assertEqual(value.internal_representation().numpy(), 10.0)
+    value = env.get_value(value_id)
+    self.assertEqual(value, 10)
+    del env
+
+  def test_executor_service_create_one_arg_computation_value_and_call(self):
+    env = TestEnv(eager_executor.EagerExecutor())
+
+    @computations.tf_computation(tf.int32)
+    def comp(x):
+      return tf.add(x, 1)
+
+    value_proto, _ = executor_service_utils.serialize_value(comp)
+    response = env.stub.CreateValue(
+        executor_pb2.CreateValueRequest(value=value_proto))
+    self.assertIsInstance(response, executor_pb2.CreateValueResponse)
+    comp_ref = response.value_ref
+
+    value_proto, _ = executor_service_utils.serialize_value(10, tf.int32)
+    response = env.stub.CreateValue(
+        executor_pb2.CreateValueRequest(value=value_proto))
+    self.assertIsInstance(response, executor_pb2.CreateValueResponse)
+    arg_ref = response.value_ref
+
+    response = env.stub.CreateCall(
+        executor_pb2.CreateCallRequest(
+            function_ref=comp_ref, argument_ref=arg_ref))
+    self.assertIsInstance(response, executor_pb2.CreateCallResponse)
+    value_id = str(response.value_ref.id)
+    value = env.get_value(value_id)
+    self.assertEqual(value, 11)
+    del env
+
+  def test_executor_service_create_and_select_from_tuple(self):
+    env = TestEnv(eager_executor.EagerExecutor())
+
+    value_proto, _ = executor_service_utils.serialize_value(10, tf.int32)
+    response = env.stub.CreateValue(
+        executor_pb2.CreateValueRequest(value=value_proto))
+    self.assertIsInstance(response, executor_pb2.CreateValueResponse)
+    ten_ref = response.value_ref
+    self.assertEqual(env.get_value(ten_ref.id), 10)
+
+    value_proto, _ = executor_service_utils.serialize_value(20, tf.int32)
+    response = env.stub.CreateValue(
+        executor_pb2.CreateValueRequest(value=value_proto))
+    self.assertIsInstance(response, executor_pb2.CreateValueResponse)
+    twenty_ref = response.value_ref
+    self.assertEqual(env.get_value(twenty_ref.id), 20)
+
+    response = env.stub.CreateTuple(
+        executor_pb2.CreateTupleRequest(element=[
+            executor_pb2.CreateTupleRequest.Element(
+                name='a', value_ref=ten_ref),
+            executor_pb2.CreateTupleRequest.Element(
+                name='b', value_ref=twenty_ref)
+        ]))
+    self.assertIsInstance(response, executor_pb2.CreateTupleResponse)
+    tuple_ref = response.value_ref
+    self.assertEqual(str(env.get_value(tuple_ref.id)), '<a=10,b=20>')
+
+    for arg_name, arg_val, result_val in [('name', 'a', 10), ('name', 'b', 20),
+                                          ('index', 0, 10), ('index', 1, 20)]:
+      response = env.stub.CreateSelection(
+          executor_pb2.CreateSelectionRequest(
+              source_ref=tuple_ref, **{arg_name: arg_val}))
+      self.assertIsInstance(response, executor_pb2.CreateSelectionResponse)
+      selection_ref = response.value_ref
+      self.assertEqual(env.get_value(selection_ref.id), result_val)
+
+    del env
 
 
 if __name__ == '__main__':
