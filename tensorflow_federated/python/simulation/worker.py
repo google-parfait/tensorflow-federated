670444960975e46ed134ea6b3e8ac0851b474270
Krzysztof Ostrowski <ostrowski@google.com>
2019-06-28 16:09:08 -0700

    Fixes a typo in the comment for simulation worker.
    PiperOrigin-RevId: 255686477

--- tensorflow_federated/python/simulation/worker.py
+++ tensorflow_federated/python/simulation/worker.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""An internal Google-specific worker binary for deployment on borg."""
+"""A generic worker binary for deployment, e.g., on GCP."""
 
 import concurrent
 import time
