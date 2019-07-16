9ed3c019f703db52225fd566222088120f016d14
Krishna Pillutla <krishnap25@user.noreply.github.com>
2019-07-15 19:24:14 -0700

    Add RFA implementation and aggregation test

---
+++ tensorflow_federated/python/research/robust_aggregation/__init__.py
@@ -0,0 +1,9 @@
+# TODO: copyright notice
+
+from .rfa import build_stateless_robust_aggregation
+from .rfa import build_robust_federated_aggregation_process
+
+__all__ = ['build_stateless_robust_aggregation',
+           'build_robust_federated_aggregation_process'
+          ]
+
