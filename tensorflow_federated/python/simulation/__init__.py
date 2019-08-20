7e28bb80f27a9a715b2529468fde2edd2cc27b8f
Brendan McMahan <mcmahan@google.com>
2019-08-20 07:19:15 -0700

    Expose ConcreteClientData in the public API. It is useful to express pre-processing that is a function of client_id as well as the dataset.
    PiperOrigin-RevId: 264377982

--- tensorflow_federated/python/simulation/__init__.py
+++ tensorflow_federated/python/simulation/__init__.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 from tensorflow_federated.python.simulation import datasets
 from tensorflow_federated.python.simulation.client_data import ClientData
+from tensorflow_federated.python.simulation.client_data import ConcreteClientData
 from tensorflow_federated.python.simulation.file_per_user_client_data import FilePerUserClientData
 from tensorflow_federated.python.simulation.from_tensor_slices_client_data import FromTensorSlicesClientData
 from tensorflow_federated.python.simulation.hdf5_client_data import HDF5ClientData
@@ -28,6 +29,7 @@ from tensorflow_federated.python.simulation.transforming_client_data import Tran
 # Used by doc generation script.
 _allowed_symbols = [
     "ClientData",
+    "ConcreteClientData",
     "FilePerUserClientData",
     "FromTensorSlicesClientData",
     "HDF5ClientData",
