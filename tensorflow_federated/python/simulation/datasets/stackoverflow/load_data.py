23c4a622d0dd1df9e0f39d94d996d52ca8504e9c
Sergii Khomenko <sergii.khomenko@stylight.com>
2019-08-22 22:09:55 +0200

    Minor fixes to datasets

--- tensorflow_federated/python/simulation/datasets/stackoverflow/load_data.py
+++ tensorflow_federated/python/simulation/datasets/stackoverflow/load_data.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Libraries for the Shakesepare dataset for federated learning simulation."""
+"""Libraries for the Stackoverflow dataset for federated learning simulation."""
 
 from __future__ import absolute_import
 from __future__ import division
