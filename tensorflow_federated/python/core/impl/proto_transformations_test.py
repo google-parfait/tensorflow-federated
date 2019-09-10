e4af2f84f5230126fec3590c6f2bc34b414db5de
Michael Reneer <michaelreneer@google.com>
2019-09-10 15:08:43 -0700

    Remove dependency `proto_transformations_test` has on `context_stack_impl` and `tensorflow_serialization`.
    PiperOrigin-RevId: 268319392

--- tensorflow_federated/python/core/impl/proto_transformations_test.py
+++ tensorflow_federated/python/core/impl/proto_transformations_test.py
@@ -16,19 +16,37 @@
 from absl.testing import absltest
 import tensorflow as tf
 
-from tensorflow_federated.python.core.impl import context_stack_impl
+from tensorflow_federated.proto.v0 import computation_pb2 as pb
+from tensorflow_federated.python.common_libs import serialization_utils
+from tensorflow_federated.python.core.api import computation_types
 from tensorflow_federated.python.core.impl import proto_transformations
-from tensorflow_federated.python.core.impl import tensorflow_serialization
 from tensorflow_federated.python.core.impl.compiler import building_block_analysis
 from tensorflow_federated.python.core.impl.compiler import building_block_factory
 from tensorflow_federated.python.core.impl.compiler import building_blocks
 from tensorflow_federated.python.core.impl.compiler import test_utils
+from tensorflow_federated.python.core.impl.compiler import type_serialization
+from tensorflow_federated.python.core.impl.utils import tensorflow_utils
 
 
-def _create_compiled_computation(py_fn, arg_type):
-  proto, _ = tensorflow_serialization.serialize_py_fn_as_tf_computation(
-      py_fn, arg_type, context_stack_impl.context_stack)
-  return building_blocks.CompiledComputation(proto)
+def _create_proto_with_unnecessary_op():
+  parameter_type = tf.int32
+
+  with tf.Graph().as_default() as graph:
+    parameter_value, parameter_binding = tensorflow_utils.stamp_parameter_in_graph(
+        'x', parameter_type, graph)
+    result_type, result_binding = tensorflow_utils.capture_result_from_graph(
+        parameter_value, graph)
+    unnecessary_op = tf.constant(0)
+    tensorflow_utils.capture_result_from_graph(unnecessary_op, graph)
+
+  function_type = computation_types.FunctionType(parameter_type, result_type)
+  serialized_function_type = type_serialization.serialize_type(function_type)
+  return pb.Computation(
+      type=serialized_function_type,
+      tensorflow=pb.TensorFlow(
+          graph_def=serialization_utils.pack_graph_def(graph.as_graph_def()),
+          parameter=parameter_binding,
+          result=result_binding))
 
 
 class PruneTensorFlowProtoTest(absltest.TestCase):
@@ -51,29 +69,20 @@ class PruneTensorFlowProtoTest(absltest.TestCase):
     self.assertEqual(ops_before, ops_after)
 
   def test_reduces_unnecessary_ops(self):
-
-    def bad_fn(x):
-      _ = tf.constant(0)
-      return x
-
-    comp = _create_compiled_computation(bad_fn, tf.int32)
-    reduced_proto = proto_transformations.prune_tensorflow_proto(comp.proto)
+    proto = _create_proto_with_unnecessary_op()
+    comp = building_blocks.CompiledComputation(proto)
+    reduced_proto = proto_transformations.prune_tensorflow_proto(proto)
     reduced_comp = building_blocks.CompiledComputation(reduced_proto)
     ops_before = building_block_analysis.count_tensorflow_ops_in(comp)
     ops_after = building_block_analysis.count_tensorflow_ops_in(reduced_comp)
     self.assertLess(ops_after, ops_before)
 
   def test_prune_does_not_change_exeuction(self):
-
-    def bad_fn(x):
-      _ = tf.constant(0)
-      return x
-
-    comp = _create_compiled_computation(bad_fn, tf.int32)
-    reduced_proto = proto_transformations.prune_tensorflow_proto(comp.proto)
+    proto = _create_proto_with_unnecessary_op()
+    reduced_proto = proto_transformations.prune_tensorflow_proto(proto)
     for k in range(5):
       self.assertEqual(
-          test_utils.run_tensorflow(comp.proto, k),
+          test_utils.run_tensorflow(proto, k),
           test_utils.run_tensorflow(reduced_proto, k))
 
 
