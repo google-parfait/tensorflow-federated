2e3d82c698544aa6b77efef57909c535e912d602
Michael Reneer <michaelreneer@google.com>
2019-11-14 09:10:16 -0800

    Move `computation_wrapper` to the `wrappers` package.
    PiperOrigin-RevId: 280442423

--- tensorflow_federated/python/core/impl/wrappers/computation_wrapper_test.py
+++ tensorflow_federated/python/core/impl/wrappers/computation_wrapper_test.py
@@ -17,10 +17,10 @@ import tensorflow as tf
 
 from tensorflow_federated.python.common_libs import test
 from tensorflow_federated.python.core.api import computation_types
-from tensorflow_federated.python.core.impl import computation_wrapper
 from tensorflow_federated.python.core.impl import context_base
 from tensorflow_federated.python.core.impl import context_stack_impl
 from tensorflow_federated.python.core.impl.utils import function_utils
+from tensorflow_federated.python.core.impl.wrappers import computation_wrapper
 
 
 class WrappedForTest(function_utils.ConcreteFunction):
