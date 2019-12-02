7e5d76e2d780f676884985ece664bb1a2a823309
Michael Reneer <michaelreneer@google.com>
2019-12-02 08:25:38 -0800

    Update flars experiment to use `checkpoint_manager`.
    PiperOrigin-RevId: 283344782

--- tensorflow_federated/python/research/flars/run_emnist.py
+++ tensorflow_federated/python/research/flars/run_emnist.py
@@ -16,7 +16,6 @@
 
 import collections
 import functools
-import io
 import logging
 import os
 import pprint
@@ -26,7 +25,6 @@ import time
 
 from absl import app
 from absl import flags
-import attr
 import pandas as pd
 import tensorflow as tf
 import tensorflow_federated as tff
@@ -37,7 +35,7 @@ from tensorflow_federated.python.research.baselines.emnist import models
 from tensorflow_federated.python.research.flars import flars_fedavg
 from tensorflow_federated.python.research.flars import flars_optimizer
 from tensorflow_federated.python.research.simple_fedavg import simple_fedavg
-from tensorflow_federated.python.research.utils import checkpoint_utils
+from tensorflow_federated.python.research.utils import checkpoint_manager
 from tensorflow_federated.python.research.utils import utils_impl
 
 with utils_impl.record_new_flags() as hparam_flags:
@@ -97,15 +95,15 @@ flags.DEFINE_boolean(
     'exists.')
 
 FLAGS = flags.FLAGS
-CHECKPOINT_PREFIX = 'ckpt_'
 
 
-def federated_averaging_training_loop(model_fn,
-                                      server_optimizer_fn,
-                                      client_datasets_fn,
-                                      total_rounds=500,
-                                      rounds_per_eval=1,
-                                      metrics_hook=lambda *args: None):
+def _federated_averaging_training_loop(model_fn,
+                                       server_optimizer_fn,
+                                       client_datasets_fn,
+                                       evaluate_fn,
+                                       total_rounds=500,
+                                       rounds_per_eval=1,
+                                       metrics_hook=None):
   """A simple example of training loop for the Federated Averaging algorithm.
 
   Args:
@@ -114,16 +112,21 @@ def federated_averaging_training_loop(model_fn,
       `tf.keras.optimizers.Optimizer`.
     client_datasets_fn: A function that takes the round number, and returns a
       list of `tf.data.Datset`, one per client.
+    evaluate_fn: A function that takes state, performs evaluation, and returns
+      evaluations metrics.
     total_rounds: Number of rounds to train.
     rounds_per_eval: How often to call the  `metrics_hook` function.
-    metrics_hook: A function taking arguments (server_state, train_metrics,
-      round_num) and performs evaluation. Optional.
+    metrics_hook: A function taking arguments (training metrics,
+      evaluation metrics, and round number). Optional.
 
   Returns:
     Final `ServerState`.
   """
-  logging.info('Starting federated_training_loop')
+  logging.info('Starting federated training loop')
+
   checkpoint_dir = os.path.join(FLAGS.root_output_dir, FLAGS.exp_name)
+  checkpoint_manager_obj = checkpoint_manager.FileCheckpointManager(
+      checkpoint_dir)
 
   if FLAGS.server_optimizer != 'flars':
     iterative_process = simple_fedavg.build_federated_averaging_process(
@@ -134,43 +137,28 @@ def federated_averaging_training_loop(model_fn,
         model_fn, server_optimizer_fn=server_optimizer_fn)
     ServerState = flars_fedavg.ServerState  # pylint: disable=invalid-name
 
-  server_state = ServerState.from_anon_tuple(iterative_process.initialize())
-  round_num = None
-  train_metrics = {}
+  # construct an initial state here to act as a checkpoint template
+  inital_state = iterative_process.initialize()
+  inital_state = ServerState.from_anon_tuple(inital_state)
 
-  latest_checkpoint_dir = checkpoint_utils.latest_checkpoint(
-      checkpoint_dir, CHECKPOINT_PREFIX)
-  logging.info('Looking for checkpoints in [%s/%s]', checkpoint_dir,
-               CHECKPOINT_PREFIX)
-  while latest_checkpoint_dir is not None:
-    # Restart from a previous round.
-    logging.info('Loading a previous checkpoint')
-    try:
-      server_state, metrics_hook.results, round_num = read_checkpoint(
-          latest_checkpoint_dir, server_state)
-      break
-    except OSError as e:
-      # Likely corrupted checkpoint, possibly job died while writing. Delete the
-      # checkpoint directory and try again.
-      logging.error('Exception [%s]', e)
-      logging.warning('Deleteing likely corrupted checkpoint at [%s]',
-                      latest_checkpoint_dir)
-      tf.io.gfile.rmtree(latest_checkpoint_dir)
-      latest_checkpoint_dir = checkpoint_utils.latest_checkpoint(
-          checkpoint_dir, CHECKPOINT_PREFIX)
+  logging.info('Looking for checkpoints in \'%s\'', checkpoint_dir)
+  state, round_num = checkpoint_manager_obj.load_latest_checkpoint(inital_state)
 
-  if round_num is not None:
-    logging.info('Restarted from checkpoint round %d', round_num)
-  else:
-    # Write the initial checkpoint
+  if state is None:
     logging.info('No previous checkpoints, initializing experiment')
+    state = inital_state
     round_num = 0
-    metrics_hook(server_state, train_metrics, round_num)
-    write_checkpoint(checkpoint_dir, server_state, metrics_hook.results,
-                     round_num)
+    if metrics_hook is not None:
+      eval_metrics = evaluate_fn(state)
+      metrics_hook({}, eval_metrics, round_num)
+    checkpoint_manager_obj.save_checkpoint(state, 0)
+  else:
+    logging.info('Restarted from checkpoint round %d', round_num)
 
   while round_num < total_rounds:
     round_num += 1
+    train_metrics = {}
+
     # Reset the executor to clear the cache, and clear the default graph to
     # garbage collect tf.Functions that will no longer be used.
     tff.framework.set_default_executor(
@@ -179,35 +167,31 @@ def federated_averaging_training_loop(model_fn,
 
     round_start_time = time.time()
     data_prep_start_time = time.time()
-    federated_train_data = client_datasets_fn(round_num)
+    train_data = client_datasets_fn(round_num)
     train_metrics['prepare_datasets_secs'] = time.time() - data_prep_start_time
 
     training_start_time = time.time()
-    anon_tuple_server_state, tff_train_metrics = iterative_process.next(
-        server_state, federated_train_data)
-    server_state = ServerState.from_anon_tuple(anon_tuple_server_state)
-    train_metrics.update(tff_train_metrics._asdict(recursive=True))
+    state, tff_train_metrics = iterative_process.next(state, train_data)
+    state = ServerState.from_anon_tuple(state)
+    tff_train_metrics = tff_train_metrics._asdict(recursive=True)
+    train_metrics.update(tff_train_metrics)
     train_metrics['training_secs'] = time.time() - training_start_time
 
     logging.info('Round {:2d} elapsed time: {:.2f}s .'.format(
         round_num, (time.time() - round_start_time)))
     train_metrics['total_round_secs'] = time.time() - round_start_time
 
-    if round_num % FLAGS.rounds_per_checkpoint == 0:
-      write_checkpoint_start_time = time.time()
-      write_checkpoint(checkpoint_dir, server_state, metrics_hook.results,
-                       round_num)
-      train_metrics['write_checkpoint_secs'] = (
-          time.time() - write_checkpoint_start_time)
+    if (round_num % FLAGS.rounds_per_checkpoint == 0 or
+        round_num == total_rounds):
+      save_checkpoint_start_time = time.time()
+      checkpoint_manager_obj.save_checkpoint(state, round_num)
+      train_metrics['save_checkpoint_secs'] = (
+          time.time() - save_checkpoint_start_time)
 
-    if round_num % rounds_per_eval == 0:
-      metrics_hook(server_state, train_metrics, round_num)
-
-  metrics_hook(server_state, train_metrics, total_rounds)
-  write_checkpoint(checkpoint_dir, server_state, metrics_hook.results,
-                   round_num)
-
-  return server_state
+    if round_num % rounds_per_eval == 0 or round_num == total_rounds:
+      if metrics_hook is not None:
+        eval_metrics = evaluate_fn(state)
+        metrics_hook(train_metrics, eval_metrics, round_num)
 
 
 def _check_not_exists(f, disable_check_exists=False):
@@ -223,156 +207,92 @@ def _check_not_exists(f, disable_check_exists=False):
     sys.exit(1)
 
 
-@attr.s(frozen=False)
-class MetricsHook(object):
+class _MetricsHook(object):
   """A callback for evaluation.
 
-  This class holds all the logic for evaluating the FedAvg on EMNIST
-  classification and writing output for later analysis (to .csv files and
+  This class holds all the logic for writing output for later analysis (to .csv
+  files and
   tensorboard). Hyperparameters are also recorded.
-
-  This class should be constructed via the `MetricsHook.build` classmethod.
   """
-  # Derived, conceptually post-init or in constructor, used in methods.
-  results_file = attr.ib()
-  summary_writer = attr.ib()
-  eval_dataset = attr.ib()
-  model = attr.ib()
-  results = attr.ib(factory=pd.DataFrame)
 
-  @classmethod
-  def build(cls, exp_name, output_dir, eval_dataset, hparam_dict):
-    """Constructs the MetricsHook.
+  def __init__(self, experiment_name, output_dir, hparam_dict):
+    """Returns an initalized `MetricsHook`.
 
     Args:
-      exp_name: A unique filesystem-friendly name for the experiment.
+      experiment_name: A unique filesystem-friendly name for the experiment.
       output_dir: A root output directory used for all experiment runs in a
-        grid. The MetricsHook will combine this with exp_name to form suitable
-        output directories for this run.
-      eval_dataset: Evaluation dataset.
+        grid. The `MetricsHook` will combine this with `experiment_name` to form
+        suitable output directories for this run.
       hparam_dict: A dictionary of hyperparameters to be recorded to .csv and
         exported to TensorBoard.
-
-    Returns:
-      The `MetricsHook` object.
     """
 
-    summary_logdir = os.path.join(output_dir, 'logdir/{}'.format(exp_name))
+    summary_logdir = os.path.join(output_dir,
+                                  'logdir/{}'.format(experiment_name))
     _check_not_exists(summary_logdir, FLAGS.disable_check_exists)
     tf.io.gfile.makedirs(summary_logdir)
 
-    summary_writer = tf.compat.v2.summary.create_file_writer(
-        summary_logdir, name=exp_name)
-    with summary_writer.as_default():
+    self._summary_writer = tf.compat.v2.summary.create_file_writer(
+        summary_logdir, name=experiment_name)
+    with self._summary_writer.as_default():
       hp.hparams(hparam_dict)
 
     # Using .bz2 rather than .zip due to
     # https://github.com/pandas-dev/pandas/issues/26023
-    results_file = os.path.join(output_dir, exp_name, 'results.csv.bz2')
+    self._results_file = os.path.join(output_dir, experiment_name,
+                                      'results.csv.bz2')
 
     # Also write the hparam_dict to a CSV:
-    hparam_dict['results_file'] = results_file
-    hparams_file = os.path.join(output_dir, exp_name, 'hparams.csv')
+    hparam_dict['results_file'] = self._results_file
+    hparams_file = os.path.join(output_dir, experiment_name, 'hparams.csv')
     utils_impl.atomic_write_to_csv(pd.Series(hparam_dict), hparams_file)
 
-    model = create_compiled_keras_model()
-
     logging.info('Writing ...')
-    logging.info('   result csv to: %s', results_file)
+    logging.info('   result csv to: %s', self._results_file)
     logging.info('    summaries to: %s', summary_logdir)
 
-    return cls(
-        results_file=results_file,
-        summary_writer=summary_writer,
-        eval_dataset=eval_dataset,
-        model=model)
+    _check_not_exists(self._results_file, FLAGS.disable_check_exists)
 
-  def __attrs_post_init__(self):
-    _check_not_exists(self.results_file, FLAGS.disable_check_exists)
-
-  def __call__(self, server_state, train_metrics, round_num):
+  def __call__(self, train_metrics, eval_metrics, round_num):
     """A function suitable for passing as an eval hook to the training_loop.
 
     Args:
-      server_state: A `ServerState`.
       train_metrics: A `dict` of training metrics computed in TFF.
+      eval_metrics: A `dict` of evalutation metrics computed in TFF.
       round_num: The current round number.
     """
-    tff.learning.assign_weights_to_keras_model(self.model, server_state.model)
-    eval_metrics = self.model.evaluate(self.eval_dataset, verbose=0)
-
     metrics = {
-        'train':
-            train_metrics,
-        'eval':
-            collections.OrderedDict(
-                zip(['loss', 'sparse_categorical_accuracy'], eval_metrics))
+        'train': train_metrics,
+        'eval': eval_metrics,
+        'round': round_num,
     }
     flat_metrics = tree.flatten_with_path(metrics)
     flat_metrics = [
         ('/'.join(map(str, path)), item) for path, item in flat_metrics
     ]
     flat_metrics = collections.OrderedDict(flat_metrics)
-    flat_metrics['round'] = round_num
 
     logging.info('Evaluation at round {:d}:\n{!s}'.format(
         round_num, pprint.pformat(flat_metrics)))
 
     # Also write metrics to a tf.summary logdir
-    with self.summary_writer.as_default():
+    with self._summary_writer.as_default():
       for name, value in flat_metrics.items():
         tf.compat.v2.summary.scalar(name, value, step=round_num)
 
-    self.results = self.results.append(flat_metrics, ignore_index=True)
-    utils_impl.atomic_write_to_csv(self.results, self.results_file)
+    if tf.io.gfile.exists(self._results_file):
+      metrics = pd.read_csv(
+          self._results_file, header=0, index_col=0, engine='c')
+      # Remove everything after `round_num`, in case the experiment was
+      # restarted at an earlier checkpoint we want to avoid duplicate metrics.
+      metrics = metrics[:round_num]
+      metrics = metrics.append(flat_metrics, ignore_index=True)
+    else:
+      metrics = pd.DataFrame(flat_metrics, index=[0])
+    utils_impl.atomic_write_to_csv(metrics, self._results_file)
 
 
-@attr.s(frozen=False)
-class ExperimentState(object):
-  round_num = attr.ib()
-  metrics_csv_string = attr.ib()
-  server_state = attr.ib()
-
-
-def write_checkpoint(checkpoint_dir, server_state, metrics_dataframe,
-                     round_num):
-  """Write the current experiment state to disk."""
-  # Clean-up old checkpoints if more than 5 exist; but not the initialization
-  # checkpoint.
-  checkpoints = sorted(tf.io.gfile.glob(os.path.join(checkpoint_dir, 'ckpt_*')))
-  for checkpoint in checkpoints[1:-3]:
-    tf.io.gfile.rmtree(checkpoint)
-
-  # We must flatten the pd.Dataframe to a single string, otherwise we don't
-  # know the nested structure (how many rounds have passed) to unpack
-  # in `checkpoint_utils.load()` during `read_checkpoint`.
-  csv_string = io.StringIO()
-  metrics_dataframe.to_csv(csv_string, header=True)
-
-  experiment_state = ExperimentState(
-      round_num=round_num,
-      metrics_csv_string=csv_string.getvalue(),
-      server_state=server_state)
-  checkpoint_utils.save(
-      experiment_state,
-      os.path.join(checkpoint_dir, 'ckpt_{:03d}'.format(round_num)))
-
-
-def read_checkpoint(filepath, example_server_state):
-  """Read a previously saved experiment state to memory."""
-  experiment_state = ExperimentState(
-      round_num=0, metrics_csv_string='', server_state=example_server_state)
-  experiment_state = checkpoint_utils.load(filepath, experiment_state)
-  metrics_dict = pd.read_csv(
-      io.BytesIO(experiment_state.metrics_csv_string.numpy()),
-      header=0,
-      index_col=0,
-      engine='c')
-  return (experiment_state.server_state, metrics_dict,
-          experiment_state.round_num.numpy())
-
-
-def create_compiled_keras_model():
+def _create_compiled_keras_model():
   """Create compiled keras model."""
   model = models.create_original_fedavg_cnn_model(
       only_digits=FLAGS.digit_only_emnist)
@@ -385,7 +305,7 @@ def create_compiled_keras_model():
   return model
 
 
-def run_experiment():
+def _run_experiment():
   """Data preprocessing and experiment execution."""
   emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data(
       only_digits=FLAGS.digit_only_emnist)
@@ -418,7 +338,7 @@ def run_experiment():
                                        next(iter(example_dataset)))
 
   def model_fn():
-    keras_model = create_compiled_keras_model()
+    keras_model = _create_compiled_keras_model()
     return tff.learning.from_compiled_keras_model(keras_model, sample_batch)
 
   def client_datasets_fn(round_num):
@@ -431,22 +351,31 @@ def run_experiment():
         for client in sampled_clients
     ]
 
+  def evaluate_fn(state):
+    keras_model = _create_compiled_keras_model()
+    tff.learning.assign_weights_to_keras_model(keras_model, state.model)
+    eval_metrics = keras_model.evaluate(emnist_test, verbose=0)
+    return {
+        'loss': eval_metrics[0],
+        'sparse_categorical_accuracy': eval_metrics[1],
+    }
+
   tf.io.gfile.makedirs(FLAGS.root_output_dir)
   hparam_dict = collections.OrderedDict([
       (name, FLAGS[name].value) for name in hparam_flags
   ])
   hparam_dict = utils_impl.remove_unused_flags('client', hparam_dict)
 
-  metrics_hook = MetricsHook.build(FLAGS.exp_name, FLAGS.root_output_dir,
-                                   emnist_test, hparam_dict)
+  metrics_hook = _MetricsHook(FLAGS.exp_name, FLAGS.root_output_dir,
+                              hparam_dict)
 
   if FLAGS.server_optimizer == 'sgd':
-    optimizer_fn = functools.partial(
+    server_optimizer_fn = functools.partial(
         tf.keras.optimizers.SGD,
         learning_rate=FLAGS.server_learning_rate,
         momentum=FLAGS.server_momentum)
   elif FLAGS.server_optimizer == 'flars':
-    optimizer_fn = functools.partial(
+    server_optimizer_fn = functools.partial(
         flars_optimizer.FLARSOptimizer,
         learning_rate=FLAGS.server_learning_rate,
         momentum=FLAGS.server_momentum,
@@ -454,10 +383,11 @@ def run_experiment():
   else:
     raise ValueError('Optimizer %s is not supported.' % FLAGS.server_optimizer)
 
-  federated_averaging_training_loop(
-      model_fn,
-      optimizer_fn,
-      client_datasets_fn,
+  _federated_averaging_training_loop(
+      model_fn=model_fn,
+      server_optimizer_fn=server_optimizer_fn,
+      client_datasets_fn=client_datasets_fn,
+      evaluate_fn=evaluate_fn,
       total_rounds=FLAGS.total_rounds,
       rounds_per_eval=FLAGS.rounds_per_eval,
       metrics_hook=metrics_hook)
@@ -472,7 +402,7 @@ def main(argv):
     tf.io.gfile.makedirs(os.path.join(FLAGS.root_output_dir, FLAGS.exp_name))
   except tf.errors.OpError:
     pass
-  run_experiment()
+  _run_experiment()
 
 
 if __name__ == '__main__':
