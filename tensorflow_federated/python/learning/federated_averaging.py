060c327ce41d06cb6a39c999996dd2fdf1283b00
Zachary Garrett <zachgarrett@google.com>
2020-02-05 09:12:02 -0800

    Add a `client_optimizer_fn` parameter to `tff.learning.build_federated_averaging_process` and allow it to be used with `tff.learning.Model` arguments.
    This is a small step into moving towards (only) using custom client training loops, rather than relying on Keras. This will also allow us to introspect Keras less, and give TFF libraries and users more flexibility and composibility.
    
    We purposing duplicate a lot of existing code and modify it slightly so that (1) existing code remains largely unchanged, (2) the clean-up pass later is strictly a clean code deletion.
    
    PiperOrigin-RevId: 293381647

--- tensorflow_federated/python/learning/federated_averaging.py
+++ tensorflow_federated/python/learning/federated_averaging.py
@@ -22,24 +22,112 @@ Communication-Efficient Learning of Deep Networks from Decentralized Data
     https://arxiv.org/abs/1602.05629
 """
 
+import collections
+from typing import Any, Callable, Optional
+import warnings
+
 import tensorflow as tf
 
 from tensorflow_federated.python import core as tff
 from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.learning import model as model_lib
 from tensorflow_federated.python.learning import model_utils
 from tensorflow_federated.python.learning.framework import optimizer_utils
 from tensorflow_federated.python.tensorflow_libs import tensor_utils
 
 
-class ClientFedAvg(optimizer_utils.ClientDeltaFn):
+class _ClientFedAvg(optimizer_utils.ClientDeltaFn):
   """Client TensorFlow logic for Federated Averaging."""
 
-  def __init__(self, model, client_weight_fn=None):
+  def __init__(self,
+               model: model_lib.Model,
+               optimizer: tf.keras.optimizers.Optimizer,
+               client_weight_fn: Optional[Callable[[Any], tf.Tensor]] = None):
     """Creates the client computation for Federated Averaging.
 
     Args:
-      model: A `tff.learning.TrainableModel`.
-      client_weight_fn: Optional function that takes the output of
+      model: A `tff.learning.Model` instance.
+      optimizer: A `tf.keras.Optimizer` instance.
+      client_weight_fn: an optional callable that takes the output of
+        `model.report_local_outputs` and returns a tensor that provides the
+        weight in the federated average of model deltas. If not provided, the
+        default is the total number of examples processed on device.
+    """
+    self._model = model_utils.enhance(model)
+    self._optimizer = optimizer
+    py_typecheck.check_type(self._model, model_utils.EnhancedModel)
+
+    if client_weight_fn is not None:
+      py_typecheck.check_callable(client_weight_fn)
+      self._client_weight_fn = client_weight_fn
+    else:
+      self._client_weight_fn = None
+
+  @property
+  def variables(self):
+    return []
+
+  @tf.function
+  def __call__(self, dataset, initial_weights):
+    model = self._model
+    optimizer = self._optimizer
+    tf.nest.map_structure(lambda a, b: a.assign(b), model.weights,
+                          initial_weights)
+
+    @tf.function
+    def reduce_fn(num_examples_sum, batch):
+      """Runs `tff.learning.Model.train_on_batch` on local client batch."""
+      with tf.GradientTape() as tape:
+        output = model.forward_pass(batch, training=True)
+
+      gradients = tape.gradient(output.loss, model.weights.trainable)
+      optimizer.apply_gradients(zip(gradients, model.weights.trainable))
+
+      if output.num_examples is None:
+        return num_examples_sum + tf.shape(output.predictions)[0]
+      else:
+        return num_examples_sum + output.num_examples
+
+    num_examples_sum = dataset.reduce(
+        initial_state=tf.constant(0), reduce_func=reduce_fn)
+
+    weights_delta = tf.nest.map_structure(tf.subtract, model.weights.trainable,
+                                          initial_weights.trainable)
+    aggregated_outputs = model.report_local_outputs()
+
+    # TODO(b/122071074): Consider moving this functionality into
+    # tff.federated_mean?
+    weights_delta, has_non_finite_delta = (
+        tensor_utils.zero_all_if_any_non_finite(weights_delta))
+    # Zero out the weight if there are any non-finite values.
+    if has_non_finite_delta > 0:
+      weights_delta_weight = tf.constant(0.0)
+    elif self._client_weight_fn is None:
+      weights_delta_weight = tf.cast(num_examples_sum, tf.float32)
+    else:
+      weights_delta_weight = self._client_weight_fn(aggregated_outputs)
+
+    return optimizer_utils.ClientOutput(
+        weights_delta, weights_delta_weight, aggregated_outputs,
+        collections.OrderedDict(
+            num_examples=num_examples_sum,
+            has_non_finite_delta=has_non_finite_delta,
+        ))
+
+
+# TODO(b/148576550): remove as part of tff.learning.TrainableModel deprecation.
+# Remove after all users have migrated.
+class _DeprecatedClientFedAvg(optimizer_utils.ClientDeltaFn):
+  """Client TensorFlow logic for Federated Averaging using a TrainableModel."""
+
+  def __init__(self,
+               model: model_lib.TrainableModel,
+               client_weight_fn: Optional[Callable[[Any], tf.Tensor]] = None):
+    """Creates the client computation for Federated Averaging.
+
+    Args:
+      model: A `tff.learning.TrainableModel` instance.
+      client_weight_fn: an optional callable that takes the output of
         `model.report_local_outputs` and returns a tensor that provides the
         weight in the federated average of model deltas. If not provided, the
         default is the total number of examples processed on device.
@@ -106,21 +194,29 @@ class ClientFedAvg(optimizer_utils.ClientDeltaFn):
         }))
 
 
+DEFAULT_SERVER_OPTIMIZER_FN = lambda: tf.keras.optimizers.SGD(learning_rate=1.0)
+
+
 def build_federated_averaging_process(
-    model_fn,
-    server_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=1.0),
-    client_weight_fn=None,
+    model_fn: Callable[[], model_lib.Model],
+    client_optimizer_fn: Optional[Callable[
+        [], tf.keras.optimizers.Optimizer]] = None,
+    server_optimizer_fn: Callable[
+        [], tf.keras.optimizers.Optimizer] = DEFAULT_SERVER_OPTIMIZER_FN,
+    client_weight_fn: Callable[[Any], tf.Tensor] = None,
     stateful_delta_aggregate_fn=None,
-    stateful_model_broadcast_fn=None):
+    stateful_model_broadcast_fn=None) -> tff.utils.IterativeProcess:
   """Builds the TFF computations for optimization using federated averaging.
 
   Args:
-    model_fn: A no-arg function that returns a `tff.learning.TrainableModel`.
-    server_optimizer_fn: A no-arg function that returns a `tf.Optimizer`. The
-      `apply_gradients` method of this optimizer is used to apply client updates
-      to the server model. The default creates a `tf.keras.optimizers.SGD` with
-      a learning rate of 1.0, which simply adds the average client delta to the
-      server's model.
+    model_fn: A no-arg function that returns a `tff.learning.Model`.
+    client_optimizer_fn: An optional no-arg callable that returns a
+      `tf.keras.Optimizer`
+    server_optimizer_fn: A no-arg callable that returns a `tf.keras.Optimizer`.
+      The `apply_gradients` method of this optimizer is used to apply client
+      updates to the server model. The default creates a
+      `tf.keras.optimizers.SGD` with a learning rate of 1.0, which simply adds
+      the average client delta to the server's model.
     client_weight_fn: Optional function that takes the output of
       `model.report_local_outputs` and returns a tensor that provides the weight
       in the federated average of model deltas. If not provided, the default is
@@ -142,9 +238,30 @@ def build_federated_averaging_process(
   Returns:
     A `tff.utils.IterativeProcess`.
   """
+  if client_optimizer_fn is None:
+    warnings.warn('tff.learning.build_federated_averaging_process will start '
+                  'requiring a new argument \'client_optimizer_fn\'. Specify '
+                  'the local client optimizer here rather than building a '
+                  'ttf.learning.TrainableModel')
+  else:
+    # Validate parameters and surfacing errors early requires building a
+    # throwaway model here.
+    with tf.Graph().as_default():
+      model = model_fn()
+      if isinstance(model, model_lib.TrainableModel):
+        raise TypeError('model_fn parameter should be a callable that produces '
+                        'tff.learning.Model, not the deprecated '
+                        'tff.learning.TrainableModel')
 
   def client_fed_avg(model_fn):
-    return ClientFedAvg(model_fn(), client_weight_fn)
+    if client_optimizer_fn is None:
+      return _DeprecatedClientFedAvg(model_fn(), client_weight_fn)
+    elif callable(client_optimizer_fn):
+      return _ClientFedAvg(model_fn(), client_optimizer_fn(), client_weight_fn)
+    else:
+      raise TypeError(f'client_optimizer_fn parameter of '
+                      'tff.learning.build_federated_averaging_process must be '
+                      'a callable. Received a {type(client_optimizer_fn)}')
 
   if stateful_delta_aggregate_fn is None:
     stateful_delta_aggregate_fn = optimizer_utils.build_stateless_mean()
--- tensorflow_federated/python/learning/federated_averaging_test.py
+++ tensorflow_federated/python/learning/federated_averaging_test.py
@@ -26,10 +26,13 @@ from tensorflow_federated.python.learning import model_examples
 from tensorflow_federated.python.learning import model_utils
 
 
-class FederatedAveragingClientTest(test.TestCase, parameterized.TestCase):
-  """Tests of ClientFedAvg that use a common model and data."""
+# TODO(b/148576550): remove as part of tff.learning.TrainableModel deprecation.
+# Remove after all users have migrated.
+class FederatedAveragingClientWithTrainableModelTest(test.TestCase,
+                                                     parameterized.TestCase):
+  """Tests of _DeprecatedClientFedAvg that use a common model and data."""
 
-  def dataset(self):
+  def create_dataset(self):
     # Create a dataset with 4 examples:
     dataset = tf.data.Dataset.from_tensor_slices(
         model_examples.TrainableLinearRegression.make_batch(
@@ -41,7 +44,7 @@ class FederatedAveragingClientTest(test.TestCase, parameterized.TestCase):
     # as it adds the batch dimension which is expected by the model.
     return dataset.repeat(2).batch(3)
 
-  def model(self):
+  def create_model(self):
     return model_examples.TrainableLinearRegression(feature_dim=2)
 
   def initial_weights(self):
@@ -51,9 +54,9 @@ class FederatedAveragingClientTest(test.TestCase, parameterized.TestCase):
     )
 
   def test_client_tf(self):
-    model = self.model()
-    dataset = self.dataset()
-    client_tf = federated_averaging.ClientFedAvg(model)
+    model = self.create_model()
+    dataset = self.create_dataset()
+    client_tf = federated_averaging._DeprecatedClientFedAvg(model)
     client_outputs = self.evaluate(client_tf(dataset, self.initial_weights()))
 
     # Both trainable parameters should have been updated,
@@ -74,18 +77,18 @@ class FederatedAveragingClientTest(test.TestCase, parameterized.TestCase):
                        np.finfo(np.float32).eps, 10.0)
 
   def test_client_tf_custom_delta_weight(self):
-    model = self.model()
-    dataset = self.dataset()
-    client_tf = federated_averaging.ClientFedAvg(
+    model = self.create_model()
+    dataset = self.create_dataset()
+    client_tf = federated_averaging._DeprecatedClientFedAvg(
         model, client_weight_fn=lambda _: tf.constant(1.5))
     client_outputs = client_tf(dataset, self.initial_weights())
     self.assertEqual(self.evaluate(client_outputs.weights_delta_weight), 1.5)
 
   @parameterized.named_parameters(('_inf', np.inf), ('_nan', np.nan))
   def test_non_finite_aggregation(self, bad_value):
-    model = self.model()
-    dataset = self.dataset()
-    client_tf = federated_averaging.ClientFedAvg(model)
+    model = self.create_model()
+    dataset = self.create_dataset()
+    client_tf = federated_averaging._DeprecatedClientFedAvg(model)
     init_weights = self.initial_weights()
     init_weights.trainable[1] = bad_value
     client_outputs = client_tf(dataset, init_weights)
@@ -97,7 +100,86 @@ class FederatedAveragingClientTest(test.TestCase, parameterized.TestCase):
         1)
 
 
-class FederatedAveragingTffTest(test.TestCase, parameterized.TestCase):
+class FederatedAveragingClientWithModelTest(test.TestCase,
+                                            parameterized.TestCase):
+  """Tests of _ClientFedAvg that use a common model and data."""
+
+  def create_dataset(self):
+    # Create a dataset with 4 examples:
+    dataset = tf.data.Dataset.from_tensor_slices(
+        model_examples.TrainableLinearRegression.make_batch(
+            x=[[0.0, 0.0], [1.0, 0.0], [2.0, 0.0], [3.0, 0.0]],
+            y=[[0.0], [0.0], [1.0], [1.0]]))
+    # Repeat the dataset 2 times with batches of 3 examples,
+    # producing 3 minibatches (the last one with only 2 examples).
+    # Note that `batch` is required for this dataset to be useable,
+    # as it adds the batch dimension which is expected by the model.
+    return dataset.repeat(2).batch(3)
+
+  def create_model(self):
+    return model_examples.LinearRegression(feature_dim=2)
+
+  def initial_weights(self):
+    return model_utils.ModelWeights(
+        trainable=[tf.zeros((2, 1)), tf.constant(0.0)],
+        non_trainable=[0.0],
+    )
+
+  def test_client_tf(self):
+    model = self.create_model()
+    dataset = self.create_dataset()
+    client_tf = federated_averaging._ClientFedAvg(
+        model, tf.keras.optimizers.SGD(learning_rate=0.1))
+    client_outputs = self.evaluate(client_tf(dataset, self.initial_weights()))
+
+    # Both trainable parameters should have been updated,
+    # and we don't return the non-trainable variable.
+    self.assertAllGreater(
+        np.linalg.norm(client_outputs.weights_delta, axis=-1), 0.1)
+    self.assertEqual(client_outputs.weights_delta_weight, 8.0)
+    self.assertEqual(client_outputs.optimizer_output['num_examples'], 8)
+    self.assertEqual(client_outputs.optimizer_output['has_non_finite_delta'], 0)
+
+    self.assertDictContainsSubset(
+        {
+            'num_examples': 8,
+            'num_examples_float': 8.0,
+            'num_batches': 3,
+        }, client_outputs.model_output)
+    self.assertBetween(client_outputs.model_output['loss'],
+                       np.finfo(np.float32).eps, 10.0)
+
+  def test_client_tf_custom_delta_weight(self):
+    model = self.create_model()
+    dataset = self.create_dataset()
+    client_tf = federated_averaging._ClientFedAvg(
+        model,
+        tf.keras.optimizers.SGD(learning_rate=0.1),
+        client_weight_fn=lambda _: tf.constant(1.5))
+    client_outputs = client_tf(dataset, self.initial_weights())
+    self.assertEqual(self.evaluate(client_outputs.weights_delta_weight), 1.5)
+
+  @parameterized.named_parameters(('_inf', np.inf), ('_nan', np.nan))
+  def test_non_finite_aggregation(self, bad_value):
+    model = self.create_model()
+    dataset = self.create_dataset()
+    client_tf = federated_averaging._ClientFedAvg(
+        model, tf.keras.optimizers.SGD(learning_rate=0.1))
+    init_weights = self.initial_weights()
+    init_weights.trainable[1] = bad_value
+    client_outputs = client_tf(dataset, init_weights)
+    self.assertEqual(self.evaluate(client_outputs.weights_delta_weight), 0.0)
+    self.assertAllClose(
+        self.evaluate(client_outputs.weights_delta), [[[0.0], [0.0]], 0.0])
+    self.assertEqual(
+        self.evaluate(client_outputs.optimizer_output['has_non_finite_delta']),
+        1)
+
+
+# TODO(b/148576550): remove as part of tff.learning.TrainableModel deprecation.
+# Remove after all users have migrated.
+class FederatedAveragingTrainableModelTffTest(test.TestCase,
+                                              parameterized.TestCase):
 
   def test_orchestration_execute(self):
     iterative_process = federated_averaging.build_federated_averaging_process(
@@ -196,6 +278,129 @@ class FederatedAveragingTffTest(test.TestCase, parameterized.TestCase):
   def test_execute_empty_data(self):
     iterative_process = federated_averaging.build_federated_averaging_process(
         model_fn=model_examples.TrainableLinearRegression)
+
+    # Results in empty dataset with correct types and shapes.
+    ds = tf.data.Dataset.from_tensor_slices(
+        collections.OrderedDict([
+            ('x', [[1.0, 2.0]]),
+            ('y', [[5.0]]),
+        ])).batch(
+            5, drop_remainder=True)
+    federated_ds = [ds] * 2
+
+    server_state = iterative_process.initialize()
+
+    first_state, metric_outputs = iterative_process.next(
+        server_state, federated_ds)
+    self.assertAllClose(
+        list(first_state.model.trainable), [[[0.0], [0.0]], 0.0])
+    self.assertEqual(metric_outputs.num_examples, 0)
+    self.assertTrue(tf.math.is_nan(metric_outputs.loss))
+
+
+class FederatedAveragingModelTffTest(test.TestCase, parameterized.TestCase):
+
+  def test_orchestration_execute(self):
+    iterative_process = federated_averaging.build_federated_averaging_process(
+        model_fn=model_examples.LinearRegression,
+        client_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.1))
+
+    ds = tf.data.Dataset.from_tensor_slices(
+        collections.OrderedDict([
+            ('x', [[1.0, 2.0], [3.0, 4.0]]),
+            ('y', [[5.0], [6.0]]),
+        ])).batch(2)
+    federated_ds = [ds] * 3
+
+    server_state = iterative_process.initialize()
+
+    prev_loss = np.inf
+    for _ in range(3):
+      server_state, metric_outputs = iterative_process.next(
+          server_state, federated_ds)
+      self.assertEqual(metric_outputs.num_examples, 2 * len(federated_ds))
+      self.assertLess(metric_outputs.loss, prev_loss)
+      prev_loss = metric_outputs.loss
+
+  @parameterized.named_parameters([
+      ('functional_model',
+       model_examples.build_linear_regression_keras_functional_model),
+      ('sequential_model',
+       model_examples.build_linear_regression_keras_sequential_model),
+      ('subclass_model',
+       model_examples.build_linear_regression_keras_subclass_model),
+  ])
+  def test_orchestration_execute_from_keras(self, build_keras_model_fn):
+    dummy_batch = collections.OrderedDict([
+        ('x', np.zeros([1, 2], np.float32)),
+        ('y', np.zeros([1, 1], np.float32)),
+    ])
+
+    def model_fn():
+      keras_model = build_keras_model_fn(feature_dims=2)
+      return keras_utils.from_keras_model(
+          keras_model,
+          dummy_batch,
+          loss=tf.keras.losses.MeanSquaredError(),
+          metrics=[])
+
+    iterative_process = federated_averaging.build_federated_averaging_process(
+        model_fn=model_fn,
+        client_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.01))
+
+    ds = tf.data.Dataset.from_tensor_slices(
+        collections.OrderedDict([
+            ('x', [[1.0, 2.0], [3.0, 4.0]]),
+            ('y', [[5.0], [6.0]]),
+        ])).batch(2)
+    federated_ds = [ds] * 3
+
+    server_state = iterative_process.initialize()
+
+    prev_loss = np.inf
+    for _ in range(3):
+      server_state, metrics = iterative_process.next(server_state, federated_ds)
+      self.assertLess(metrics.loss, prev_loss)
+      prev_loss = metrics.loss
+
+  def test_orchestration_execute_from_keras_with_lookup(self):
+    self.skipTest('https://github.com/tensorflow/federated/issues/783')
+
+    def model_fn():
+      dummy_batch = collections.OrderedDict([
+          ('x', tf.constant([['R']], tf.string)),
+          ('y', tf.zeros([1, 1], tf.float32)),
+      ])
+      keras_model = model_examples.build_lookup_table_keras_model()
+      return keras_utils.from_compiled_keras_model(
+          keras_model,
+          dummy_batch,
+          loss=tf.keras.losses.MeanSquaredError(),
+          metrics=[])
+
+    iterative_process = federated_averaging.build_federated_averaging_process(
+        model_fn=model_fn,
+        client_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.1))
+
+    ds = tf.data.Dataset.from_tensor_slices(
+        collections.OrderedDict([
+            ('x', [['R'], ['G'], ['B']]),
+            ('y', [[1.0], [2.0], [3.0]]),
+        ])).batch(2)
+    federated_ds = [ds] * 3
+
+    server_state = iterative_process.initialize()
+
+    prev_loss = np.inf
+    for _ in range(3):
+      server_state, metrics = iterative_process.next(server_state, federated_ds)
+      self.assertLess(metrics.loss, prev_loss)
+      prev_loss = metrics.loss
+
+  def test_execute_empty_data(self):
+    iterative_process = federated_averaging.build_federated_averaging_process(
+        model_fn=model_examples.LinearRegression,
+        client_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.1))
 
     # Results in empty dataset with correct types and shapes.
     ds = tf.data.Dataset.from_tensor_slices(
