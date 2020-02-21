955a39f02d1aa3e47035bc49eb624a0035871e1c
Zachary Garrett <zachgarrett@google.com>
2020-02-21 13:50:58 -0800

    Automated rollback of commit 4e8de62a5cbff3e131b8f4e2947fc7e979e41e91
    PiperOrigin-RevId: 296504410

--- tensorflow_federated/python/learning/federated_averaging.py
+++ tensorflow_federated/python/learning/federated_averaging.py
@@ -24,6 +24,7 @@ Communication-Efficient Learning of Deep Networks from Decentralized Data
 
 import collections
 from typing import Any, Callable, Optional
+import warnings
 
 import tensorflow as tf
 
@@ -52,7 +53,6 @@ class _ClientFedAvg(optimizer_utils.ClientDeltaFn):
         weight in the federated average of model deltas. If not provided, the
         default is the total number of examples processed on device.
     """
-    py_typecheck.check_type(model, model_lib.Model)
     self._model = model_utils.enhance(model)
     self._optimizer = optimizer
     py_typecheck.check_type(self._model, model_utils.EnhancedModel)
@@ -115,6 +115,85 @@ class _ClientFedAvg(optimizer_utils.ClientDeltaFn):
         ))
 
 
+# TODO(b/148576550): remove as part of tff.learning.TrainableModel deprecation.
+# Remove after all users have migrated.
+class _DeprecatedClientFedAvg(optimizer_utils.ClientDeltaFn):
+  """Client TensorFlow logic for Federated Averaging using a TrainableModel."""
+
+  def __init__(self,
+               model: model_lib.TrainableModel,
+               client_weight_fn: Optional[Callable[[Any], tf.Tensor]] = None):
+    """Creates the client computation for Federated Averaging.
+
+    Args:
+      model: A `tff.learning.TrainableModel` instance.
+      client_weight_fn: an optional callable that takes the output of
+        `model.report_local_outputs` and returns a tensor that provides the
+        weight in the federated average of model deltas. If not provided, the
+        default is the total number of examples processed on device.
+    """
+    self._model = model_utils.enhance(model)
+    py_typecheck.check_type(self._model, model_utils.EnhancedTrainableModel)
+
+    if client_weight_fn is not None:
+      py_typecheck.check_callable(client_weight_fn)
+      self._client_weight_fn = client_weight_fn
+    else:
+      self._client_weight_fn = None
+
+  @property
+  def variables(self):
+    return []
+
+  @tf.function
+  def __call__(self, dataset, initial_weights):
+    # TODO(b/113112108): Remove this temporary workaround and restore check for
+    # `tf.data.Dataset` after subclassing the currently used custom data set
+    # representation from it.
+    if 'Dataset' not in str(type(dataset)):
+      raise TypeError('Expected a data set, found {}.'.format(
+          py_typecheck.type_string(type(dataset))))
+
+    model = self._model
+    tf.nest.map_structure(lambda a, b: a.assign(b), model.weights,
+                          initial_weights)
+
+    @tf.function
+    def reduce_fn(num_examples_sum, batch):
+      """Runs `tff.learning.Model.train_on_batch` on local client batch."""
+      output = model.train_on_batch(batch)
+      if output.num_examples is None:
+        return num_examples_sum + tf.shape(output.predictions)[0]
+      else:
+        return num_examples_sum + output.num_examples
+
+    num_examples_sum = dataset.reduce(
+        initial_state=tf.constant(0), reduce_func=reduce_fn)
+
+    weights_delta = tf.nest.map_structure(tf.subtract, model.weights.trainable,
+                                          initial_weights.trainable)
+    aggregated_outputs = model.report_local_outputs()
+
+    # TODO(b/122071074): Consider moving this functionality into
+    # tff.federated_mean?
+    weights_delta, has_non_finite_delta = (
+        tensor_utils.zero_all_if_any_non_finite(weights_delta))
+    if self._client_weight_fn is None:
+      weights_delta_weight = tf.cast(num_examples_sum, tf.float32)
+    else:
+      weights_delta_weight = self._client_weight_fn(aggregated_outputs)
+    # Zero out the weight if there are any non-finite values.
+    if has_non_finite_delta > 0:
+      weights_delta_weight = tf.constant(0.0)
+
+    return optimizer_utils.ClientOutput(
+        weights_delta, weights_delta_weight, aggregated_outputs,
+        tensor_utils.to_odict({
+            'num_examples': num_examples_sum,
+            'has_non_finite_delta': has_non_finite_delta,
+        }))
+
+
 DEFAULT_SERVER_OPTIMIZER_FN = lambda: tf.keras.optimizers.SGD(learning_rate=1.0)
 
 
@@ -159,9 +238,30 @@ def build_federated_averaging_process(
   Returns:
     A `tff.utils.IterativeProcess`.
   """
+  if client_optimizer_fn is None:
+    warnings.warn('tff.learning.build_federated_averaging_process will start '
+                  'requiring a new argument \'client_optimizer_fn\'. Specify '
+                  'the local client optimizer here rather than building a '
+                  'ttf.learning.TrainableModel')
+  else:
+    # Validate parameters and surfacing errors early requires building a
+    # throwaway model here.
+    with tf.Graph().as_default():
+      model = model_fn()
+      if isinstance(model, model_lib.TrainableModel):
+        raise TypeError('model_fn parameter should be a callable that produces '
+                        'tff.learning.Model, not the deprecated '
+                        'tff.learning.TrainableModel')
 
   def client_fed_avg(model_fn):
-    return _ClientFedAvg(model_fn(), client_optimizer_fn(), client_weight_fn)
+    if client_optimizer_fn is None:
+      return _DeprecatedClientFedAvg(model_fn(), client_weight_fn)
+    elif callable(client_optimizer_fn):
+      return _ClientFedAvg(model_fn(), client_optimizer_fn(), client_weight_fn)
+    else:
+      raise TypeError(f'client_optimizer_fn parameter of '
+                      'tff.learning.build_federated_averaging_process must be '
+                      'a callable. Received a {type(client_optimizer_fn)}')
 
   if stateful_delta_aggregate_fn is None:
     stateful_delta_aggregate_fn = optimizer_utils.build_stateless_mean()
