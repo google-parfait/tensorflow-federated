4bf48d0951265381bc589c6f7d55208ed4c668de
Michael Reneer <michaelreneer@google.com>
2020-04-29 16:39:32 -0700

    Move `tf.data.Dataset` end-to-end tests to the package dedicated to these sorts of tests.
    PiperOrigin-RevId: 309122979

--- tensorflow_federated/python/tests/dataset_test.py
+++ tensorflow_federated/python/tests/dataset_test.py
@@ -16,31 +16,24 @@
 import collections
 
 import tensorflow as tf
+import tensorflow_federated as tff
 
 from tensorflow_federated.python.common_libs import test
-from tensorflow_federated.python.core.impl import computation_impl
-from tensorflow_federated.python.core.impl import tensorflow_serialization
-from tensorflow_federated.python.core.impl.context_stack import context_stack_impl
-from tensorflow_federated.python.core.impl.executors import default_executor
 
 tf.compat.v1.enable_v2_behavior()
 
 
-class IntegrationTest(test.TestCase):
+class DatasetTest(test.TestCase):
 
   # TODO(b/137602785): bring GPU test back after the fix for `wrap_function`.
   @test.skip_test_for_gpu
   def test_fetch_value_with_nested_datasets(self):
 
+    @tff.tf_computation
     def return_two_datasets():
       return [tf.data.Dataset.range(5), tf.data.Dataset.range(5)]
 
-    executable_return_two_datasets = computation_impl.ComputationImpl(
-        tensorflow_serialization.serialize_py_fn_as_tf_computation(
-            return_two_datasets, None, context_stack_impl.context_stack)[0],
-        context_stack_impl.context_stack)
-
-    x = executable_return_two_datasets()
+    x = return_two_datasets()
     self.assertEqual([i for i in iter(x[0])], list(range(5)))
     self.assertEqual([i for i in iter(x[1])], list(range(5)))
 
@@ -48,16 +41,11 @@ class IntegrationTest(test.TestCase):
   @test.skip_test_for_gpu
   def test_fetch_value_with_dataset_and_tensor(self):
 
+    @tff.tf_computation
     def return_dataset_and_tensor():
       return [tf.constant(0), tf.data.Dataset.range(5), tf.constant(5)]
 
-    executable_return_dataset_and_tensor = computation_impl.ComputationImpl(
-        tensorflow_serialization.serialize_py_fn_as_tf_computation(
-            return_dataset_and_tensor, None,
-            context_stack_impl.context_stack)[0],
-        context_stack_impl.context_stack)
-
-    x = executable_return_dataset_and_tensor()
+    x = return_dataset_and_tensor()
     self.assertEqual(x[0], 0)
     self.assertEqual([x for x in iter(x[1])], list(range(5)))
     self.assertEqual(x[2], 5)
@@ -66,18 +54,14 @@ class IntegrationTest(test.TestCase):
   @test.skip_test_for_gpu
   def test_fetch_value_with_datasets_nested_at_second_level(self):
 
+    @tff.tf_computation
     def return_two_datasets():
       return [
           tf.constant(0), [tf.data.Dataset.range(5),
                            tf.data.Dataset.range(5)]
       ]
 
-    executable_return_two_datasets = computation_impl.ComputationImpl(
-        tensorflow_serialization.serialize_py_fn_as_tf_computation(
-            return_two_datasets, None, context_stack_impl.context_stack)[0],
-        context_stack_impl.context_stack)
-
-    x = executable_return_two_datasets()
+    x = return_two_datasets()
     self.assertEqual(x[0], 0)
     self.assertEqual([i for i in iter(x[1][0])], list(range(5)))
     self.assertEqual([i for i in iter(x[1][1])], list(range(5)))
@@ -86,16 +70,12 @@ class IntegrationTest(test.TestCase):
   @test.skip_test_for_gpu
   def test_fetch_value_with_empty_dataset_and_tensors(self):
 
+    @tff.tf_computation
     def return_dataset():
       ds1 = tf.data.Dataset.from_tensor_slices([[1, 1], [1, 1]])
       return [tf.constant([0., 0.]), ds1.batch(5).take(0)]
 
-    executable_return_dataset = computation_impl.ComputationImpl(
-        tensorflow_serialization.serialize_py_fn_as_tf_computation(
-            return_dataset, None, context_stack_impl.context_stack)[0],
-        context_stack_impl.context_stack)
-
-    x = executable_return_dataset()
+    x = return_dataset()
     self.assertAllEqual(x[0], [0., 0.])
     self.assertEqual(x[1].element_spec,
                      tf.TensorSpec(shape=(None, 2), dtype=tf.int32))
@@ -106,17 +86,13 @@ class IntegrationTest(test.TestCase):
   @test.skip_test_for_gpu
   def test_fetch_value_with_empty_structured_dataset_and_tensors(self):
 
+    @tff.tf_computation
     def return_dataset():
       ds1 = tf.data.Dataset.from_tensor_slices(
           collections.OrderedDict([('a', [1, 1]), ('b', [1, 1])]))
       return [tf.constant([0., 0.]), ds1.batch(5).take(0)]
 
-    executable_return_dataset = computation_impl.ComputationImpl(
-        tensorflow_serialization.serialize_py_fn_as_tf_computation(
-            return_dataset, None, context_stack_impl.context_stack)[0],
-        context_stack_impl.context_stack)
-
-    x = executable_return_dataset()
+    x = return_dataset()
     self.assertAllEqual(x[0], [0., 0.])
     self.assertEqual(
         x[1].element_spec,
@@ -129,5 +105,5 @@ class IntegrationTest(test.TestCase):
 
 
 if __name__ == '__main__':
-  default_executor.initialize_default_executor()
+  # default_executor.initialize_default_executor()
   test.main()
