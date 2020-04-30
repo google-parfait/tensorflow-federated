faf878479411b024457b260845908bfc3dff1d78
jvmancuso <jason@manc.us>
2020-04-30 19:06:03 -0400

    remove dependencies on tff core

--- tensorflow_federated/python/core/utils/execution_tracing.py
+++ tensorflow_federated/python/core/utils/execution_tracing.py
@@ -7,24 +7,6 @@ import logging
 import re
 from typing import Any, Dict, Optional, Tuple
 
-from tensorflow.python.framework.ops import EagerTensor as tf_EagerTensor
-
-from tensorflow_federated.proto.v0 import computation_pb2
-from tensorflow_federated.python.common_libs import anonymous_tuple
-from tensorflow_federated.python.common_libs import tracing
-from tensorflow_federated.python.core.api import computation_types
-from tensorflow_federated.python.core.impl import computation_impl
-from tensorflow_federated.python.core.impl.compiler import intrinsic_defs
-from tensorflow_federated.python.core.impl.executors import caching_executor
-from tensorflow_federated.python.core.impl.executors import composing_executor
-from tensorflow_federated.python.core.impl.executors import eager_tf_executor
-from tensorflow_federated.python.core.impl.executors import federating_executor
-from tensorflow_federated.python.core.impl.executors import reference_resolving_executor
-from tensorflow_federated.python.core.impl.executors import remote_executor
-from tensorflow_federated.python.core.impl.executors import sizing_executor
-from tensorflow_federated.python.core.impl.executors import thread_delegating_executor
-from tensorflow_federated.python.core.impl.executors import transforming_executor
-
 
 class TraceFilterMode(Enum):
   VALUES = 0
@@ -139,18 +121,18 @@ class ExecutionTracingProvider(tracing.TracingProvider):
         p=prefix_str, m=main_str, sa=suffix_args, sk=suffix_kwargs)
 
     logging.debug(final)
-    
 
-  def _prefix(self, ctx, type_):
+  def _prefix(self, ctx, type_string):
     indentation = '  ' * self._current_call_level
-    template = "{indentation}{type} {hash}"
+    template = '{indentation}{type} {hash}'
 
     if ctx is None:
-      return template.format(indentation=indentation, type=type_, hash="NO_ID")
-    return template.format(indentation=indentation, type=type_, hash=hash(ctx))
+      return template.format(indentation=indentation, type=type_string, hash='NO_ID')
+    return template.format(indentation=indentation, type=type_string, hash=hash(ctx))
 
   def _format_object(self, x):
-    strategy = self._formatting_strategies.get(type(x), self._default_strategy)
+    strategy = self._formatting_strategies.get(
+        type(x).__name__, self._default_strategy)
     return strategy(x)
 
   def _format_anon_tuple(self, tup):
@@ -158,11 +140,11 @@ class ExecutionTracingProvider(tracing.TracingProvider):
     values = tup._element_array
     fmt_strs = []
     for n, v in zip(names, values):
-      fmt_strs.append("{}: {}".format(n, self._format_object(v)))
-    return "({})".format(", ".join(fmt_strs))
+      fmt_strs.append('{}: {}'.format(n, self._format_object(v)))
+    return '({})'.format(', '.join(fmt_strs))
 
-  def _register_formatting_strategy(self, type_, formatting_strategy):
-    self._formatting_strategies[type_] = formatting_strategy
+  def _register_formatting_strategy(self, type_string, formatting_strategy):
+    self._formatting_strategies[type_string] = formatting_strategy
 
   # Register all types here
   def _make_formatting_strategies(self):
@@ -173,84 +155,78 @@ class ExecutionTracingProvider(tracing.TracingProvider):
     self.register_primitives()
 
   def register_executors(self):
-    register = self._register_formatting_strategy
-
-    for type_ in [
-        caching_executor.CachingExecutor,
-        composing_executor.ComposingExecutor,
-        eager_tf_executor.EagerTFExecutor,
-        federating_executor.FederatingExecutor,
-        reference_resolving_executor.ReferenceResolvingExecutor,
-        remote_executor.RemoteExecutor,
-        sizing_executor.SizingExecutor,
-        thread_delegating_executor.ThreadDelegatingExecutor,
-        transforming_executor.TransformingExecutor,
+    for type_string in [
+        'CachingExecutor',
+        'ComposingExecutor',
+        'EagerTFExecutor',
+        'FederatingExecutor',
+        'ReferenceResolvingExecutor',
+        'RemoteExecutor',
+        'SizingExecutor',
+        'ThreadDelegatingExecutor',
+        'TransformingExecutor',
     ]:
-      register(type_, lambda x: "<{} @{}>".format(type_.__name__, id(x)))
+      self._register_formatting_strategy(
+          type_string, lambda x: '<{} @{}>'.format(type_string, id(x)))
 
   def register_executor_values(self):
-    register = self._register_formatting_strategy
-
-    for type_ in [
-        caching_executor.CachedValue,
-        composing_executor.CompositeValue,
-        eager_tf_executor.EagerValue,
-        federating_executor.FederatingExecutorValue,
-        reference_resolving_executor.ReferenceResolvingExecutorValue,
-        remote_executor.RemoteValue,
+    for type_string in [
+        'CachedValue',
+        'CompositeValue',
+        'EagerValue',
+        'FederatingExecutorValue',
+        'ReferenceResolvingExecutorValue',
+        'RemoteValue',
     ]:
-      register(
-          type_, lambda x: "<{} @{} : {}>".format(type_.__name__, id(x),
+      self._register_formatting_strategy(
+          type_string, lambda x: '<{} @{} : {}>'.format(type_string, id(x),
                                                   str(x.type_signature)))
 
   def register_computation_types(self):
-    register = self._register_formatting_strategy
-
-    for type_ in [
-        computation_types.TensorType,
-        computation_types.FederatedType,
-        computation_types.FunctionType,
-        computation_types.SequenceType,
+    for type_string in [
+        'TensorType',
+        'FederatedType',
+        'FunctionType',
+        'SequenceType',
     ]:
-      register(type_, lambda x: "<{}>".format(x))
+      self._register_formatting_strategy(
+            type_string, lambda x: '<{}>'.format(x))
 
   def register_primitives(self):
-    register = self._register_formatting_strategy
-
-    register(float, lambda x: "<{} : float>".format(x))
-    register(int, lambda x: "<{} : int>".format(x))
-    register(type(None), lambda _: "-")
-    register(
+    self._register_formatting_strategy(
+        float, lambda x: '<{} : float>'.format(x))
+    self._register_formatting_strategy(int, lambda x: '<{} : int>'.format(x))
+    self._register_formatting_strategy(type(None), lambda _: '-')
+    self._register_formatting_strategy(
         list,
-        lambda x: "<" + ", ".join([self._format_object(xi) for xi in x]) + ">")
-    register(
+        lambda x: '<' + ', '.join([self._format_object(xi) for xi in x]) + '>')
+    self._register_formatting_strategy(
         tuple,
-        lambda x: "<" + ", ".join(tuple(self._format_object(xi)
-                                        for xi in x)) + ">")
+        lambda x: '<' + ', '.join(tuple(
+            self._format_object(xi) for xi in x)) + '>')
 
-    register(tf_EagerTensor,
-             lambda x: "<{} @{}>".format(tf_EagerTensor.__name__, id(x)))
+    self._register_formatting_strategy(
+        'EagerTensor',
+        lambda x: '<{} @{}>'.format('EagerTensor', id(x)))
 
-    register(
-        anonymous_tuple.AnonymousTuple,
-        lambda x: "<AnonymousTuple @{} : {}>".format(
+    self._register_formatting_strategy(
+        'AnonymousTuple',
+        lambda x: '<AnonymousTuple @{} : {}>'.format(
             id(x), self._format_anon_tuple(x)))
 
   def register_computations(self):
-    register = self._register_formatting_strategy
-
-    register(
-        computation_impl.ComputationImpl,
-        lambda x: "<ComputationImpl {} @{} : {}>".format(
+    self._register_formatting_strategy(
+        'ComputationImpl',
+        lambda x: '<ComputationImpl {} @{} : {}>'.format(
             x._computation_proto.WhichOneof('computation'), id(x), x.
             type_signature))
 
-    register(
-        computation_pb2.Computation,
-        lambda x: "<ComputationPb {} @{} : {}>".format(
+    self._register_formatting_strategy(
+        'ComputationPb',
+        lambda x: '<ComputationPb {} @{} : {}>'.format(
             x.WhichOneof('computation'), id(x), x.type.WhichOneof('type')))
 
-    register(
-        intrinsic_defs.IntrinsicDef,
-        lambda x: "<IntrinsicDef {} {} @{}>".format(x.uri, x.type_signature,
+    self._register_formatting_strategy(
+        'IntrinsicDef',
+        lambda x: '<IntrinsicDef {} {} @{}>'.format(x.uri, x.type_signature,
                                                     id(x)))
