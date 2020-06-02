a8538c62c7cf5cdbfd5940c9ba388b7791584b69
Keith Rush <krush@google.com>
2020-06-02 09:25:36 -0700

    Introduces bind_computation_to_reference in FederatedComputationContext.
    PiperOrigin-RevId: 314344758

--- tensorflow_federated/python/core/impl/federated_computation_context.py
+++ tensorflow_federated/python/core/impl/federated_computation_context.py
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """The implementation of a context to use in building federated computations."""
+from typing import List, Tuple
 
 from tensorflow_federated.python.common_libs import py_typecheck
 from tensorflow_federated.python.core.api import computation_types
 from tensorflow_federated.python.core.impl import value_impl
+from tensorflow_federated.python.core.impl.compiler import building_blocks
 from tensorflow_federated.python.core.impl.context_stack import context_base
 from tensorflow_federated.python.core.impl.context_stack import context_stack_base
 from tensorflow_federated.python.core.impl.types import type_analysis
@@ -56,6 +58,8 @@ class FederatedComputationContext(context_base.Context):
     self._context_stack = context_stack
     self._parent = parent
     self._name = name
+    self._symbol_bindings = []
+    self._next_symbol_val = 0
 
   @property
   def name(self):
@@ -64,6 +68,22 @@ class FederatedComputationContext(context_base.Context):
   @property
   def parent(self):
     return self._parent
+
+  def bind_computation_to_reference(
+      self, comp: building_blocks.ComputationBuildingBlock
+  ) -> building_blocks.Reference:
+    """Binds a computation to a symbol, returns a reference to this binding."""
+    name = 'fc_{name}_symbol_{val}'.format(
+        name=self._name, val=self._next_symbol_val)
+    self._next_symbol_val += 1
+    self._symbol_bindings.append((name, comp))
+    ref = building_blocks.Reference(name, comp.type_signature)
+    return ref
+
+  @property
+  def symbol_bindings(
+      self) -> List[Tuple[str, building_blocks.ComputationBuildingBlock]]:
+    return self._symbol_bindings
 
   def ingest(self, val, type_spec):
     val = value_impl.to_value(val, type_spec, self._context_stack)
