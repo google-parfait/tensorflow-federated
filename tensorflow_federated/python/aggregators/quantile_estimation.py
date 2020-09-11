46b86b5400bd06f1560f93364994ec44117d9c8f
Galen Andrew <galenandrew@google.com>
2020-09-11 11:37:06 -0700

    Adds quantile estimation aggregation process factory.
    PiperOrigin-RevId: 331189543

---
+++ tensorflow_federated/python/aggregators/quantile_estimation.py
@@ -0,0 +1,121 @@
+# Copyright 2020, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Iterative process for quantile estimation."""
+
+import tensorflow as tf
+import tensorflow_privacy as tfp
+
+from tensorflow_federated.python.aggregators import factory
+from tensorflow_federated.python.aggregators import sum_factory
+from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.core.api import computation_types
+from tensorflow_federated.python.core.api import computations
+from tensorflow_federated.python.core.api import intrinsics
+from tensorflow_federated.python.core.api import placements
+from tensorflow_federated.python.core.templates import iterative_process
+
+
+class PrivateQuantileEstimatorProcess(iterative_process.IterativeProcess):
+  """A `tff.templates.IterativeProcess for estimating private quantiles.
+
+  This iterative process uses a `tensorflow_privacy.QuantileEstimatorQuery` to
+  maintain an estimate of the target quantile that is updated after each round.
+  The `next` function has the following type signature:
+
+  (<{state_type}@SERVER,{float32}@CLIENTS> -> {state_type}@SERVER)
+
+  Given a `state` of type `state_type`, the most recent estimate of the target
+  quantile can be retrieved using `get_current_estimate(state)`.
+  """
+
+  def __init__(
+      self,
+      quantile_estimator_query: tfp.QuantileEstimatorQuery,
+      record_aggregation_factory: factory.AggregationProcessFactory = None):
+    """Initializes `PrivateQuantileEstimatorProcess`.
+
+    Args:
+      quantile_estimator_query: A `tfp.QuantileEstimatorQuery` for estimating
+        quantiles with differential privacy.
+      record_aggregation_factory: A `tff.aggregators.AggregationProcessFactory`
+        to aggregate counts of values below the current estimate. If `None`,
+        defaults to `tff.aggregators.SumFactory`.
+    """
+    py_typecheck.check_type(quantile_estimator_query,
+                            tfp.QuantileEstimatorQuery)
+    if record_aggregation_factory is None:
+      record_aggregation_factory = sum_factory.SumFactory()
+    else:
+      py_typecheck.check_type(record_aggregation_factory,
+                              factory.AggregationProcessFactory)
+
+    # 1. Define tf_computations.
+    initial_state_fn = computations.tf_computation(
+        quantile_estimator_query.initial_global_state)
+    quantile_state_type = initial_state_fn.type_signature.result
+    derive_sample_params = computations.tf_computation(
+        quantile_estimator_query.derive_sample_params, quantile_state_type)
+    get_quantile_record = computations.tf_computation(
+        quantile_estimator_query.preprocess_record,
+        derive_sample_params.type_signature.result, tf.float32)
+    quantile_record_type = get_quantile_record.type_signature.result
+    get_noised_result = computations.tf_computation(
+        quantile_estimator_query.get_noised_result, quantile_record_type,
+        quantile_state_type)
+    quantile_agg_process = record_aggregation_factory.create(
+        quantile_record_type)
+
+    # 2. Define federated_computations.
+    @computations.federated_computation()
+    def init_fn():
+      return intrinsics.federated_zip(
+          (intrinsics.federated_eval(initial_state_fn, placements.SERVER),
+           quantile_agg_process.initialize()))
+
+    @computations.federated_computation(init_fn.type_signature.result,
+                                        computation_types.FederatedType(
+                                            tf.float32, placements.CLIENTS))
+    def next_fn(state, value):
+      quantile_query_state, agg_state = state
+
+      params = intrinsics.federated_broadcast(
+          intrinsics.federated_map(derive_sample_params, quantile_query_state))
+      quantile_record = intrinsics.federated_map(get_quantile_record,
+                                                 (params, value))
+
+      (new_agg_state, agg_result,
+       agg_measurements) = quantile_agg_process.next(agg_state, quantile_record)
+
+      # We expect the quantile record aggregation process to be something simple
+      # like basic sum, so we won't surface its measurements.
+      del agg_measurements
+
+      _, new_quantile_query_state = intrinsics.federated_map(
+          get_noised_result, (agg_result, quantile_query_state))
+
+      return intrinsics.federated_zip((new_quantile_query_state, new_agg_state))
+
+    super().__init__(init_fn, next_fn)
+
+  @classmethod
+  def get_current_estimate(cls, state):
+    """Gets the most recent estimate of the given quantile from the state.
+
+    Args:
+      state: The state of a `PrivateQuantileEstimatorProcess`.
+
+    Returns:
+      The most recent estimate of the given quantile.
+    """
+    return state[0].current_estimate
---
+++ tensorflow_federated/python/aggregators/quantile_estimation_test.py
@@ -0,0 +1,129 @@
+# Copyright 2020, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for quantile estimation."""
+
+import collections
+
+from absl.testing import parameterized
+import numpy as np
+import tensorflow as tf
+import tensorflow_privacy as tfp
+
+from tensorflow_federated.python.aggregators import quantile_estimation
+from tensorflow_federated.python.common_libs import test
+from tensorflow_federated.python.core.api import computation_types
+from tensorflow_federated.python.core.api import placements
+from tensorflow_federated.python.core.backends.native import execution_contexts
+from tensorflow_federated.python.core.impl.types import type_conversions
+
+
+class PrivateQEComputationTest(test.TestCase, parameterized.TestCase):
+
+  @parameterized.named_parameters(('private', True), ('non_private', False))
+  def test_process_type_signature(self, private):
+    if private:
+      quantile_estimator_query = tfp.QuantileEstimatorQuery(
+          initial_estimate=1.0,
+          target_quantile=0.5,
+          learning_rate=1.0,
+          below_estimate_stddev=0.5,
+          expected_num_records=100,
+          geometric_update=True)
+    else:
+      quantile_estimator_query = tfp.NoPrivacyQuantileEstimatorQuery(
+          initial_estimate=1.0,
+          target_quantile=0.5,
+          learning_rate=1.0,
+          geometric_update=True)
+
+    process = quantile_estimation.PrivateQuantileEstimatorProcess(
+        quantile_estimator_query)
+
+    query_state = quantile_estimator_query.initial_global_state()
+    sum_process_state = ()
+
+    server_state_type = computation_types.FederatedType(
+        type_conversions.type_from_tensors((query_state, sum_process_state)),
+        placements.SERVER)
+
+    self.assertEqual(
+        computation_types.FunctionType(
+            parameter=None, result=server_state_type),
+        process.initialize.type_signature)
+
+    client_value_type = computation_types.FederatedType(tf.float32,
+                                                        placements.CLIENTS)
+    self.assertTrue(
+        process.next.type_signature.is_equivalent_to(
+            computation_types.FunctionType(
+                parameter=collections.OrderedDict(
+                    state=server_state_type, value=client_value_type),
+                result=server_state_type)))
+
+  def test_bad_query(self):
+    non_quantile_estimator_query = tfp.GaussianSumQuery(
+        l2_norm_clip=1.0, stddev=1.0)
+
+    with self.assertRaises(TypeError):
+      quantile_estimation.PrivateQuantileEstimatorProcess(
+          non_quantile_estimator_query)
+
+  def test_bad_aggregation_factory(self):
+    quantile_estimator_query = tfp.NoPrivacyQuantileEstimatorQuery(
+        initial_estimate=1.0,
+        target_quantile=0.5,
+        learning_rate=1.0,
+        geometric_update=True)
+
+    with self.assertRaises(TypeError):
+      quantile_estimation.PrivateQuantileEstimatorProcess(
+          quantile_estimator_query=quantile_estimator_query,
+          record_aggregation_factory="I'm not a record_aggregation_factory.")
+
+
+class PrivateQEExecutionTest(test.TestCase, parameterized.TestCase):
+
+  @parameterized.named_parameters(('arithmetic', False), ('geometric', True))
+  def test_adaptation(self, geometric_update):
+    initial_estimate = 3.14159
+    target_quantile = 0.61803
+    learning_rate = 2.71828
+
+    quantile_estimator_query = tfp.NoPrivacyQuantileEstimatorQuery(
+        initial_estimate=initial_estimate,
+        target_quantile=target_quantile,
+        learning_rate=learning_rate,
+        geometric_update=geometric_update)
+
+    process = quantile_estimation.PrivateQuantileEstimatorProcess(
+        quantile_estimator_query)
+
+    state = process.initialize()
+    self.assertAllClose(process.get_current_estimate(state), initial_estimate)
+
+    # Run on two records greater than estimate.
+    state = process.next(state, [initial_estimate + 1, initial_estimate + 2])
+
+    if geometric_update:
+      expected_estimate = (
+          initial_estimate * np.exp(learning_rate * target_quantile))
+    else:
+      expected_estimate = initial_estimate + learning_rate * target_quantile
+
+    self.assertAllClose(process.get_current_estimate(state), expected_estimate)
+
+
+if __name__ == '__main__':
+  execution_contexts.set_local_execution_context()
+  test.main()
