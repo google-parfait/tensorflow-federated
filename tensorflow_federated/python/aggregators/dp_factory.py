a553c7b60ed1416a8bae4c8e1dcc4da48f3f36e2
Galen Andrew <galenandrew@google.com>
2020-10-15 16:00:41 -0700

    Adds DifferentiallyPrivateFactory, an AggregationProcessFactory for DPQueries from tensorflow_privacy.
    PiperOrigin-RevId: 337400971

---
+++ tensorflow_federated/python/aggregators/dp_factory.py
@@ -0,0 +1,157 @@
+# Copyright 2020, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Factory for aggregations parameterized by tensorflow_privacy DPQueries."""
+
+import collections
+from typing import Optional
+import tensorflow_privacy as tfp
+
+from tensorflow_federated.python.aggregators import factory
+from tensorflow_federated.python.aggregators import sum_factory
+from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.core.api import computation_types
+from tensorflow_federated.python.core.api import computations
+from tensorflow_federated.python.core.api import intrinsics
+from tensorflow_federated.python.core.api import placements
+from tensorflow_federated.python.core.templates import aggregation_process
+from tensorflow_federated.python.core.templates import measured_process
+
+
+class DifferentiallyPrivateFactory(factory.AggregationProcessFactory):
+  """`AggregationProcessFactory` for tensorflow_privacy DPQueries.
+
+  The created `tff.templates.AggregationProcess` aggregates values placed at
+  `CLIENTS` according to the provided DPQuery, and outputs the result placed at
+  `SERVER`.
+
+  A DPQuery defines preprocessing to perform on each value, and postprocessing
+  to perform on the aggregated, preprocessed values. Provided the preprocessed
+  values ("records") are aggregated in a way that is consistent with the
+  DPQuery, formal (epsilon, delta) privacy guarantees can be derived. This
+  aggregation is controlled by `record_aggregation_factory`.
+
+  A simple summation (using the default `tff.templates.SumFactory`) is usually
+  acceptable. Aggregations that change the records (such as compression or
+  secure aggregation) may be allowed so long as they do not increase the
+  sensitivity of the query. It is the users' responsibility to ensure that the
+  mode of aggregation is consistent with the DPQuery. Note that the DPQuery's
+  built-in aggregation functions (accumulate_preprocessed_record and
+  merge_sample_states) are ignored in favor of the provided aggregator.
+
+  To obtain concrete (epsilon, delta) guarantees, one could use the analysis
+  tools provided in tensorflow_privacy by using QueryWithLedger.
+  """
+
+  def __init__(self,
+               query: tfp.DPQuery,
+               record_aggregation_factory: Optional[
+                   factory.AggregationProcessFactory] = None):
+    """Initializes `DifferentiallyPrivateFactory`.
+
+    Args:
+      query: A `tfp.SumAggregationDPQuery` to perform private estimation.
+      record_aggregation_factory: A `tff.aggregators.AggregationProcessFactory`
+        to aggregate values after preprocessing by the `query`. If `None`,
+        defaults to `tff.aggregators.SumFactory`. The provided factory is
+        assumed to implement a sum, and to have the property that it does not
+        increase the sensitivity of the query-- typically this means that it
+        should not increase the l2 norm of the records when aggregating.
+
+    Raises:
+      TypeError: If `query` is not an instance of `tfp.SumAggregationDPQuery` or
+        `record_aggregation_factory` is not an instance of
+        `tff.aggregators.AggregationProcessFactory`.
+    """
+    py_typecheck.check_type(query, tfp.SumAggregationDPQuery)
+    self._query = query
+
+    if record_aggregation_factory is None:
+      record_aggregation_factory = sum_factory.SumFactory()
+
+    py_typecheck.check_type(record_aggregation_factory,
+                            factory.AggregationProcessFactory)
+    self._record_aggregation_factory = record_aggregation_factory
+
+  def create(
+      self,
+      value_type: factory.ValueType) -> aggregation_process.AggregationProcess:
+    """Creates a `tff.aggregators.AggregationProcess` aggregating `value_type`.
+
+    The provided `value_type` is a non-federated `tff.Type` object, that is,
+    `value_type.is_federated()` should return `False`. Provided `value_type`
+    must be a `tff.TensorType` or a `tff.StructType`.
+
+    The returned `tff.aggregators.AggregationProcess` will be created for
+    aggregating values matching `value_type`. That is, its `next` method will
+    expect type `<S@SERVER, {value_type}@CLIENTS>`, where `S` is the unplaced
+    return type of its `initialize` method.
+
+    Args:
+      value_type: A `tff.Type` without placement.
+
+    Returns:
+      A `tff.templates.AggregationProcess`.
+    """
+    py_typecheck.check_type(value_type, factory.ValueType.__args__)
+
+    query_initial_state_fn = computations.tf_computation(
+        self._query.initial_global_state)
+
+    query_state_type = query_initial_state_fn.type_signature.result
+    derive_sample_params = computations.tf_computation(
+        self._query.derive_sample_params, query_state_type)
+    get_query_record = computations.tf_computation(
+        self._query.preprocess_record,
+        derive_sample_params.type_signature.result, value_type)
+    query_record_type = get_query_record.type_signature.result
+    get_noised_result = computations.tf_computation(
+        self._query.get_noised_result, query_record_type, query_state_type)
+    derive_metrics = computations.tf_computation(self._query.derive_metrics,
+                                                 query_state_type)
+
+    record_agg_process = self._record_aggregation_factory.create(
+        query_record_type)
+
+    @computations.federated_computation()
+    def init_fn():
+      return intrinsics.federated_zip(
+          (intrinsics.federated_eval(query_initial_state_fn, placements.SERVER),
+           record_agg_process.initialize()))
+
+    @computations.federated_computation(init_fn.type_signature.result,
+                                        computation_types.FederatedType(
+                                            value_type, placements.CLIENTS))
+    def next_fn(state, value):
+      query_state, agg_state = state
+
+      params = intrinsics.federated_broadcast(
+          intrinsics.federated_map(derive_sample_params, query_state))
+      record = intrinsics.federated_map(get_query_record, (params, value))
+
+      (new_agg_state, agg_result,
+       agg_measurements) = record_agg_process.next(agg_state, record)
+
+      result, new_query_state = intrinsics.federated_map(
+          get_noised_result, (agg_result, query_state))
+
+      query_metrics = intrinsics.federated_map(derive_metrics, new_query_state)
+
+      new_state = (new_query_state, new_agg_state)
+      measurements = collections.OrderedDict(
+          query_metrics=query_metrics, record_agg_process=agg_measurements)
+      return measured_process.MeasuredProcessOutput(
+          intrinsics.federated_zip(new_state), result,
+          intrinsics.federated_zip(measurements))
+
+    return aggregation_process.AggregationProcess(init_fn, next_fn)
---
+++ tensorflow_federated/python/aggregators/dp_factory_test.py
@@ -0,0 +1,190 @@
+# Copyright 2020, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for DifferentiallyPrivateFactory."""
+
+import collections
+
+from absl.testing import parameterized
+import tensorflow as tf
+import tensorflow_privacy as tfp
+
+from tensorflow_federated.python.aggregators import dp_factory
+from tensorflow_federated.python.aggregators import factory
+from tensorflow_federated.python.aggregators import test_utils
+from tensorflow_federated.python.core.api import computation_types
+from tensorflow_federated.python.core.api import placements
+from tensorflow_federated.python.core.api import test_case
+from tensorflow_federated.python.core.backends.native import execution_contexts
+from tensorflow_federated.python.core.impl.types import type_conversions
+from tensorflow_federated.python.core.templates import aggregation_process
+from tensorflow_federated.python.core.templates import measured_process
+
+_test_dp_query = tfp.GaussianSumQuery(l2_norm_clip=1.0, stddev=0.0)
+
+_test_struct_type = [(tf.float32, (2,)), tf.float32]
+_test_inner_agg_factory = test_utils.SumPlusOneFactory()
+
+
+class DPFactoryComputationTest(test_case.TestCase, parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      ('float_simple', tf.float32, None),
+      ('struct_simple', _test_struct_type, None),
+      ('float_inner', tf.float32, _test_inner_agg_factory),
+      ('struct_inner', _test_struct_type, _test_inner_agg_factory))
+  def test_type_properties(self, value_type, inner_agg_factory):
+    agg_factory = dp_factory.DifferentiallyPrivateFactory(
+        _test_dp_query, inner_agg_factory)
+    self.assertIsInstance(agg_factory, factory.AggregationProcessFactory)
+    value_type = computation_types.to_type(value_type)
+    process = agg_factory.create(value_type)
+    self.assertIsInstance(process, aggregation_process.AggregationProcess)
+
+    query_state = _test_dp_query.initial_global_state()
+    query_state_type = type_conversions.type_from_tensors(query_state)
+    query_metrics_type = type_conversions.type_from_tensors(
+        _test_dp_query.derive_metrics(query_state))
+
+    inner_state_type = tf.int32 if inner_agg_factory else ()
+
+    server_state_type = computation_types.at_server(
+        (query_state_type, inner_state_type))
+    expected_initialize_type = computation_types.FunctionType(
+        parameter=None, result=server_state_type)
+    self.assertTrue(
+        process.initialize.type_signature.is_equivalent_to(
+            expected_initialize_type))
+
+    inner_measurements_type = tf.int32 if inner_agg_factory else ()
+    expected_measurements_type = computation_types.at_server(
+        collections.OrderedDict(
+            query_metrics=query_metrics_type,
+            record_agg_process=inner_measurements_type))
+
+    expected_next_type = computation_types.FunctionType(
+        parameter=collections.OrderedDict(
+            state=server_state_type,
+            value=computation_types.at_clients(value_type)),
+        result=measured_process.MeasuredProcessOutput(
+            state=server_state_type,
+            result=computation_types.at_server(value_type),
+            measurements=expected_measurements_type))
+    self.assertTrue(
+        process.next.type_signature.is_equivalent_to(expected_next_type))
+
+  def test_init_non_dp_query_raises(self):
+    with self.assertRaises(TypeError):
+      dp_factory.DifferentiallyPrivateFactory('not a dp_query')
+
+  def test_init_non_agg_factory_raises(self):
+    with self.assertRaises(TypeError):
+      dp_factory.DifferentiallyPrivateFactory(_test_dp_query,
+                                              'not an agg factory')
+
+  @parameterized.named_parameters(
+      ('federated_type',
+       computation_types.FederatedType(tf.float32, placements.SERVER)),
+      ('function_type', computation_types.FunctionType(None, ())),
+      ('sequence_type', computation_types.SequenceType(tf.float32)))
+  def test_incorrect_value_type_raises(self, bad_value_type):
+    agg_factory = dp_factory.DifferentiallyPrivateFactory(_test_dp_query)
+    with self.assertRaises(TypeError):
+      agg_factory.create(bad_value_type)
+
+
+class DPFactoryExecutionTest(test_case.TestCase):
+
+  def test_simple_sum(self):
+    agg_factory = dp_factory.DifferentiallyPrivateFactory(_test_dp_query)
+    value_type = computation_types.to_type(tf.float32)
+    process = agg_factory.create(value_type)
+
+    # The test query has clip 1.0 and no noise, so this computes clipped sum.
+
+    state = process.initialize()
+
+    client_data = [0.5, 1.0, 1.5]
+    output = process.next(state, client_data)
+    self.assertAllClose(2.5, output.result)
+
+  def test_structure_sum(self):
+    agg_factory = dp_factory.DifferentiallyPrivateFactory(_test_dp_query)
+    value_type = computation_types.to_type([tf.float32, tf.float32])
+    process = agg_factory.create(value_type)
+
+    # The test query has clip 1.0 and no noise, so this computes clipped sum.
+
+    state = process.initialize()
+
+    # pyformat: disable
+    client_data = [
+        [0.1, 0.2],         # not clipped (norm < 1)
+        [5 / 13, 12 / 13],  # not clipped (norm == 1)
+        [3.0, 4.0]          # clipped to 3/5, 4/5
+    ]
+    output = process.next(state, client_data)
+
+    expected_result = [0.1 +  5 / 13 + 3 / 5,
+                       0.2 + 12 / 13 + 4 / 5]
+    # pyformat: enable
+    self.assertAllClose(expected_result, output.result)
+
+  def test_inner_sum(self):
+    agg_factory = dp_factory.DifferentiallyPrivateFactory(
+        _test_dp_query, _test_inner_agg_factory)
+    value_type = computation_types.to_type(tf.float32)
+    process = agg_factory.create(value_type)
+
+    # The test query has clip 1.0 and no noise, so this computes clipped sum.
+    # Inner agg adds another 1.0 (post-clipping).
+
+    state = process.initialize()
+    self.assertAllEqual(0, state[1])
+
+    client_data = [0.5, 1.0, 1.5]
+    output = process.next(state, client_data)
+    self.assertAllEqual(1, output.state[1])
+    self.assertAllClose(3.5, output.result)
+    self.assertAllEqual(test_utils.MEASUREMENT_CONSTANT,
+                        output.measurements['record_agg_process'])
+
+  def test_adaptive_query(self):
+    query = tfp.QuantileAdaptiveClipSumQuery(
+        initial_l2_norm_clip=1.0,
+        noise_multiplier=0.0,
+        target_unclipped_quantile=1.0,
+        learning_rate=1.0,
+        clipped_count_stddev=0.0,
+        expected_num_records=3.0,
+        geometric_update=False)
+    agg_factory = dp_factory.DifferentiallyPrivateFactory(query)
+    value_type = computation_types.to_type(tf.float32)
+    process = agg_factory.create(value_type)
+
+    state = process.initialize()
+
+    client_data = [0.5, 1.5, 2.0]  # Two clipped on first round.
+    expected_result = 0.5 + 1.0 + 1.0
+    output = process.next(state, client_data)
+    self.assertAllClose(expected_result, output.result)
+
+    # Clip is increased by 2/3 to 5/3.
+    expected_result = 0.5 + 1.5 + 5 / 3
+    output = process.next(output.state, client_data)
+    self.assertAllClose(expected_result, output.result)
+
+
+if __name__ == '__main__':
+  execution_contexts.set_local_execution_context()
+  test_case.main()
