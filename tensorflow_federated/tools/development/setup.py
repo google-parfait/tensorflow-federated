c563bae6f69b87c6362a52584e806c698efe42a6
Michael Reneer <michaelreneer@google.com>
2020-11-19 17:41:26 -0800

    Update the Python package classifiers to reflect the correct Python compatibility.
    PiperOrigin-RevId: 343404565

--- tensorflow_federated/tools/development/setup.py
+++ tensorflow_federated/tools/development/setup.py
@@ -123,6 +123,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
