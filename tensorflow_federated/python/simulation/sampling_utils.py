7726eedd102ffd43aa1957eb861c285aba2e92c9
Zachary Charles <zachcharles@google.com>
2020-12-21 12:31:30 -0800

    Add utilities for pseudo-random client sampling.
    PiperOrigin-RevId: 348510108

---
+++ tensorflow_federated/python/simulation/sampling_utils.py
@@ -0,0 +1,107 @@
+# Copyright 2019, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utilities for sampling clients, either randomly or pseudo-randomly."""
+
+import functools
+from typing import Callable, List, Optional, Sequence, TypeVar
+
+import numpy as np
+
+from tensorflow_federated.python.simulation import client_data
+
+
+#  Settings for a multiplicative linear congruential generator (aka Lehmer
+#  generator) suggested in 'Random Number Generators: Good
+#  Ones are Hard to Find' by Park and Miller.
+MLCG_MODULUS = 2**(31) - 1
+MLCG_MULTIPLIER = 16807
+
+
+# Type variable for matching the input and output of `build_sampling_fn`
+T = TypeVar('T')
+
+
+def build_sampling_fn(
+    sample_range: Sequence[T],
+    size: int,
+    replace: bool = False,
+    random_seed: Optional[int] = None) -> Callable[[int], List[T]]:
+  """Builds the function for sampling from the input iterator at each round.
+
+  Args:
+    sample_range: A 1-D array-like sequence, to be used as input to
+    `np.random.choice`. Samples are generated randomly from the elements of the
+    sequence.
+    size: The number of samples to return each round.
+    replace: A boolean indicating whether the sampling is done with replacement
+      (True) or without replacement (False).
+    random_seed: If random_seed is set as an integer, then we use it as a random
+      seed for which clients are sampled at each round. In this case, we set a
+      random seed before sampling clients according to a multiplicative linear
+      congruential generator (aka Lehmer generator, see 'The Art of Computer
+      Programming, Vol. 3' by Donald Knuth for reference). This does not affect
+      model initialization, shuffling, or other such aspects of the federated
+      training process.
+
+  Returns:
+    A function that takes as input an integer `round_num` and returns a list of
+    elements sampled (pseudo-)randomly from the input `sample_range`.
+  """
+  if isinstance(random_seed, int):
+    mlcg_start = np.random.RandomState(random_seed).randint(1, MLCG_MODULUS - 1)
+
+    def get_pseudo_random_int(round_num):
+      return pow(MLCG_MULTIPLIER, round_num,
+                 MLCG_MODULUS) * mlcg_start % MLCG_MODULUS
+
+  def sample(round_num, random_seed):
+    if isinstance(random_seed, int):
+      random_state = np.random.RandomState(get_pseudo_random_int(round_num))
+    else:
+      random_state = np.random.RandomState()
+    return random_state.choice(
+        sample_range, size=size, replace=replace).tolist()
+
+  return functools.partial(sample, random_seed=random_seed)
+
+
+def build_client_sampling_fn(
+    dataset: client_data.ClientData,
+    clients_per_round: int,
+    random_seed: Optional[int] = None) -> Callable[[int], List[str]]:
+  """Builds a function that (pseudo-)randomly samples clients.
+
+  The function samples a number of clients (without replacement within a given
+  round, but with replacement across rounds) and returns their ids.
+
+  Args:
+    dataset: A `tff.simulation.ClientData` object.
+    clients_per_round: The number of client participants in each round.
+    random_seed: If random_seed is set as an integer, then we use it as a random
+      seed for which clients are sampled at each round. In this case, we set a
+      random seed before sampling clients according to a multiplicative linear
+      congruential generator (aka Lehmer generator, see 'The Art of Computer
+      Programming, Vol. 3' by Donald Knuth for reference). This does not affect
+      model initialization, shuffling, or other such aspects of the federated
+      training process. Note that this will alter the global numpy random seed.
+
+  Returns:
+    A function that takes as input an integer `round_num` and returns a a list
+    of ids corresponding to the sampled clients.
+  """
+  return build_sampling_fn(
+      dataset.client_ids,
+      size=clients_per_round,
+      replace=False,
+      random_seed=random_seed)
---
+++ tensorflow_federated/python/simulation/sampling_utils_test.py
@@ -0,0 +1,140 @@
+# Copyright 2019, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import parameterized
+import tensorflow as tf
+
+from tensorflow_federated.python.simulation import client_data
+from tensorflow_federated.python.simulation import sampling_utils
+
+
+def create_tf_dataset_for_client(client_id):
+  del client_id
+  return tf.data.Dataset.range(1)
+
+
+class SamplingTest(tf.test.TestCase, parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      {
+          'testcase_name': '_int_no_replace',
+          'a': range(100),
+          'replace': False
+      }, {
+          'testcase_name': '_int_replace',
+          'a': range(5),
+          'replace': True
+      }, {
+          'testcase_name': '_sequence_no_replace',
+          'a': [str(i) for i in range(100)],
+          'replace': False
+      }, {
+          'testcase_name': '_sequence_replace',
+          'a': [str(i) for i in range(5)],
+          'replace': True
+      })
+  def test_build_sampling_fn_with_random_seed(self, a, replace):
+    size = 10
+    random_seed = 1
+    round_num = 5
+
+    sample_fn_1 = sampling_utils.build_sampling_fn(
+        a, size, replace=replace, random_seed=random_seed)
+    sample_1 = sample_fn_1(round_num)
+
+    sample_fn_2 = sampling_utils.build_sampling_fn(
+        a, size, replace=replace, random_seed=random_seed)
+    sample_2 = sample_fn_2(round_num)
+
+    self.assertEqual(sample_1, sample_2)
+
+  @parameterized.named_parameters(
+      {
+          'testcase_name': '_int_no_replace',
+          'a': range(100),
+          'replace': False
+      }, {
+          'testcase_name': '_int_replace',
+          'a': range(5),
+          'replace': True
+      }, {
+          'testcase_name': '_sequence_no_replace',
+          'a': [str(i) for i in range(100)],
+          'replace': False
+      }, {
+          'testcase_name': '_sequence_replace',
+          'a': [str(i) for i in range(5)],
+          'replace': True
+      })
+  def test_build_sampling_fn_without_random_seed(self, a, replace):
+    size = 10
+    round_num = 5
+
+    sample_fn_1 = sampling_utils.build_sampling_fn(a, size, replace=replace)
+    sample_1 = sample_fn_1(round_num)
+
+    sample_fn_2 = sampling_utils.build_sampling_fn(a, size, replace=replace)
+    sample_2 = sample_fn_2(round_num)
+
+    self.assertNotEqual(sample_1, sample_2)
+
+  def test_client_sampling_with_one_client(self):
+    tff_dataset = client_data.ConcreteClientData([2],
+                                                 create_tf_dataset_for_client)
+    client_sampling_fn = sampling_utils.build_client_sampling_fn(
+        tff_dataset, clients_per_round=1)
+    client_ids = client_sampling_fn(round_num=7)
+    self.assertEqual(client_ids, [2])
+
+  def test_client_sampling_fn_with_random_seed(self):
+    tff_dataset = client_data.ConcreteClientData([0, 1, 2, 3, 4],
+                                                 create_tf_dataset_for_client)
+
+    client_sampling_fn_1 = sampling_utils.build_client_sampling_fn(
+        tff_dataset, clients_per_round=1, random_seed=363)
+    client_ids_1 = client_sampling_fn_1(round_num=5)
+
+    client_sampling_fn_2 = sampling_utils.build_client_sampling_fn(
+        tff_dataset, clients_per_round=1, random_seed=363)
+    client_ids_2 = client_sampling_fn_2(round_num=5)
+
+    self.assertEqual(client_ids_1, client_ids_2)
+
+  def test_different_random_seed_give_different_clients(self):
+    tff_dataset = client_data.ConcreteClientData(
+        list(range(100)), create_tf_dataset_for_client)
+
+    client_sampling_fn_1 = sampling_utils.build_client_sampling_fn(
+        tff_dataset, clients_per_round=50, random_seed=1)
+    client_ids_1 = client_sampling_fn_1(round_num=1001)
+
+    client_sampling_fn_2 = sampling_utils.build_client_sampling_fn(
+        tff_dataset, clients_per_round=50, random_seed=2)
+    client_ids_2 = client_sampling_fn_2(round_num=1001)
+
+    self.assertNotEqual(client_ids_1, client_ids_2)
+
+  def test_client_sampling_fn_without_random_seed(self):
+    tff_dataset = client_data.ConcreteClientData(
+        list(range(100)), create_tf_dataset_for_client)
+    client_sampling_fn = sampling_utils.build_client_sampling_fn(
+        tff_dataset, clients_per_round=50)
+    client_ids_1 = client_sampling_fn(round_num=0)
+
+    client_ids_2 = client_sampling_fn(round_num=0)
+    self.assertNotEqual(client_ids_1, client_ids_2)
+
+
+if __name__ == '__main__':
+  tf.test.main()
