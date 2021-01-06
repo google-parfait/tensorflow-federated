61d452f60df1ef9e009b1d54c21b2a4d020c1646
Krzysztof Ostrowski <ostrowski@google.com>
2021-01-05 18:44:06 -0800

    Adds typechecks to reconcile XLA and TFF type information during XLA serialization.
    PiperOrigin-RevId: 350262669

--- tensorflow_federated/experimental/python/core/impl/utils/xla_serialization.py
+++ tensorflow_federated/experimental/python/core/impl/utils/xla_serialization.py
@@ -16,6 +16,7 @@
 from typing import Optional
 
 from jax.lib.xla_bridge import xla_client
+import numpy as np
 
 from google.protobuf import any_pb2
 from tensorflow_federated.proto.v0 import computation_pb2 as pb
@@ -102,6 +103,34 @@ def _make_xla_binding_for_type(
   return binding
 
 
+def _remove_struct_element_names_from_tff_type(type_spec):
+  """Removes names of struct elements from `type_spec`.
+
+  Args:
+    type_spec: An instance of `computation_types.Type` that must be a tensor, a
+      (possibly) nested structure of tensors, or a function.
+
+  Returns:
+    A modified version of `type_spec` with element names in stuctures removed.
+
+  Raises:
+    TypeError: if arg is of the wrong type.
+  """
+  if type_spec is None:
+    return None
+  if isinstance(type_spec, computation_types.FunctionType):
+    return computation_types.FunctionType(
+        _remove_struct_element_names_from_tff_type(type_spec.parameter),
+        _remove_struct_element_names_from_tff_type(type_spec.result))
+  if isinstance(type_spec, computation_types.TensorType):
+    return type_spec
+  py_typecheck.check_type(type_spec, computation_types.StructType)
+  return computation_types.StructType([
+      (None, _remove_struct_element_names_from_tff_type(v))
+      for _, v in structure.iter_elements(type_spec)
+  ])
+
+
 def create_xla_tff_computation(xla_computation, type_spec):
   """Creates an XLA TFF computation.
 
@@ -111,12 +140,123 @@ def create_xla_tff_computation(xla_computation, type_spec):
 
   Returns:
     An instance of `pb.Computation`.
+
+  Raises:
+    ValueError: if the arguments are invalid or incompatible with each other,
+      e.g., because the TFF types mismatch.
   """
   py_typecheck.check_type(xla_computation, xla_client.XlaComputation)
   py_typecheck.check_type(type_spec, computation_types.FunctionType)
+  parameter_binding = _make_xla_binding_for_type(type_spec.parameter)
+  result_binding = _make_xla_binding_for_type(type_spec.result)
+  reconstructed_type = xla_computation_and_bindings_to_tff_type(
+      xla_computation, parameter_binding, result_binding)
+  py_typecheck.check_type(reconstructed_type, computation_types.FunctionType)
+  expected_type = _remove_struct_element_names_from_tff_type(type_spec)
+  if not reconstructed_type.is_equivalent_to(expected_type):
+    raise ValueError(
+        'The TFF type of the XLA computation {} does not match the expected '
+        'TFF type {}.'.format(str(reconstructed_type), str(expected_type)))
   return pb.Computation(
       type=type_serialization.serialize_type(type_spec),
       xla=pb.Xla(
           hlo_module=pack_xla_computation(xla_computation),
-          parameter=_make_xla_binding_for_type(type_spec.parameter),
-          result=_make_xla_binding_for_type(type_spec.result)))
+          parameter=parameter_binding,
+          result=result_binding))
+
+
+def xla_computation_and_bindings_to_tff_type(xla_computation, parameter_binding,
+                                             result_binding):
+  """Constructs the TFF type from an `xla_client.XlaComputation` and bindings.
+
+  NOTE: This is a helper function, primarily intended for use in checking the
+  well-formedness of TFF computations during serialization and deserialization,
+  and for serialization testing/debugging purposes.
+
+  Args:
+    xla_computation: An instance of `xla_client.XlaComputation` to get type for.
+    parameter_binding: An instance of `pb.Xla.Binding` for the parameter.
+    result_binding: An instance of `pb.Xla.Binding` for the result.
+
+  Returns:
+    An instance of `computation_types.Type`.
+  """
+  py_typecheck.check_type(xla_computation, xla_client.XlaComputation)
+  program_shape = xla_computation.program_shape()
+  return computation_types.FunctionType(
+      xla_shapes_and_binding_to_tff_type(program_shape.parameter_shapes(),
+                                         parameter_binding),
+      xla_shapes_and_binding_to_tff_type([program_shape.result_shape()],
+                                         result_binding))
+
+
+def xla_shapes_and_binding_to_tff_type(xla_shapes, binding):
+  """Constructs the TFF type from a list of `xla_client.Shape` and a binding.
+
+  Args:
+    xla_shapes: A list of `xla_client.Shape` instances.
+    binding: An instance of `pb.Xla.Binding` (or `None` if there's none).
+
+  Returns:
+    An instance of `computation_types.Type` (or `None`).
+  """
+  py_typecheck.check_type(xla_shapes, list)
+  if binding is not None:
+    py_typecheck.check_type(binding, pb.Xla.Binding)
+  tensor_shapes = []
+  for shape in xla_shapes:
+    tensor_shapes += flatten_xla_shape(shape)
+  unused_shape_indexes = set(range(len(tensor_shapes)))
+
+  def _get_type(binding):
+    if binding is None:
+      return None
+    kind = binding.WhichOneof('binding')
+    if kind == 'tensor':
+      index = binding.tensor.index
+      if (index < 0) or (index >= len(tensor_shapes)):
+        raise ValueError(
+            'Binding refers to an inexistent index {}.'.format(index))
+      if index not in unused_shape_indexes:
+        raise ValueError(
+            'Duplicate bindings referring to index {}.'.format(index))
+      unused_shape_indexes.remove(index)
+      shape = tensor_shapes[index]
+      return computation_types.TensorType(shape.numpy_dtype(),
+                                          shape.dimensions())
+    if kind == 'struct':
+      return computation_types.StructType([
+          (None, _get_type(x)) for x in binding.struct.element
+      ])
+    if kind is None:
+      return None
+    raise ValueError('Unrecognized binding type {}.'.format(kind))
+
+  tff_type = _get_type(binding)
+  if unused_shape_indexes:
+    raise ValueError(
+        'Binding fails to capture tensors {}.'.format(unused_shape_indexes))
+  return tff_type
+
+
+def flatten_xla_shape(xla_shape):
+  """Flattens a possibly nested tuple XLA shape into a list of tensor shapes.
+
+  Args:
+    xla_shape: An instance of `xla_client.Shape` (could be a nested structure).
+
+  Returns:
+    A Python list of `xla_client.Shape` instances representing tensors.
+  """
+  py_typecheck.check_type(xla_shape, xla_client.Shape)
+  if xla_shape.is_tuple():
+    tensor_shapes = []
+    for shape in xla_shape.tuple_shapes():
+      tensor_shapes += flatten_xla_shape(shape)
+    return tensor_shapes
+  else:
+    # Must be a tensor (array) type; verify this by probing for dimensions and
+    # element_type, since there's no explicit way to check otherwise.
+    py_typecheck.check_type(xla_shape.element_type(), np.dtype)
+    py_typecheck.check_type(xla_shape.dimensions(), tuple)
+    return [xla_shape]
--- tensorflow_federated/experimental/python/core/impl/utils/xla_serialization_test.py
+++ tensorflow_federated/experimental/python/core/impl/utils/xla_serialization_test.py
@@ -23,27 +23,37 @@ from tensorflow_federated.python.core.api import computation_types
 from tensorflow_federated.python.core.impl.types import type_serialization
 
 
-def _make_test_xla_comp():
+def _make_test_xla_comp_noarg_to_int32():
   builder = xla_client.XlaBuilder('comp')
   xla_client.ops.Constant(builder, np.int32(10))
+  return builder.build()
+
+
+def _make_test_xla_comp_int32x10_to_int32x10():
+  builder = xla_client.XlaBuilder('comp')
+  tensor_shape = xla_client.Shape.array_shape(
+      xla_client.dtype_to_etype(np.int32), (10,))
+  param = xla_client.ops.Parameter(builder, 0, tensor_shape)
+  constant = xla_client.ops.Constant(builder, np.zeros((10,), dtype=np.int32))
+  xla_client.ops.Add(param, constant)
   return builder.build()
 
 
 class XlaUtilsTest(absltest.TestCase):
 
   def test_pack_xla_computation(self):
-    xla_comp = _make_test_xla_comp()
+    xla_comp = _make_test_xla_comp_noarg_to_int32()
     any_pb = xla_serialization.pack_xla_computation(xla_comp)
     self.assertEqual(type(any_pb), any_pb2.Any)
 
   def test_pack_unpack_xla_computation_roundtrip(self):
-    xla_comp = _make_test_xla_comp()
+    xla_comp = _make_test_xla_comp_noarg_to_int32()
     any_pb = xla_serialization.pack_xla_computation(xla_comp)
     new_comp = xla_serialization.unpack_xla_computation(any_pb)
     self.assertEqual(new_comp.as_hlo_text(), xla_comp.as_hlo_text())
 
-  def test_create_xla_tff_computation(self):
-    xla_comp = _make_test_xla_comp()
+  def test_create_xla_tff_computation_noarg(self):
+    xla_comp = _make_test_xla_comp_noarg_to_int32()
     comp_pb = xla_serialization.create_xla_tff_computation(
         xla_comp, computation_types.FunctionType(None, np.int32))
     self.assertIsInstance(comp_pb, pb.Computation)
@@ -55,6 +65,170 @@ class XlaUtilsTest(absltest.TestCase):
                   xla_comp.as_hlo_text())
     self.assertEqual(str(comp_pb.xla.parameter), '')
     self.assertEqual(str(comp_pb.xla.result), 'tensor {\n' '  index: 0\n' '}\n')
+
+  def test_create_xla_tff_computation_raises_missing_arg_in_xla(self):
+    xla_comp = _make_test_xla_comp_noarg_to_int32()
+    with self.assertRaises(ValueError):
+      xla_serialization.create_xla_tff_computation(
+          xla_comp, computation_types.FunctionType(np.int32, np.int32))
+
+  def test_create_xla_tff_computation_raises_missing_arg_in_type_spec(self):
+    xla_comp = _make_test_xla_comp_int32x10_to_int32x10()
+    with self.assertRaises(ValueError):
+      xla_serialization.create_xla_tff_computation(
+          xla_comp, computation_types.FunctionType(None, np.int32))
+
+  def test_create_xla_tff_computation_raises_arg_type_mismatch(self):
+    xla_comp = _make_test_xla_comp_int32x10_to_int32x10()
+    with self.assertRaises(ValueError):
+      xla_serialization.create_xla_tff_computation(
+          xla_comp, computation_types.FunctionType(np.int32, (np.int32, (10,))))
+
+  def test_create_xla_tff_computation_raises_result_type_mismatch(self):
+    xla_comp = _make_test_xla_comp_int32x10_to_int32x10()
+    with self.assertRaises(ValueError):
+      xla_serialization.create_xla_tff_computation(
+          xla_comp, computation_types.FunctionType((np.int32, (10,)), np.int32))
+
+  def test_create_xla_tff_computation_int32x10_to_int32x10(self):
+    xla_comp = _make_test_xla_comp_int32x10_to_int32x10()
+    comp_pb = xla_serialization.create_xla_tff_computation(
+        xla_comp,
+        computation_types.FunctionType((np.int32, (10,)), (np.int32, (10,))))
+    self.assertIsInstance(comp_pb, pb.Computation)
+    self.assertEqual(comp_pb.WhichOneof('computation'), 'xla')
+    type_spec = type_serialization.deserialize_type(comp_pb.type)
+    self.assertEqual(str(type_spec), '(int32[10] -> int32[10])')
+
+  def test_flatten_xla_tensor_shape(self):
+    tensor_shape = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.int32), (10,))
+    flattened = xla_serialization.flatten_xla_shape(tensor_shape)
+    self.assertIsInstance(flattened, list)
+    self.assertListEqual(flattened, [tensor_shape])
+
+  def test_flatten_xla_tuple_shape(self):
+    tensor_shape_1 = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.int32), (10,))
+    tensor_shape_2 = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.float32), (20,))
+    tuple_shape = xla_client.Shape.tuple_shape([tensor_shape_1, tensor_shape_2])
+    flattened = xla_serialization.flatten_xla_shape(tuple_shape)
+    self.assertIsInstance(flattened, list)
+    self.assertListEqual(flattened, [tensor_shape_1, tensor_shape_2])
+
+  def test_xla_shapes_and_binding_to_tff_type_with_tensor(self):
+    tensor_shape = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.int32), (10,))
+    xla_shapes = [tensor_shape]
+    binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
+    tff_type = xla_serialization.xla_shapes_and_binding_to_tff_type(
+        xla_shapes, binding)
+    self.assertEqual(str(tff_type), 'int32[10]')
+
+  def test_xla_shapes_and_binding_to_tff_type_with_tuple(self):
+    tensor_shape_1 = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.int32), (10,))
+    tensor_shape_2 = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.float32), (20,))
+    xla_shapes = [tensor_shape_1, tensor_shape_2]
+    binding = pb.Xla.Binding(
+        struct=pb.Xla.StructBinding(element=[
+            pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=1)),
+            pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
+        ]))
+    tff_type = xla_serialization.xla_shapes_and_binding_to_tff_type(
+        xla_shapes, binding)
+    self.assertEqual(str(tff_type), '<float32[20],int32[10]>')
+
+  def test_xla_shapes_and_binding_to_tff_type_raises_unused_tensor(self):
+    tensor_shape = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.int32), (10,))
+    xla_shapes = [tensor_shape]
+    binding = None
+    with self.assertRaises(ValueError):
+      xla_serialization.xla_shapes_and_binding_to_tff_type(xla_shapes, binding)
+
+  def test_xla_shapes_and_binding_to_tff_type_raises_unused_element(self):
+    tensor_shape_1 = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.int32), (10,))
+    tensor_shape_2 = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.float32), (20,))
+    xla_shapes = [tensor_shape_1, tensor_shape_2]
+    binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=1))
+    with self.assertRaises(ValueError):
+      xla_serialization.xla_shapes_and_binding_to_tff_type(xla_shapes, binding)
+
+  def test_xla_computation_and_bindings_to_tff_type_none_binding_to_int32(self):
+    builder = xla_client.XlaBuilder('comp')
+    xla_client.ops.Constant(builder, np.int32(10))
+    xla_computation = builder.build()
+    parameter_binding = None
+    result_binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
+    tff_type = xla_serialization.xla_computation_and_bindings_to_tff_type(
+        xla_computation, parameter_binding, result_binding)
+    self.assertEqual(str(tff_type), '( -> int32)')
+
+  def test_xla_computation_and_bindings_to_tff_type_noarg_to_int32(self):
+    xla_computation = _make_test_xla_comp_noarg_to_int32()
+    parameter_binding = pb.Xla.Binding()
+    result_binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
+    tff_type = xla_serialization.xla_computation_and_bindings_to_tff_type(
+        xla_computation, parameter_binding, result_binding)
+    self.assertEqual(str(tff_type), '( -> int32)')
+
+  def test_xla_computation_and_bindings_to_tff_type_empty_tuple_to_int32(self):
+    builder = xla_client.XlaBuilder('comp')
+    xla_client.ops.Parameter(builder, 0, xla_client.Shape.tuple_shape([]))
+    xla_client.ops.Constant(builder, np.int32(10))
+    xla_computation = builder.build()
+    parameter_binding = pb.Xla.Binding()
+    result_binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
+    tff_type = xla_serialization.xla_computation_and_bindings_to_tff_type(
+        xla_computation, parameter_binding, result_binding)
+    self.assertEqual(str(tff_type), '( -> int32)')
+
+  def test_xla_computation_and_bindings_to_tff_type_int32_tuple_to_int32(self):
+    builder = xla_client.XlaBuilder('comp')
+    tensor_shape = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.int32), (10,))
+    tuple_shape = xla_client.Shape.tuple_shape([tensor_shape])
+    param = xla_client.ops.Parameter(builder, 0, tuple_shape)
+    constant = xla_client.ops.Constant(builder, np.zeros((10,), dtype=np.int32))
+    xla_client.ops.Add(xla_client.ops.GetTupleElement(param, 0), constant)
+    xla_computation = builder.build()
+    parameter_binding = pb.Xla.Binding(
+        struct=pb.Xla.StructBinding(
+            element=[pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))]))
+    result_binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
+    tff_type = xla_serialization.xla_computation_and_bindings_to_tff_type(
+        xla_computation, parameter_binding, result_binding)
+    self.assertEqual(str(tff_type), '(<int32[10]> -> int32[10])')
+
+  def test_xla_computation_and_bindings_to_tff_type_raises_noarg(self):
+    xla_computation = _make_test_xla_comp_int32x10_to_int32x10()
+    parameter_binding = None
+    result_binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
+    with self.assertRaises(ValueError):
+      xla_serialization.xla_computation_and_bindings_to_tff_type(
+          xla_computation, parameter_binding, result_binding)
+
+  def test_xla_computation_and_bindings_to_tff_type_raises_unused_element(self):
+    builder = xla_client.XlaBuilder('comp')
+    tensor_shape = xla_client.Shape.array_shape(
+        xla_client.dtype_to_etype(np.int32), (10,))
+    tuple_shape = xla_client.Shape.tuple_shape([tensor_shape, tensor_shape])
+    param = xla_client.ops.Parameter(builder, 0, tuple_shape)
+    constant = xla_client.ops.Constant(builder, np.zeros((10,), dtype=np.int32))
+    xla_client.ops.Add(xla_client.ops.GetTupleElement(param, 0), constant)
+    xla_computation = builder.build()
+    parameter_binding = pb.Xla.Binding(
+        struct=pb.Xla.StructBinding(
+            element=[pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))]))
+    result_binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
+    with self.assertRaises(ValueError):
+      xla_serialization.xla_computation_and_bindings_to_tff_type(
+          xla_computation, parameter_binding, result_binding)
 
 
 if __name__ == '__main__':
