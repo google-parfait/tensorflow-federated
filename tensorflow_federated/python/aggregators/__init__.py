ffaa5d0716d0e78d9dc823a547cdd5cbbe70fca6
Zachary Garrett <zachgarrett@google.com>
2021-03-02 13:26:48 -0800

    Expose `UnweightedReservoirSamplingFactory` in the `tff.aggregators` package.
    PiperOrigin-RevId: 360501463

--- tensorflow_federated/python/aggregators/__init__.py
+++ tensorflow_federated/python/aggregators/__init__.py
@@ -30,5 +30,6 @@ from tensorflow_federated.python.aggregators.mean import UnweightedMeanFactory
 from tensorflow_federated.python.aggregators.quantile_estimation import PrivateQuantileEstimationProcess
 from tensorflow_federated.python.aggregators.robust import clipping_factory
 from tensorflow_federated.python.aggregators.robust import zeroing_factory
+from tensorflow_federated.python.aggregators.sampling import UnweightedReservoirSamplingFactory
 from tensorflow_federated.python.aggregators.secure import SecureSumFactory
 from tensorflow_federated.python.aggregators.sum_factory import SumFactory
