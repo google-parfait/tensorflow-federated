133144eea5eda2d5c10c23a00e1840b13af134ca
Zachary Charles <zachcharles@google.com>
2021-03-05 11:56:54 -0800

    Create a LearningProcess template specializing IterativeProcess.
    PiperOrigin-RevId: 361194593

---
+++ tensorflow_federated/python/learning/learning_process.py
@@ -0,0 +1,223 @@
+# Copyright 2021, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Defines a template for stateful processes used for learning-oriented tasks."""
+
+import attr
+
+from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.core.api import computation_base
+from tensorflow_federated.python.core.api import computation_types
+from tensorflow_federated.python.core.api import placements
+from tensorflow_federated.python.core.templates import errors
+from tensorflow_federated.python.core.templates import iterative_process
+
+
+class LearningProcessPlacementError(TypeError):
+  """Raises when a learning process does not have expected placements."""
+  pass
+
+
+class LearningProcessSequenceTypeError(TypeError):
+  """Raises when a learning process does not have the expected sequence type."""
+
+
+class LearningProcessOutputError(TypeError):
+  """Raises when a learning process does not have the expected output type."""
+
+
+class ReportFnTypeSignatureError(TypeError):
+  """Raises when the type signature `report_fn` is not correct."""
+
+
+@attr.s(frozen=True, eq=False, slots=True)
+class LearningProcessOutput:
+  """A structure containing the output of a `LearningProcess.next` computation.
+
+  Attributes:
+    state: A structure that will be passed to invocation of
+      `LearningProcess.next`. Not intended for inspection externally, contains
+      implementation details of the process.
+    metrics: Metrics derived from the applying the process to a given state and
+      input. This output is intended for surfacing values to better understand
+      the progress of a learning process, and is not directly intended to be
+      used as input to `LearningProcess.next`. For example, this output may
+      include classical metrics surrounding the quality of a machine learning
+      model (eg. the average accuracy of a model across the participating
+      clients), or operational metrics concerning systems-oriented information
+      (eg. timing information and the amount of communication occurring between
+      clients and server).
+  """
+  state = attr.ib()
+  metrics = attr.ib()
+
+
+class LearningProcess(iterative_process.IterativeProcess):
+  """A stateful process for learning tasks that produces metrics.
+
+  This class inherits the constraints documented by
+  `tff.templates.IterativeProcess`, including an `initialize` and `next`
+  attribute. The `LearningProcess` also contains an additional `report`
+  attribute.
+
+  All of `initialize`, `next` and `report`  must be `tff.Computation`s, with the
+  following type signatures:
+    - initialize: `( -> S@SERVER)`
+    - next: `(<S@SERVER, {D*}@CLIENTS> -> <state=S@SERVER, metrics=M@SERVER>)`
+    - report: `(S -> R)`
+  where `{D*}@CLIENTS` represents the sequence of data at a client, with `D`
+  denoting the type of a single member of that sequence, and `R` representing
+  the (unplaced) output type of the `report` function.
+
+  For example, given a LearningProcess `process` and client data `data`, we
+  could call the following to initialize, update the state three times, and get
+  a report of the resulting state:
+  ```
+  state = process.initialize()
+  for _ in range(3):
+    state, metrics = process.next(state, data)
+  report = process.report(state)
+  """
+
+  def __init__(self, initialize_fn: computation_base.Computation,
+               next_fn: computation_base.Computation,
+               report_fn: computation_base.Computation):
+    """Creates a `tff.templates.AggregationProcess`.
+
+    Args:
+      initialize_fn: A no-arg `tff.Computation` that creates the initial state
+        of the learning process.
+      next_fn: A `tff.Computation` that defines an iterated function. Given that
+        `initialize_fn` returns a type `S@SERVER`, the `next_fn` must return a
+        `LearningProcessOutput` where the `state` attribute matches the type
+        `S@SERVER`, and accepts two argument of types `S@SERVER` and
+        `{D*}@CLIENTS`.
+     report_fn: A `tff.Computation` that accepts an input `S` where the output
+       of `initialize_fn` is of type `S@SERVER`. This computation is used to
+       create a representation of the state that can be used for downstream
+       tasks without requiring access to the entire server state. For example,
+       `report_fn` could be used to extract model weights for computing metrics
+       on held-out data.
+
+    Raises:
+      TypeError: If `initialize_fn` and `next_fn` are not instances of
+        `tff.Computation`.
+      TemplateInitFnParamNotEmptyError: If `initialize_fn` has any input
+        arguments.
+      TemplateStateNotAssignableError: If the `state` returned by either
+        `initialize_fn` or `next_fn` is not assignable to the first input
+        argument of `next_fn`.
+      TemplateNextFnNumArgsError: If `next_fn` does not have at exactly two
+        input arguments.
+      LearningProcessPlacementError: If the placements of `initialize_fn` and
+        `next_fn` do not match the expected type placements.
+      LearningProcessOutputError: If `next_fn` does not return a
+        `LearningProcessOutput`.
+      LearningProcessSequenceTypeError: If the second argument to `next_fn` is
+        not a sequence type.
+    """
+    super().__init__(initialize_fn, next_fn)
+
+    init_fn_result = initialize_fn.type_signature.result
+    if init_fn_result.placement != placements.SERVER:
+      raise LearningProcessPlacementError(
+          f'The result of `initialize_fn` must be placed at `SERVER` but found '
+          f'placement {init_fn_result.placement}.')
+
+    next_result_type = next_fn.type_signature.result
+    if not (isinstance(next_result_type, computation_types.StructWithPythonType)
+            and next_result_type.python_container is LearningProcessOutput):
+      raise LearningProcessOutputError(
+          f'The `next_fn` of a `LearningProcess` must return a '
+          f'`LearningProcessOutput` object, but returns {next_result_type!r}')
+
+    # We perform a more strict type check on the inputs to `next_fn` than in the
+    # base class.
+    next_fn_param = next_fn.type_signature.parameter
+    if not next_fn_param.is_struct() or len(next_fn_param) != 2:
+      raise errors.TemplateNextFnNumArgsError(
+          f'The `next_fn` must have two input arguments, but found an input '
+          f'of type {next_fn_param}.')
+    if next_fn_param[1].placement != placements.CLIENTS:
+      raise LearningProcessPlacementError(
+          f'The second input argument of `next_fn` must be placed at `CLIENTS`,'
+          f' but found placement {next_fn_param[1].placement}.')
+    if not next_fn_param[1].member.is_sequence():
+      raise LearningProcessSequenceTypeError(
+          f'The member type of the second input argument to `next_fn` must be a'
+          f' `tff.SequenceType` but found {next_fn_param[1].member} instead.')
+
+    next_fn_result = next_fn.type_signature.result
+    if next_fn_result.metrics.placement != placements.SERVER:
+      raise LearningProcessPlacementError(
+          f'The result of `next_fn` must be placed at `SERVER` but found '
+          f'placement {next_fn_result.metrics.placement} for `metrics`.')
+
+    py_typecheck.check_type(report_fn, computation_base.Computation)
+
+    report_fn_type = report_fn.type_signature
+    if report_fn_type.is_federated():
+      raise LearningProcessPlacementError(
+          f'The `report_fn` must not be a federated computation, '
+          f'but found `report_fn` with type signature:\n'
+          f'{report_fn_type}')
+
+    report_fn_param = report_fn.type_signature.parameter
+    state_type_without_placement = initialize_fn.type_signature.result.member
+    if not report_fn_param.is_assignable_from(state_type_without_placement):
+      raise ReportFnTypeSignatureError(
+          f'The input type of `report_fn` must be assignable from '
+          f'the member type of the output of `initialize_fn`, but found input '
+          f'type {report_fn_param}, which is not assignable from '
+          f'{state_type_without_placement}.')
+
+    self._report_fn = report_fn
+
+  @property
+  def initialize(self) -> computation_base.Computation:
+    """A `tff.Computation` that initializes the process.
+
+    This computation must have no input arguments, and its output must be the
+    initial state of the iterative process, placed at `SERVER`.
+
+    Returns:
+      A `tff.Computation`.
+    """
+    return super().next
+
+  @property
+  def next(self) -> computation_base.Computation:
+    """A `tff.Computation` that runs one iteration of the process.
+
+    The first argument of this computation should always be the current state
+    (originally produced by the `initialize` attribute), the second argument
+    must be a `tff.SequenceType` placed at `CLIENTS`. The return type must be
+    a `LearningProcessOutput`, with each field placed at `SERVER`.
+
+    Returns:
+      A `tff.Computation`.
+    """
+    return super().next
+
+  @property
+  def report(self) -> computation_base.Computation:
+    """A `tff.Computation` that creates a representation of the process state.
+
+    This computation should accept the unplaced member of the current state of
+    the process (originally produced by the `initialize` attribute), and returns
+    an unplaced representation of that state.
+
+    Returns:
+      A `tff.Computation`.
+    """
+    return self._report_fn
---
+++ tensorflow_federated/python/learning/learning_process_test.py
@@ -0,0 +1,273 @@
+# Copyright 2021, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import collections
+
+import tensorflow as tf
+
+from tensorflow_federated.python.core.api import computation_types
+from tensorflow_federated.python.core.api import computations
+from tensorflow_federated.python.core.api import intrinsics
+from tensorflow_federated.python.core.api import placements
+from tensorflow_federated.python.core.api import test_case
+from tensorflow_federated.python.core.templates import errors
+from tensorflow_federated.python.learning import learning_process
+
+LearningProcessOutput = learning_process.LearningProcessOutput
+
+
+@computations.federated_computation()
+def test_init_fn():
+  return intrinsics.federated_value(0, placements.SERVER)
+
+
+test_state_type = test_init_fn.type_signature.result
+
+
+@computations.tf_computation
+def sum_sequence(s):
+  spec = s.element_spec
+  return s.reduce(
+      tf.zeros(spec.shape, spec.dtype),
+      lambda s, t: tf.nest.map_structure(tf.add, s, t))
+
+
+ClientIntSequenceType = computation_types.at_clients(
+    computation_types.SequenceType(tf.int32))
+
+
+def build_next_fn(server_init_fn):
+
+  @computations.federated_computation(server_init_fn.type_signature.result,
+                                      ClientIntSequenceType)
+  def next_fn(state, client_values):
+    metrics = intrinsics.federated_map(sum_sequence, client_values)
+    metrics = intrinsics.federated_sum(metrics)
+    return LearningProcessOutput(state, metrics)
+
+  return next_fn
+
+
+def build_report_fn(server_init_fn):
+
+  @computations.tf_computation(server_init_fn.type_signature.result.member)
+  def report_fn(state):
+    return state
+
+  return report_fn
+
+
+test_next_fn = build_next_fn(test_init_fn)
+test_report_fn = build_report_fn(test_init_fn)
+
+
+class LearningProcessTest(test_case.TestCase):
+
+  def test_construction_does_not_raise(self):
+    try:
+      learning_process.LearningProcess(test_init_fn, test_next_fn,
+                                       test_report_fn)
+    except:  # pylint: disable=bare-except
+      self.fail('Could not construct a valid LearningProcess.')
+
+  def test_construction_with_empty_state_does_not_raise(self):
+
+    @computations.federated_computation()
+    def empty_initialize_fn():
+      return intrinsics.federated_value((), placements.SERVER)
+
+    next_fn = build_next_fn(empty_initialize_fn)
+    report_fn = build_report_fn(empty_initialize_fn)
+
+    try:
+      learning_process.LearningProcess(empty_initialize_fn, next_fn, report_fn)
+    except:  # pylint: disable=bare-except
+      self.fail('Could not construct a LearningProcess with empty state.')
+
+  def test_construction_with_unknown_dimension_does_not_raise(self):
+    create_empty_string = computations.tf_computation()(
+        lambda: tf.constant([], dtype=tf.string))
+    initialize_fn = computations.federated_computation()(
+        lambda: intrinsics.federated_value(create_empty_string(), placements.
+                                           SERVER))
+    next_fn = build_next_fn(initialize_fn)
+    report_fn = build_report_fn(initialize_fn)
+
+    try:
+      learning_process.LearningProcess(initialize_fn, next_fn, report_fn)
+    except:  # pylint: disable=bare-except
+      self.fail('Could not construct a LearningProcess with state type having '
+                'statically unknown shape.')
+
+  def test_init_not_tff_computation_raises(self):
+    with self.assertRaisesRegex(TypeError, r'Expected .*\.Computation, .*'):
+      init_fn = lambda: 0
+      learning_process.LearningProcess(init_fn, test_next_fn, test_report_fn)
+
+  def test_next_not_tff_computation_raises(self):
+    with self.assertRaisesRegex(TypeError, r'Expected .*\.Computation, .*'):
+      learning_process.LearningProcess(
+          initialize_fn=test_init_fn,
+          next_fn=lambda state, client_data: LearningProcessOutput(state, ()),
+          report_fn=test_report_fn)
+
+  def test_init_param_not_empty_raises(self):
+
+    @computations.federated_computation(
+        computation_types.FederatedType(tf.int32, placements.SERVER))
+    def one_arg_initialize_fn(x):
+      return x
+
+    with self.assertRaises(errors.TemplateInitFnParamNotEmptyError):
+      learning_process.LearningProcess(one_arg_initialize_fn, test_next_fn,
+                                       test_report_fn)
+
+  def test_init_state_not_assignable(self):
+    float_initialize_fn = computations.federated_computation()(lambda: 0.0)
+    with self.assertRaises(errors.TemplateStateNotAssignableError):
+      learning_process.LearningProcess(float_initialize_fn, test_next_fn,
+                                       test_report_fn)
+
+  def test_next_state_not_assignable(self):
+    float_initialize_fn = computations.federated_computation()(lambda: 0.0)
+    float_next_fn = build_next_fn(float_initialize_fn)
+    with self.assertRaises(errors.TemplateStateNotAssignableError):
+      learning_process.LearningProcess(test_init_fn, float_next_fn,
+                                       test_report_fn)
+
+  def test_init_fn_with_client_placed_state_raises(self):
+
+    init_fn = computations.federated_computation(
+        lambda: intrinsics.federated_value(0, placements.CLIENTS))
+
+    @computations.federated_computation(init_fn.type_signature.result,
+                                        ClientIntSequenceType)
+    def next_fn(state, client_values):
+      return LearningProcessOutput(state, client_values)
+
+    with self.assertRaises(learning_process.LearningProcessPlacementError):
+      learning_process.LearningProcess(init_fn, next_fn, test_report_fn)
+
+  def test_next_return_tuple_raises(self):
+
+    @computations.federated_computation(test_state_type, ClientIntSequenceType)
+    def tuple_next_fn(state, client_values):
+      metrics = intrinsics.federated_map(sum_sequence, client_values)
+      metrics = intrinsics.federated_sum(metrics)
+      return (state, metrics)
+
+    with self.assertRaises(learning_process.LearningProcessOutputError):
+      learning_process.LearningProcess(test_init_fn, tuple_next_fn,
+                                       test_report_fn)
+
+  def test_next_return_namedtuple_raises(self):
+
+    learning_process_output = collections.namedtuple('LearningProcessOutput',
+                                                     ['state', 'metrics'])
+
+    @computations.federated_computation(test_state_type, ClientIntSequenceType)
+    def namedtuple_next_fn(state, client_values):
+      metrics = intrinsics.federated_map(sum_sequence, client_values)
+      metrics = intrinsics.federated_sum(metrics)
+      return learning_process_output(state, metrics)
+
+    with self.assertRaises(learning_process.LearningProcessOutputError):
+      learning_process.LearningProcess(test_init_fn, namedtuple_next_fn,
+                                       test_report_fn)
+
+  def test_next_return_odict_raises(self):
+
+    @computations.federated_computation(test_state_type, ClientIntSequenceType)
+    def odict_next_fn(state, client_values):
+      metrics = intrinsics.federated_map(sum_sequence, client_values)
+      metrics = intrinsics.federated_sum(metrics)
+      return collections.OrderedDict(state=state, metrics=metrics)
+
+    with self.assertRaises(learning_process.LearningProcessOutputError):
+      learning_process.LearningProcess(test_init_fn, odict_next_fn,
+                                       test_report_fn)
+
+  def test_next_fn_with_one_parameter_raises(self):
+
+    @computations.federated_computation(test_state_type)
+    def next_fn(state):
+      return LearningProcessOutput(state, 0)
+
+    with self.assertRaises(errors.TemplateNextFnNumArgsError):
+      learning_process.LearningProcess(test_init_fn, next_fn, test_report_fn)
+
+  def test_next_fn_with_three_parameters_raises(self):
+
+    @computations.federated_computation(test_state_type, ClientIntSequenceType,
+                                        test_state_type)
+    def next_fn(state, client_values, second_state):  # pylint: disable=unused-argument
+      metrics = intrinsics.federated_map(sum_sequence, client_values)
+      metrics = intrinsics.federated_sum(metrics)
+      return LearningProcessOutput(state, metrics)
+
+    with self.assertRaises(errors.TemplateNextFnNumArgsError):
+      learning_process.LearningProcess(test_init_fn, next_fn, test_report_fn)
+
+  def test_next_fn_with_non_client_placed_second_arg_raises(self):
+
+    int_sequence_at_server = computation_types.FederatedType(
+        computation_types.SequenceType(tf.int32), placements.SERVER)
+
+    @computations.federated_computation(test_state_type, int_sequence_at_server)
+    def next_fn(state, server_values):
+      metrics = intrinsics.federated_map(sum_sequence, server_values)
+      return LearningProcessOutput(state, metrics)
+
+    with self.assertRaises(learning_process.LearningProcessPlacementError):
+      learning_process.LearningProcess(test_init_fn, next_fn, test_report_fn)
+
+  def test_next_fn_with_non_sequence_second_arg_raises(self):
+    ints_at_clients = computation_types.FederatedType(tf.int32,
+                                                      placements.CLIENTS)
+
+    @computations.federated_computation(test_state_type, ints_at_clients)
+    def next_fn(state, client_values):
+      metrics = intrinsics.federated_sum(client_values)
+      return LearningProcessOutput(state, metrics)
+
+    with self.assertRaises(learning_process.LearningProcessSequenceTypeError):
+      learning_process.LearningProcess(test_init_fn, next_fn, test_report_fn)
+
+  def test_next_fn_with_client_placed_metrics_result_raises(self):
+
+    @computations.federated_computation(test_state_type, ClientIntSequenceType)
+    def next_fn(state, metrics):
+      return LearningProcessOutput(state, metrics)
+
+    with self.assertRaises(learning_process.LearningProcessPlacementError):
+      learning_process.LearningProcess(test_init_fn, next_fn, test_report_fn)
+
+  def test_non_tff_computation_report_fn_raises(self):
+    report_fn = lambda x: x
+    with self.assertRaisesRegex(TypeError, r'Expected .*\.Computation, .*'):
+      learning_process.LearningProcess(test_init_fn, test_next_fn, report_fn)
+
+  def test_federated_report_fn_raises(self):
+    report_fn = computations.federated_computation(test_state_type)(lambda x: x)
+    with self.assertRaises(learning_process.ReportFnTypeSignatureError):
+      learning_process.LearningProcess(test_init_fn, test_next_fn, report_fn)
+
+  def test_report_param_not_assignable(self):
+    report_fn = computations.tf_computation(tf.float32)(lambda x: x)
+    with self.assertRaises(learning_process.ReportFnTypeSignatureError):
+      learning_process.LearningProcess(test_init_fn, test_next_fn, report_fn)
+
+
+if __name__ == '__main__':
+  test_case.main()
