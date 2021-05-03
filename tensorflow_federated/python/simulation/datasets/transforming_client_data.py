9daa78dffcf073fff19cdf94d6cd95d2050f4ed0
Galen Andrew <galenandrew@google.com>
2021-05-03 12:04:08 -0700

    TransformingClientData now works only with transformation functions that can be serialized as tf.computation.
    Also more general and less cumbersome method to split clients into pseudoclients: the user can specify a function to generate a list of pseudoclient ids for each client id. This allows, for example, differing numbers of pseudoclients for different clients.
    
    Also updates infinite_emnist to work with the new transforming_client_data.
    
    PiperOrigin-RevId: 371748160

--- tensorflow_federated/python/simulation/datasets/transforming_client_data.py
+++ tensorflow_federated/python/simulation/datasets/transforming_client_data.py
@@ -13,144 +13,96 @@
 # limitations under the License.
 """Expands ClientData by performing transformations."""
 
-import bisect
-import re
 from typing import Any, Callable, List, Optional
 
 import tensorflow as tf
 
 from tensorflow_federated.python.common_libs import py_typecheck
 from tensorflow_federated.python.simulation.datasets import client_data
+from tensorflow_federated.python.simulation.datasets import serializable_client_data
 
-CLIENT_ID_REGEX = re.compile(r'^(.*)_(\d+)$')
 
-
-class TransformingClientData(client_data.ClientData):
+class TransformingClientData(serializable_client_data.SerializableClientData):
   """Transforms client data, potentially expanding by adding pseudo-clients.
 
-  Each client of the raw_client_data is "expanded" into some number of
-  pseudo-clients. Each client ID is a string consisting of the original client
-  ID plus a concatenated integer index. For example, the raw client id
-  "client_a" might be expanded into pseudo-client ids "client_a_0", "client_a_1"
-  and "client_a_2". A function fn(x) maps datapoint x to a new datapoint,
-  where the constructor of fn is parameterized by the (raw) client_id and index
-  i. For example if x is an image, then make_transform_fn("client_a", 0)(x)
-  might be the identity, while make_transform_fn("client_a", 1)(x) could be a
-  random rotation of the image with the angle determined by a hash of "client_a"
-  and "1". Typically by convention the index 0 corresponds to the identity
-  function if the identity is supported.
+  Each client of the base_client_data is "expanded" into some number of
+  pseudo-clients. A serializable function fn(x) maps datapoint x to a new
+  datapoint, where the constructor of fn is parameterized by the expanded
+  client_id. For example if the client_id "client_A" has two expansions,
+  "client_A-0" and "client_A-1" then make_transform_fn("client_A-0")(x) might be
+  the identity, while make_transform_fn("client_A-1")(x) could be a random
+  rotation of the image with the angle determined by a hash of the string
+  "client_A-1".
   """
 
-  def __init__(self,
-               raw_client_data: client_data.ClientData,
-               make_transform_fn: Callable[[str, int], Callable[[Any], Any]],
-               num_transformed_clients: Optional[int] = None):
+  def __init__(
+      self,
+      base_client_data: serializable_client_data.SerializableClientData,
+      make_transform_fn: Callable[[str], Callable[[Any], Any]],
+      expand_client_id: Optional[Callable[[str], List[str]]] = None,
+      reduce_client_id: Optional[Callable[[str], str]] = None):
     """Initializes the TransformingClientData.
 
     Args:
-      raw_client_data: A ClientData to expand.
-      make_transform_fn: A function that returns a callable that maps datapoint
-        x to a new datapoint x'. make_transform_fn will be called as
-        make_transform_fn(raw_client_id, i) where i is an integer index, and
-        should return a function fn(x)->x. For example if x is an image, then
-        make_transform_fn("client_a", 0)(x) might be the identity, while
-        make_transform_fn("client_a", 1)(x) could be a random rotation of the
-        image with the angle determined by a hash of "client_a" and "1". If
-        transform_fn_cons returns `None`, no transformation is performed.
-        Typically by convention the index 0 corresponds to the identity function
-        if the identity is supported.
-      num_transformed_clients: The total number of transformed clients to
-        produce. If `None`, only the original clients will be transformed. If it
-        is an integer multiple k of the number of real clients, there will be
-        exactly k pseudo-clients per real client, with indices 0...k-1. Any
-        remainder g will be generated from the first g real clients and will be
-        given index k.
+      base_client_data: A ClientData to expand.
+      make_transform_fn: A function to be called as
+        `make_transform_fn(client_id)`, where `client_id` is the expanded client
+        id, which should return a function `transform_fn` that maps a datapoint
+        x whose element type structure correspondes to `base_client_data` to a
+        new datapoint x'. It must be traceable as a `tf.function`.
+      expand_client_id: An optional function that maps a client id of
+        `base_client_data` to a list of expanded client ids. If None, the
+        transformed data will have the same size and ids as the original.
+      reduce_client_id: An function that maps an expanded client id back to the
+        raw client id. Must be traceable as a `tf.function`. Must be specified
+        if and only if `expand_client_id` is.
     """
-    py_typecheck.check_type(raw_client_data, client_data.ClientData)
+    py_typecheck.check_type(base_client_data, client_data.ClientData)
     py_typecheck.check_callable(make_transform_fn)
 
-    raw_client_ids = raw_client_data.client_ids
+    raw_client_ids = base_client_data.client_ids
     if not raw_client_ids:
-      raise ValueError('`raw_client_data` must be non-empty.')
+      raise ValueError('`base_client_data` must be non-empty.')
 
-    if num_transformed_clients is None:
-      num_transformed_clients = len(raw_client_ids)
-    else:
-      py_typecheck.check_type(num_transformed_clients, int)
-      if num_transformed_clients <= 0:
-        raise ValueError('`num_transformed_clients` must be positive.')
-
-    self._raw_client_data = raw_client_data
+    self._base_client_data = base_client_data
     self._make_transform_fn = make_transform_fn
 
-    self._has_pseudo_clients = num_transformed_clients > len(raw_client_ids)
+    if (expand_client_id is None) != (reduce_client_id is None):
+      raise ValueError('Must specify both or neither of `expand_client_id` and '
+                       '`reduce_client_id`.')
 
-    if self._has_pseudo_clients:
-      num_digits = len(str(num_transformed_clients - 1))
-      format_str = '{}_{:0' + str(num_digits) + '}'
-
-      k = num_transformed_clients // len(raw_client_ids)
-      self._client_ids = []
-      for raw_client_id in raw_client_ids:
-        for i in range(k):
-          self._client_ids.append(format_str.format(raw_client_id, i))
-      num_extra_client_ids = num_transformed_clients - k * len(raw_client_ids)
-      for c in range(num_extra_client_ids):
-        self._client_ids.append(format_str.format(raw_client_ids[c], k))
-    else:
+    if expand_client_id is None:
       self._client_ids = raw_client_ids
+      # pylint: disable=unnecessary-lambda
+      self._reduce_client_id = lambda s: tf.constant(s)
+      # pylint: enable=unnecessary-lambda
+    else:
+      self._client_ids = []
+      for client_id in raw_client_ids:
+        for expanded_client_id in expand_client_id(client_id):
+          self._client_ids.append(expanded_client_id)
+      self._reduce_client_id = reduce_client_id
 
-    # Already sorted if raw_client_data.client_ids are, but just to be sure...
     self._client_ids = sorted(self._client_ids)
+
+    example_dataset = self._create_dataset(next(iter(self._client_ids)))
+    self._element_type_structure = example_dataset.element_spec
 
   @property
   def client_ids(self) -> List[str]:
     return self._client_ids
 
-  def split_client_id(self, client_id):
-    """Splits pseudo-client id into raw client id and index components.
+  def _create_dataset(self, client_id: str) -> tf.data.Dataset:
+    orig_client_id = self._reduce_client_id(client_id)
+    orig_dataset = self._base_client_data.serializable_dataset_fn(
+        orig_client_id)
+    transform = self._make_transform_fn(client_id)
+    return orig_dataset.map(transform, tf.data.experimental.AUTOTUNE)
 
-    Args:
-      client_id: The pseudo-client id.
-
-    Returns:
-      A tuple (raw_client_id, index) where raw_client_id is the string of the
-      raw client_id, and index is the integer index of the pseudo-client.
-    """
-    if not self._has_pseudo_clients:
-      return client_id, 0
-
-    py_typecheck.check_type(client_id, str)
-    match = CLIENT_ID_REGEX.search(client_id)
-    if not match:
-      raise ValueError('client_id must be a valid string from client_ids.')
-    raw_client_id = match.group(1)
-    index = int(match.group(2))
-    return raw_client_id, index
-
-  def create_tf_dataset_for_client(self, client_id: str) -> tf.data.Dataset:
-    py_typecheck.check_type(client_id, str)
-    i = bisect.bisect_left(self._client_ids, client_id)
-    if i == len(self._client_ids) or self._client_ids[i] != client_id:
-      raise ValueError('client_id must be a valid string from client_ids.')
-
-    raw_client_id, index = self.split_client_id(client_id)
-    raw_dataset = self._raw_client_data.create_tf_dataset_for_client(
-        raw_client_id)
-
-    transform_fn = self._make_transform_fn(raw_client_id, index)
-    if not transform_fn:
-      return raw_dataset
-    else:
-      py_typecheck.check_callable(transform_fn)
-      return raw_dataset.map(transform_fn, tf.data.experimental.AUTOTUNE)
+  @property
+  def serializable_dataset_fn(self):
+    return self._create_dataset
 
   @property
   def element_type_structure(self):
-    return self._raw_client_data.element_type_structure
-
-  @property
-  def dataset_computation(self):
-    raise NotImplementedError('TranscormingClientData contains non-TensorFlow '
-                              'logic and is currently incompatible with '
-                              'dataset_computation.')
+    return self._element_type_structure
