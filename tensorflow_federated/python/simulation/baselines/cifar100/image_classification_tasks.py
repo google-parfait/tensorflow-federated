fc15806da2ba5dd1a993b71a5095834870424809
Zachary Charles <zachcharles@google.com>
2021-06-28 23:53:39 +0000

    Refactor CIFAR100 task for greater modularity.
    PiperOrigin-RevId: 381974565

--- tensorflow_federated/python/simulation/baselines/cifar100/image_classification_tasks.py
+++ tensorflow_federated/python/simulation/baselines/cifar100/image_classification_tasks.py
@@ -26,6 +26,7 @@ from tensorflow_federated.python.simulation.baselines import task_data
 from tensorflow_federated.python.simulation.baselines.cifar100 import image_classification_preprocessing
 from tensorflow_federated.python.simulation.baselines.cifar100 import resnet_models
 from tensorflow_federated.python.simulation.datasets import cifar100
+from tensorflow_federated.python.simulation.datasets import client_data
 
 
 class ResnetModel(enum.Enum):
@@ -66,6 +67,77 @@ def _get_resnet_model(model_id: Union[str, ResnetModel],
     raise ValueError('The model id must be one of {}, found {}'.format(
         _RESNET_MODELS, model_enum))
   return keras_model_fn(input_shape=input_shape, num_classes=_NUM_CLASSES)
+
+
+def create_image_classification_task_with_datasets(
+    train_client_spec: client_spec.ClientSpec,
+    eval_client_spec: Optional[client_spec.ClientSpec],
+    model_id: Union[str, ResnetModel],
+    crop_height: int,
+    crop_width: int,
+    train_data: client_data.ClientData,
+    test_data: client_data.ClientData,
+) -> baseline_task.BaselineTask:
+  """Creates a baseline task for image classification on CIFAR-100.
+
+  Args:
+    train_client_spec: A `tff.simulation.baselines.ClientSpec` specifying how to
+      preprocess train client data.
+    eval_client_spec: An optional `tff.simulation.baselines.ClientSpec`
+      specifying how to preprocess evaluation client data. If set to `None`, the
+      evaluation datasets will use a batch size of 64 with no extra
+      preprocessing.
+    model_id: A string identifier for a digit recognition model. Must be one of
+      `resnet18`, `resnet34`, `resnet50`, `resnet101` and `resnet152. These
+      correspond to various ResNet architectures. Unlike standard ResNet
+      architectures though, the batch normalization layers are replaced with
+      group normalization.
+    crop_height: An integer specifying the desired height for cropping images.
+      Must be between 1 and 32 (the height of uncropped CIFAR-100 images). By
+      default, this is set to
+      `tff.simulation.baselines.cifar100.DEFAULT_CROP_HEIGHT`.
+    crop_width: An integer specifying the desired width for cropping images.
+      Must be between 1 and 32 (the width of uncropped CIFAR-100 images). By
+      default this is set to
+      `tff.simulation.baselines.cifar100.DEFAULT_CROP_WIDTH`.
+    train_data: A `tff.simulation.datasets.ClientData` used for training.
+    test_data: A `tff.simulation.datasets.ClientData` used for testing.
+
+  Returns:
+    A `tff.simulation.baselines.BaselineTask`.
+  """
+  if crop_height < 1 or crop_width < 1 or crop_height > 32 or crop_width > 32:
+    raise ValueError('The crop_height and crop_width must be between 1 and 32.')
+  crop_shape = (crop_height, crop_width, 3)
+
+  if eval_client_spec is None:
+    eval_client_spec = client_spec.ClientSpec(
+        num_epochs=1, batch_size=64, shuffle_buffer_size=1)
+
+  train_preprocess_fn = image_classification_preprocessing.create_preprocess_fn(
+      train_client_spec, crop_shape=crop_shape)
+  eval_preprocess_fn = image_classification_preprocessing.create_preprocess_fn(
+      eval_client_spec, crop_shape=crop_shape)
+
+  task_datasets = task_data.BaselineTaskDatasets(
+      train_data=train_data,
+      test_data=test_data,
+      validation_data=None,
+      train_preprocess_fn=train_preprocess_fn,
+      eval_preprocess_fn=eval_preprocess_fn)
+
+  keras_model = _get_resnet_model(model_id, crop_shape)
+  loss = tf.keras.losses.SparseCategoricalCrossentropy()
+  metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
+
+  def model_fn() -> model.Model:
+    return keras_utils.from_keras_model(
+        keras_model=keras_model,
+        loss=loss,
+        input_spec=task_datasets.element_type_structure,
+        metrics=metrics)
+
+  return baseline_task.BaselineTask(task_datasets, model_fn)
 
 
 def create_image_classification_task(
@@ -110,10 +182,6 @@ def create_image_classification_task(
   Returns:
     A `tff.simulation.baselines.BaselineTask`.
   """
-  if crop_height < 1 or crop_width < 1 or crop_height > 32 or crop_width > 32:
-    raise ValueError('The crop_height and crop_width must be between 1 and 32.')
-  crop_shape = (crop_height, crop_width, 3)
-
   if use_synthetic_data:
     synthetic_data = cifar100.get_synthetic()
     cifar_train = synthetic_data
@@ -121,31 +189,8 @@ def create_image_classification_task(
   else:
     cifar_train, cifar_test = cifar100.load_data(cache_dir=cache_dir)
 
-  if eval_client_spec is None:
-    eval_client_spec = client_spec.ClientSpec(
-        num_epochs=1, batch_size=64, shuffle_buffer_size=1)
-
-  train_preprocess_fn = image_classification_preprocessing.create_preprocess_fn(
-      train_client_spec, crop_shape=crop_shape)
-  eval_preprocess_fn = image_classification_preprocessing.create_preprocess_fn(
-      eval_client_spec, crop_shape=crop_shape)
-
-  task_datasets = task_data.BaselineTaskDatasets(
-      train_data=cifar_train,
-      test_data=cifar_test,
-      validation_data=None,
-      train_preprocess_fn=train_preprocess_fn,
-      eval_preprocess_fn=eval_preprocess_fn)
-
-  keras_model = _get_resnet_model(model_id, crop_shape)
-  loss = tf.keras.losses.SparseCategoricalCrossentropy()
-  metrics = [tf.keras.metrics.SparseCategoricalAccuracy()]
-
-  def model_fn() -> model.Model:
-    return keras_utils.from_keras_model(
-        keras_model=keras_model,
-        loss=loss,
-        input_spec=task_datasets.element_type_structure,
-        metrics=metrics)
-
-  return baseline_task.BaselineTask(task_datasets, model_fn)
+  return create_image_classification_task_with_datasets(train_client_spec,
+                                                        eval_client_spec,
+                                                        model_id, crop_height,
+                                                        crop_width, cifar_train,
+                                                        cifar_test)
