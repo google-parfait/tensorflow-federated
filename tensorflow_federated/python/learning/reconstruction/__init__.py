64bd2da45feb30cb14edf7d0c0ed96dcf7bc35f2
Karan Singhal <karansinghal@google.com>
2021-07-07 11:02:29 -0700

    Add __init__ symbols and visibility for learning/reconstruction.
    PiperOrigin-RevId: 383450323

--- tensorflow_federated/python/learning/reconstruction/__init__.py
+++ tensorflow_federated/python/learning/reconstruction/__init__.py
@@ -12,3 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Libraries for using Federated Reconstruction algorithms."""
+
+from tensorflow_federated.python.learning.reconstruction.evaluation_computation import build_federated_evaluation
+from tensorflow_federated.python.learning.reconstruction.keras_utils import from_keras_model
+from tensorflow_federated.python.learning.reconstruction.model import BatchOutput
+from tensorflow_federated.python.learning.reconstruction.model import Model
+from tensorflow_federated.python.learning.reconstruction.reconstruction_utils import build_dataset_split_fn
+from tensorflow_federated.python.learning.reconstruction.reconstruction_utils import DatasetSplitFn
+from tensorflow_federated.python.learning.reconstruction.reconstruction_utils import get_global_variables
+from tensorflow_federated.python.learning.reconstruction.reconstruction_utils import get_local_variables
+from tensorflow_federated.python.learning.reconstruction.reconstruction_utils import simple_dataset_split_fn
+from tensorflow_federated.python.learning.reconstruction.training_process import build_training_process
+from tensorflow_federated.python.learning.reconstruction.training_process import ClientOutput
