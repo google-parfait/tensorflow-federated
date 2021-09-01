2f6cc691be0da3eca5b935c9c4fb2660d8e1b09f
Michael Reneer <michaelreneer@google.com>
2021-09-01 15:27:14 -0700

    Update to use TF version `2.6.0`.
    PiperOrigin-RevId: 394327712

--- tensorflow_federated/tools/python_package/setup.py
+++ tensorflow_federated/tools/python_package/setup.py
@@ -70,7 +70,7 @@ REQUIRED_PACKAGES = [
     'semantic-version~=2.8.5',
     'tensorflow-model-optimization~=0.5.0',
     'tensorflow-privacy~=0.7.2',
-    'tensorflow~=2.5.0',
+    'tensorflow~=2.6.0',
     'tqdm~=4.28.1',
 ]
 
