0c0ddfc3d64ecb561e262e0d2778e54390401169
Keith Rush <krush@google.com>
2021-09-16 17:22:32 -0700

    Adds ConcreteComputation to TFF framework.
    PiperOrigin-RevId: 397205065

--- tensorflow_federated/python/core/framework/__init__.py
+++ tensorflow_federated/python/core/framework/__init__.py
@@ -16,6 +16,7 @@
 from tensorflow_federated.python.core.impl.compiler.building_blocks import ComputationBuildingBlock
 from tensorflow_federated.python.core.impl.compiler.intrinsic_reductions import replace_intrinsics_with_bodies
 from tensorflow_federated.python.core.impl.compiler.tree_to_cc_transformations import TFParser
+from tensorflow_federated.python.core.impl.computation.computation_impl import ConcreteComputation
 from tensorflow_federated.python.core.impl.computation.computation_serialization import deserialize_computation
 from tensorflow_federated.python.core.impl.computation.computation_serialization import serialize_computation
 from tensorflow_federated.python.core.impl.context_stack.context_base import Context
