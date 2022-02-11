62560b8ad2dc96fabcefcc7a76318f29be2210dd
Michael Reneer <michaelreneer@google.com>
2022-02-11 15:20:47 -0800

    Automated rollback of commit a366c08105505f9c48379bdf8aae1dccd43e978f
    PiperOrigin-RevId: 428102397

--- tensorflow_federated/python/learning/algorithms/__init__.py
+++ tensorflow_federated/python/learning/algorithms/__init__.py
@@ -20,5 +20,3 @@ from tensorflow_federated.python.learning.algorithms.fed_prox import build_unwei
 from tensorflow_federated.python.learning.algorithms.fed_prox import build_weighted_fed_prox
 from tensorflow_federated.python.learning.algorithms.fed_sgd import build_fed_sgd
 from tensorflow_federated.python.learning.algorithms.kmeans_clustering import build_fed_kmeans
-from tensorflow_federated.python.learning.algorithms.mime import build_unweighted_mime_lite
-from tensorflow_federated.python.learning.algorithms.mime import build_weighted_mime_lite
