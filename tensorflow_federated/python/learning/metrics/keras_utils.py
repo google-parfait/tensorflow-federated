bbd21493af146a1a4916f44b2edc7ecd7ae20463
Zachary Garrett <zachgarrett@google.com>
2022-04-27 18:26:30 -0700

    Create an API to convert `tf.keras.metrics.Metric` to a set of pure `tf.functions.
    PiperOrigin-RevId: 445021922

---
+++ tensorflow_federated/python/learning/metrics/keras_utils.py
@@ -0,0 +1,185 @@
+# Copyright 2022, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# pytype: skip-file
+# This modules disables the Pytype analyzer, see
+# https://github.com/tensorflow/federated/blob/main/docs/pytype.md for more
+# information.
+"""Module for Keras metrics integration."""
+
+import collections
+import functools
+import itertools
+from typing import Any, Callable, OrderedDict, Tuple, TypeVar, Union
+
+import tensorflow as tf
+
+from tensorflow_federated.python.tensorflow_libs import variable_utils
+
+StateVar = TypeVar('StateVar')
+MetricConstructor = Callable[[], tf.keras.metrics.Metric]
+MetricConstructors = OrderedDict[str, MetricConstructor]
+MetricStructure = OrderedDict[str, tf.keras.metrics.Metric]
+MetricsConstructor = Callable[[], MetricStructure]
+
+
+def create_functional_metric_fns(
+    metrics_constructor: Union[MetricConstructor, MetricsConstructor,
+                               MetricConstructors]
+) -> Tuple[Callable[[], StateVar], Callable[[StateVar, ...], StateVar],
+           Callable[[StateVar], Any]]:
+  """Turn a Keras metric construction method into a tuple of pure functions.
+
+  This can be used to convert Keras metrics for use in
+  `tff.learning.models.FunctionalModel`. The method traces the metric logic into
+  three `tf.function` with explicit `state` parameters that replace the
+  closure over internal `tf.Variable` of the `tf.keras.metrics.Metric`.
+
+  IMPORTANT: Only metrics whose `tf.keras.metrics.Metric.update_state` method
+  take two arguments (`y_true` and `y_pred`) are supported.
+
+  Example:
+
+    >>> metric = tf.keras.metrics.Accuracy()
+    >>> metric.update_state([1.0, 1.0], [0.0, 1.0])
+    >>> metric.result()  # == 0.5
+    >>>
+    >>> metric_fns = tff.learning.metrics.create_functional_metric_fns(
+    >>>    tf.keras.metrics.Accuracy)
+    >>> initialize, update, finalize = metric_fns
+    >>> state = initialize()
+    >>> state = update(state, [1.0, 1.0], [0.0, 1.0])
+    >>> finalize(state)  # == 0.5
+
+  Args:
+    metrics_constructor: Either a no-arg callable that returns a
+      `tf.keras.metrics.Metric` or an `OrderedDict` of `str` names to
+      `tf.keras.metrics.Metric`, or `OrderedDict` of no-arg callables returning
+      `tf.keras.metrics.Metric` instances.  The no-arg callables can be the
+      metric class itself (e.g.  `tf.keras.metrics.Accuracy`) in which case the
+      default metric configuration will be used. It also supports lambdas or
+      `functools.partial` to provide alternate metric configurations.
+
+  Returns:
+    A 3-tuple of `tf.function`s namely `(initialize, update, finalize)`.
+    `initialize` is a no-arg function used to create the algrebraic "zero"
+    before reducing the metric over batches of examples. `update` is a function
+    with the same signature as the `metric_constructor().update_state` method
+    with a preprended `state` argumetn first, and is used to add an observation
+    to the metric. `finalize` only takes a `state` argument and returns the
+    final metric value based on observations previously added.
+
+  Raises:
+    TypeError: If `metrics_constructor` is not a callable or `OrderedDict`, or
+      if `metrics_constructor` is a callable returning values of the wrong type.
+  """
+  if isinstance(metrics_constructor, collections.OrderedDict):
+    metrics_constructor = functools.partial(tf.nest.map_structure,
+                                            lambda m: m(), metrics_constructor)
+  if not callable(metrics_constructor):
+    raise TypeError('`metrics_constructor` must be a callable or '
+                    f'`collections.OrderedDict`. Got {metrics_constructor!r} '
+                    f'which is a {type(metrics_constructor)!r}.')
+
+  # Eagerly validate that the metrics_constructor returns values that
+  # have the expected properties to provide better debugging messages to caller.
+  def check_keras_metric_type(obj):
+    if not isinstance(obj, tf.keras.metrics.Metric):
+      raise TypeError('Found non-tf.keras.metrics.Metric value: '
+                      f'{type(obj)}: {obj!r}.')
+    try:
+      obj.update_state(
+          y_true=tf.zeros([1], obj.dtype), y_pred=tf.zeros([1], obj.dtype))
+    except TypeError as e:
+      raise TypeError('Only tf.keras.metrics.Metric with an `update_state` '
+                      'taking a `y_true` and a `y_pred` argument are supported.'
+                      'Examples include `Accuracy` and `Precision`, where as '
+                      '`Sum` is not supported') from e
+
+  try:
+    metrics_structure = metrics_constructor()
+    tf.nest.map_structure(check_keras_metric_type, metrics_structure)
+  except ValueError as e:
+    raise ValueError(
+        '`metrics_constructor` must return a `tf.keras.metrics.Metric` '
+        'instance, or an OrderedDict of string to keras metrics.') from e
+  if isinstance(metrics_structure, collections.OrderedDict):
+    non_string_keys = [
+        name for name in metrics_structure.keys() if not isinstance(name, str)
+    ]
+    if non_string_keys:
+      raise TypeError('`metrics_constructor` must return an `OrdredDict` keyed '
+                      f'by `str`. Got keys {non_string_keys} that were not '
+                      'type `str`.')
+  del metrics_structure
+
+  # IMPORTANT: the following code relies on the order of the `tf.Variable`s in
+  # `tf.keras.metrics.Metric.variables` to match the order that they are created
+  # at runtime. If this changes,
+  # `build_replace_variable_with_parameter_creator` will yield the wrong
+  # parameters in `update` and `finalize` calls.
+
+  @tf.function
+  def initialize():
+    with tf.variable_creator_scope(variable_utils.create_tensor_variable):
+      return tf.nest.map_structure(lambda m: m.variables, metrics_constructor())
+
+  def build_replace_variable_with_parameter_creator(parameters):
+    """Create a creation function that replaces variables with parameters.
+
+    Args:
+      parameters: Either a list of `tf.function` parameters ordered in the same
+        order that `tf.Variable` will be created, or a `collections.OrderedDict`
+        whose depth-first traversal matches the order of `tf.Variable` creation.
+
+    Returns:
+      A callable that can be used in a `tf.variable_creator_scope` to replace
+      `tf.Variable` creation with `tf.function` parameters.
+    """
+    if isinstance(parameters, collections.OrderedDict):
+      parameter_iterator = itertools.chain(*parameters.values())
+    elif isinstance(parameters, list):
+      parameter_iterator = iter(parameters)
+    else:
+      raise TypeError('Internal coding error: `parameters` must be a `list` or '
+                      f'`OrderedDict` type, got {type(parameters)}.')
+
+    def creator_fn(next_creator_fn, **kwargs):
+      del next_creator_fn  # Unused.
+      kwargs.pop('initial_value')
+      return variable_utils.TensorVariable(
+          initial_value=next(parameter_iterator), **kwargs)
+
+    return creator_fn
+
+  @tf.function
+  def update(state, y_true, y_pred, sample_weight=None):
+    del sample_weight  # Unused.
+
+    def update(metric):
+      metric.update_state(y_true, y_pred, sample_weight=None)
+      return metric.variables
+
+    with tf.variable_creator_scope(
+        build_replace_variable_with_parameter_creator(state)):
+      return tf.nest.map_structure(update, metrics_constructor())
+
+  @tf.function
+  def finalize(state):
+    with tf.variable_creator_scope(
+        build_replace_variable_with_parameter_creator(state)):
+      return tf.nest.map_structure(lambda metric: metric.result(),
+                                   metrics_constructor())
+
+  return initialize, update, finalize
---
+++ tensorflow_federated/python/learning/metrics/keras_utils_test.py
@@ -0,0 +1,211 @@
+# Copyright 2022, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import collections
+import itertools
+
+from absl.testing import parameterized
+import numpy as np
+import tensorflow as tf
+
+from tensorflow_federated.python.learning.metrics import counters
+from tensorflow_federated.python.learning.metrics import keras_utils
+
+
+class CreateFunctionalMetricTest(tf.test.TestCase, parameterized.TestCase):
+
+  def assertAllEqual(self, a, b, msg=None):
+    # assertAllEqual fails to walk nested structures nicely, but assertAllClose
+    # does, so we can simply set the tolerance to 0.0 to achieve equality tests.
+    self.assertAllClose(a, b, msg=msg, rtol=0.0, atol=0.0)
+
+  def assert_no_variable_ops(self, graph_def):
+    all_nodes = itertools.chain(
+        graph_def.node, *[f.node_def for f in graph_def.library.function])
+    self.assertEmpty([node.op for node in all_nodes if 'Variable' in node.op])
+
+  def assert_binary_metric_variableless_functions(self, initialize_fn,
+                                                  update_fn, finalize_fn):
+    self.assert_no_variable_ops(
+        initialize_fn.get_concrete_function().graph.as_graph_def())
+    state_spec = tf.nest.map_structure(tf.TensorSpec.from_tensor,
+                                       initialize_fn())
+    self.assert_no_variable_ops(
+        update_fn.get_concrete_function(
+            state=state_spec,
+            y_true=tf.TensorSpec(shape=[None], dtype=tf.float32),
+            y_pred=tf.TensorSpec(shape=[None], dtype=tf.float32),
+        ).graph.as_graph_def())
+    self.assert_no_variable_ops(
+        finalize_fn.get_concrete_function(
+            state=state_spec).graph.as_graph_def())
+
+  @parameterized.named_parameters(
+      ('NumBatchesCounter', counters.NumBatchesCounter),
+      ('NumExamplesCounter', counters.NumExamplesCounter),
+  )
+  def test_custom_tff_metrics(self, metric_constructor):
+    metric = metric_constructor()
+    initialize, update, finalize = keras_utils.create_functional_metric_fns(
+        metric_constructor)
+    self.assert_binary_metric_variableless_functions(initialize, update,
+                                                     finalize)
+    state = initialize()
+    self.assertAllEqual(metric.variables, state)
+    predictions = np.asarray([0.0, 1.0])
+    labels = np.asarray([1.0, 1.0])
+    metric.update_state(y_pred=predictions, y_true=labels)
+    state = update(state, y_pred=predictions, y_true=labels)
+    self.assertAllEqual(metric.variables, state)
+    self.assertAllEqual(metric.result(), finalize(state))
+    predictions = np.asarray([0.0, 1.0, 0.0])
+    labels = np.asarray([1.0, 1.0, 0.0])
+    metric.update_state(y_pred=predictions, y_true=labels)
+    state = update(state, y_pred=predictions, y_true=labels)
+    self.assertAllEqual(metric.variables, state)
+    self.assertAllEqual(metric.result(), finalize(state))
+
+  @parameterized.named_parameters(
+      # pylint: disable=g-complex-comprehension
+      (name, getattr(tf.keras.metrics, name)) for name in [
+          'Accuracy',
+          'BinaryAccuracy',
+          'BinaryCrossentropy',
+          'CategoricalAccuracy',
+          'CategoricalCrossentropy',
+          'CategoricalHinge',
+          'CosineSimilarity',
+          'FalseNegatives',
+          'FalsePositives',
+          'KLDivergence',
+          'MeanAbsoluteError',
+          'MeanSquaredError',
+      ]
+      # pylint: enable=g-complex-comprehension
+  )
+  def test_binary_metrics(self, metric_constructor):
+    metric = metric_constructor()
+    initialize, update, finalize = keras_utils.create_functional_metric_fns(
+        metric_constructor)
+    self.assert_binary_metric_variableless_functions(initialize, update,
+                                                     finalize)
+    state = initialize()
+    self.assertAllEqual(metric.variables, state)
+    predictions = [0.0, 1.0]
+    labels = [1.0, 1.0]
+    metric.update_state(y_pred=predictions, y_true=labels)
+    state = update(state, y_pred=predictions, y_true=labels)
+    self.assertAllEqual(metric.variables, state)
+    self.assertAllEqual(metric.result(), finalize(state))
+    predictions = [0.0, 1.0, 0.0]
+    labels = [1.0, 1.0, 0.0]
+    metric.update_state(y_pred=predictions, y_true=labels)
+    state = update(state, y_pred=predictions, y_true=labels)
+    self.assertAllEqual(metric.variables, state)
+    self.assertAllEqual(metric.result(), finalize(state))
+
+  def test_non_default_metric(self):
+
+    def metric_constructor():
+      return tf.keras.metrics.Precision(thresholds=[0.25, 0.5, 0.75])
+
+    metric = metric_constructor()
+    initialize, update, finalize = keras_utils.create_functional_metric_fns(
+        metric_constructor)
+    state = initialize()
+    self.assertAllEqual(metric.variables, state)
+    predictions = [0.0, 1.0]
+    labels = [1.0, 1.0]
+    metric.update_state(y_pred=predictions, y_true=labels)
+    state = update(state, y_pred=predictions, y_true=labels)
+    self.assertAllEqual(metric.variables, state)
+    self.assertAllEqual(metric.result(), finalize(state))
+    predictions = [0.0, 1.0, 0.0]
+    labels = [1.0, 1.0, 0.0]
+    metric.update_state(y_pred=predictions, y_true=labels)
+    state = update(state, y_pred=predictions, y_true=labels)
+    self.assertAllEqual(metric.variables, state)
+    self.assertAllEqual(metric.result(), finalize(state))
+
+  def test_multiple_metrics_constructor(self):
+
+    def metrics_constructor():
+      return collections.OrderedDict(
+          precisions=tf.keras.metrics.Precision(thresholds=[0.25, 0.5, 0.75]),
+          accuracy=tf.keras.metrics.Accuracy())
+
+    metrics = metrics_constructor()
+    initialize, update, finalize = keras_utils.create_functional_metric_fns(
+        metrics_constructor)
+    state = initialize()
+    self.assertAllEqual(
+        tf.nest.map_structure(lambda m: m.variables, metrics), state)
+    predictions = [0.0, 1.0]
+    labels = [1.0, 1.0]
+    tf.nest.map_structure(
+        lambda m: m.update_state(y_pred=predictions, y_true=labels), metrics)
+    state = update(state, y_pred=predictions, y_true=labels)
+    self.assertAllEqual(
+        tf.nest.map_structure(lambda m: m.variables, metrics), state)
+    self.assertAllEqual(
+        tf.nest.map_structure(lambda m: m.result(), metrics), finalize(state))
+    predictions = [0.0, 1.0, 0.0]
+    labels = [1.0, 1.0, 0.0]
+    tf.nest.map_structure(
+        lambda m: m.update_state(y_pred=predictions, y_true=labels), metrics)
+    state = update(state, y_pred=predictions, y_true=labels)
+    self.assertAllEqual(
+        tf.nest.map_structure(lambda m: m.variables, metrics), state)
+    self.assertAllEqual(
+        tf.nest.map_structure(lambda m: m.result(), metrics), finalize(state))
+
+  @parameterized.named_parameters(
+      # pylint: disable=g-complex-comprehension
+      (name, getattr(tf.keras.metrics, name)) for name in [
+          'Sum',
+          'Mean',
+      ]
+      # pylint: enable=g-complex-comprehension
+  )
+  def test_unary_metrics(self, metric_constructor):
+    with self.assertRaises(TypeError):
+      keras_utils.create_functional_metric_fns(metric_constructor)
+
+  def test_composite_metrics_fn(self):
+    initialize, update, finalize = keras_utils.create_functional_metric_fns(
+        collections.OrderedDict(
+            accuracy=tf.keras.metrics.Accuracy,
+            precision_at_5=lambda: tf.keras.metrics.Precision(thresholds=[0.5]))
+    )
+    metrics_by_name = collections.OrderedDict(
+        accuracy=tf.keras.metrics.Accuracy(),
+        precision_at_5=tf.keras.metrics.Precision(thresholds=[0.5]))
+    state = initialize()
+    self.assertAllClose(
+        state, tf.nest.map_structure(lambda m: m.variables, metrics_by_name))
+    predictions = [0.25, 0.5, 1.0]
+    labels = [0.0, 0.0, 1.0]
+    state = update(state, y_true=labels, y_pred=predictions)
+    tf.nest.map_structure(
+        lambda m: m.update_state(y_true=labels, y_pred=predictions),
+        metrics_by_name)
+    self.assertAllClose(
+        state, tf.nest.map_structure(lambda m: m.variables, metrics_by_name))
+    finalized_metrics = finalize(state)
+    metric_result = tf.nest.map_structure(lambda m: m.result(), metrics_by_name)
+    self.assertAllClose(finalized_metrics, metric_result)
+
+
+if __name__ == '__main__':
+  tf.test.main()
