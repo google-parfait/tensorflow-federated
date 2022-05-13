af74f3e57f00527d937a075012c72b7855567d96
Keith Rush <krush@google.com>
2022-05-13 09:54:03 -0700

    Adds logging of expected sequential computations to local TFF-C++ runtime.
    PiperOrigin-RevId: 448517906

--- tensorflow_federated/python/core/impl/executor_stacks/cpp_executor_factory.py
+++ tensorflow_federated/python/core/impl/executor_stacks/cpp_executor_factory.py
@@ -18,8 +18,10 @@
 # information.
 """A collection of constructors for basic types of executor stacks."""
 
+import math
 from typing import Optional, Sequence
 
+from absl import logging
 import cachetools
 
 from tensorflow_federated.python.common_libs import py_typecheck
@@ -27,6 +29,12 @@ from tensorflow_federated.python.core.impl.executor_stacks import executor_stack
 from tensorflow_federated.python.core.impl.executors import executor_bindings
 from tensorflow_federated.python.core.impl.executors import executor_factory
 from tensorflow_federated.python.core.impl.types import placements
+
+
+# Users likely do not intend to run 4 or more TensorFlow functions sequentially;
+# we special-case to warn users explicitly in this case, in addition to
+# logging in the case of any implied sequential execution.
+_CONCURRENCY_LEVEL_TO_WARN = 4
 
 
 def _get_hashable_key(cardinalities: executor_factory.CardinalitiesType):
@@ -53,6 +61,29 @@ class CPPExecutorFactory(executor_factory.ExecutorFactory):
     self._executors = cachetools.LRUCache(self._cache_size)
 
 
+def _log_and_warn_on_sequential_execution(max_concurrent_computation_calls: int,
+                                          num_clients: int,
+                                          expected_concurrency_factor: int):
+  """Logs warnings that users may be using the runtime in an unexpected way."""
+
+  if expected_concurrency_factor >= _CONCURRENCY_LEVEL_TO_WARN:
+    logging.warning(
+        'Running %s clients with max concurrency %s will result in significant '
+        'serialization of execution; running %s TensorFlow functions '
+        'sequentially. This invocation could benefit significantly from more '
+        'resources (e.g. more GPUs), or moving to TFF\'s distributed runtime.')
+  else:
+    logging.info(
+        'TFF-C++ local executor configured to maximally run %s '
+        'calls into TensorFlow in  parallel; asked to run %s '
+        'clients. This will result in %s invocations running '
+        'sequentially, indicating that this invocation will run '
+        'faster when equipped with increased resources or invoked '
+        'against the distributed TFF runtime.',
+        max_concurrent_computation_calls, num_clients,
+        expected_concurrency_factor)
+
+
 def local_cpp_executor_factory(
     default_num_clients: int = 0,
     max_concurrent_computation_calls: Optional[int] = None
@@ -65,6 +96,13 @@ def local_cpp_executor_factory(
   ) -> executor_bindings.Executor:
     if cardinalities.get(placements.CLIENTS) is None:
       cardinalities[placements.CLIENTS] = default_num_clients
+    num_clients = cardinalities[placements.CLIENTS]
+    if max_concurrent_computation_calls is not None and num_clients > max_concurrent_computation_calls:
+      expected_concurrency_factor = math.ceil(num_clients /
+                                              max_concurrent_computation_calls)
+      _log_and_warn_on_sequential_execution(max_concurrent_computation_calls,
+                                            num_clients,
+                                            expected_concurrency_factor)
     tf_executor = executor_bindings.create_tensorflow_executor(
         max_concurrent_computation_calls)
     sub_federating_reference_resolving_executor = executor_bindings.create_reference_resolving_executor(
