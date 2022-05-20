167a2b6c467da6e2502dce232e28117f574c3c56
Michael Reneer <michaelreneer@google.com>
2022-05-20 14:17:12 -0700

    Move computation documentation from the `api` package to the symbol.
    This change mirrors cl/449806375
    
    PiperOrigin-RevId: 450061608

--- tensorflow_federated/experimental/python/core/api/computations.py
+++ tensorflow_federated/experimental/python/core/api/computations.py
@@ -16,24 +16,3 @@
 from tensorflow_federated.experimental.python.core.impl.wrappers import computation_wrapper_instances
 
 jax_computation = computation_wrapper_instances.jax_wrapper
-jax_computation.__doc__ = (
-    """Decorates/wraps Python functions containing JAX code as TFF computations.
-
-  This wrapper can be used in a similar manner to `tff.tf_computation`, with
-  exception of the following:
-
-  * The code in the wrapped Python function must be JAX code that can be
-    compiled to XLA (e.g., code that one would expect to be able to annotate
-    with `@jax.jit`).
-
-  * The inputs and outputs must be tensors, or (possibly recursively) nested
-    structures of tensors. Sequences are currently not supported.
-
-  Example:
-
-  ```
-  @tff.experimental.jax_computation(tf.int32)
-  def comp(x):
-    return jax.numpy.add(x, np.int32(10))
-  ```
-  """)
