6c7d92187950f98cab4f80703b7fe80860068331
Hardik Vala <hardikv@google.com>
2022-06-01 18:04:09 -0700

    Allow public import of CreateDataDescriptor.
    PiperOrigin-RevId: 452427512

--- tensorflow_federated/python/core/framework/__init__.py
+++ tensorflow_federated/python/core/framework/__init__.py
@@ -30,6 +30,7 @@ from tensorflow_federated.python.core.impl.executors.cardinalities_utils import 
 from tensorflow_federated.python.core.impl.executors.cardinality_carrying_base import CardinalityCarrying
 from tensorflow_federated.python.core.impl.executors.data_backend_base import DataBackend
 from tensorflow_federated.python.core.impl.executors.data_descriptor import CardinalityFreeDataDescriptor
+from tensorflow_federated.python.core.impl.executors.data_descriptor import CreateDataDescriptor
 from tensorflow_federated.python.core.impl.executors.data_descriptor import DataDescriptor
 from tensorflow_federated.python.core.impl.executors.data_executor import DataExecutor
 from tensorflow_federated.python.core.impl.executors.eager_tf_executor import EagerTFExecutor
