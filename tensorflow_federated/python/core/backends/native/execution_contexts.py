37f6ae2b72b1bb844295a4ac1986367d464072d2
Keith Rush <krush@google.com>
2022-06-09 16:30:19 -0700

    Adds a localhost-server managing execution context.
    This context can be used to execute code against a locally-hosted TFF C++ runtime, as in the test_contexts implementation.
    
    PiperOrigin-RevId: 454039171

--- tensorflow_federated/python/core/backends/native/execution_contexts.py
+++ tensorflow_federated/python/core/backends/native/execution_contexts.py
@@ -14,9 +14,16 @@
 """Execution contexts for the native backend."""
 
 from concurrent import futures
-from typing import List, Optional, Sequence
+import os
+import signal
+import subprocess
+import sys
+import time
+from typing import List, Optional, Sequence, Tuple
 
+from absl import logging
 import grpc
+import portpicker
 
 from tensorflow_federated.python.core.backends.native import compiler
 from tensorflow_federated.python.core.backends.native import mergeable_comp_compiler
@@ -26,6 +33,11 @@ from tensorflow_federated.python.core.impl.execution_contexts import mergeable_c
 from tensorflow_federated.python.core.impl.execution_contexts import sync_execution_context
 from tensorflow_federated.python.core.impl.executor_stacks import python_executor_stacks
 from tensorflow_federated.python.core.impl.executors import executor_factory
+from tensorflow_federated.python.core.impl.executors import remote_executor
+from tensorflow_federated.python.core.impl.executors import remote_executor_grpc_stub
+from tensorflow_federated.python.core.impl.types import placements
+
+_LOCALHOST_SERVER_WAIT_TIME_SEC = 1.
 
 
 def _make_basic_python_execution_context(*, executor_fn, compiler_fn,
@@ -260,3 +272,111 @@ def set_mergeable_comp_execution_context(
   context = create_mergeable_comp_execution_context(
       executor_factories=executor_factories)
   context_stack_impl.context_stack.set_default_context(context)
+
+
+def set_localhost_server_cpp_execution_context(
+    binary_path: str,
+    default_num_clients: int = 0,
+    max_concurrent_computation_calls: int = 1,
+):
+  """Sets default context to a localhost TFF executor."""
+  context = create_localhost_server_cpp_execution_context(
+      binary_path=binary_path,
+      default_num_clients=default_num_clients,
+      max_concurrent_computation_calls=max_concurrent_computation_calls,
+  )
+  context_stack_impl.context_stack.set_default_context(context)
+
+
+def create_localhost_server_cpp_execution_context(
+    binary_path: str,
+    default_num_clients: int = 0,
+    max_concurrent_computation_calls: int = 0,
+) -> sync_execution_context.ExecutionContext:
+  """Creates an execution context backed by TFF-C++ runtime.
+
+  This exexucion context starts a TFF-C++ worker assumed to be at path
+  `binary_path`, serving on `port`, and constructs a simple (Python) remote
+  execution context to talk to this worker.
+
+  Args:
+    binary_path: The absolute path to the binary defining the TFF-C++ worker.
+    default_num_clients: The number of clients to use as the default
+      cardinality, if thus number cannot be inferred by the arguments of a
+      computation.
+    max_concurrent_computation_calls: The maximum number of concurrent calls to
+      a single computation in the CPP runtime. If `None`, there is no limit.
+
+  Returns:
+    An instance of `context_base.Context` representing the TFF-C++ runtime.
+  """
+  service_binary = binary_path
+
+  def start_process() -> Tuple[subprocess.Popen[bytes], int]:
+    port = portpicker.pick_unused_port()
+    args = [
+        service_binary, f'--port={port}',
+        f'--max_concurrent_computation_calls={max_concurrent_computation_calls}'
+    ]
+    logging.debug('Starting TFF C++ server on port: %s', port)
+    return subprocess.Popen(args, stdout=sys.stdout, stderr=sys.stderr), port
+
+  class ServiceManager():
+    """Class responsible for managing a local TFF executor service."""
+
+    def __init__(self):
+      self._stub = None
+      self._process = None
+
+    def __del__(self):
+      if isinstance(self._process, subprocess.Popen):
+        os.kill(self._process.pid, signal.SIGINT)
+      self._process.wait()
+
+    def get_stub(self) -> remote_executor_grpc_stub.RemoteExecutorGrpcStub:
+      """Ensures a TFF service is running.
+
+      Returns stub representing this service.
+
+      This function ensures that the stub it returns is running, and managers
+      the state of the process hosting the TFF service. It additionally ensures
+      that it runs only one TFF service at a time.
+
+      Returns:
+        An TFF remote executor stub which is guaranteed to be running.
+      """
+      if self._stub is not None:
+        if self._stub.is_ready:
+          return self._stub
+        # Stub is not ready; since we block below, this must imply that the
+        # service is down. Kill the process and restart below.
+        os.kill(self._process.pid, signal.SIGINT)
+        logging.debug('Waiting for existing processes to complete')
+        self._process.wait()
+      # Start a process and block til the associated stub is ready.
+      process, port = start_process()
+      channel = grpc.insecure_channel('localhost:{}'.format(port))
+      stub = remote_executor_grpc_stub.RemoteExecutorGrpcStub(channel)
+      self._process = process
+      self._stub = stub
+      while not self._stub.is_ready:
+        time.sleep(_LOCALHOST_SERVER_WAIT_TIME_SEC)
+        logging.debug('TFF service manager sleeping; stub is not ready.')
+      return self._stub
+
+  service_manager = ServiceManager()
+
+  def stack_fn(cardinalities):
+    if cardinalities.get(placements.CLIENTS) is None:
+      cardinalities[placements.CLIENTS] = default_num_clients
+    stub = service_manager.get_stub()
+    ex = remote_executor.RemoteExecutor(stub)
+    ex.set_cardinalities(cardinalities)
+    return ex
+
+  factory = python_executor_stacks.ResourceManagingExecutorFactory(
+      executor_stack_fn=stack_fn)
+  return _make_basic_python_execution_context(
+      executor_fn=factory,
+      compiler_fn=compiler.desugar_and_transform_to_native,
+      asynchronous=False)
