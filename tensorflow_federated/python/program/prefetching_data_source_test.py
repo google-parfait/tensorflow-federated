1b2fda9a63e1dde3226caf463a7588d754310c56
Michael Reneer <michaelreneer@google.com>
2022-10-20 16:42:09 -0700

    Add more tests for `tff.program.PrefetchingDataSource`.
    PiperOrigin-RevId: 482633878

--- tensorflow_federated/python/program/prefetching_data_source_test.py
+++ tensorflow_federated/python/program/prefetching_data_source_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import asyncio
 import unittest
 from unittest import mock
 
@@ -22,13 +21,15 @@ import tensorflow as tf
 
 from tensorflow_federated.python.core.backends.native import execution_contexts
 from tensorflow_federated.python.core.backends.native import execution_contexts
+from tensorflow_federated.python.core.impl.federated_context import federated_computation
 from tensorflow_federated.python.core.impl.types import computation_types
 from tensorflow_federated.python.core.impl.types import placements
 from tensorflow_federated.python.program import data_source as data_source_lib
 from tensorflow_federated.python.program import prefetching_data_source
 
 
-class PrefetchingDataSourceIteratorTest(parameterized.TestCase):
+class PrefetchingDataSourceIteratorTest(parameterized.TestCase,
+                                        unittest.IsolatedAsyncioTestCase):
 
   def test_init_does_not_raise_type_error(self):
     mock_iterator = mock.create_autospec(
@@ -205,6 +206,78 @@ class PrefetchingDataSourceIteratorTest(parameterized.TestCase):
           num_clients_to_prefetch=num_clients_to_prefetch,
           prefetch_threshold=1)
 
+  # pyformat: disable
+  @parameterized.named_parameters(
+      ('async_python',
+       execution_contexts.create_local_async_python_execution_context()),
+  )
+  # pyformat: enable
+  async def test_select_returns_data(self, context):
+    mock_iterator = mock.create_autospec(
+        data_source_lib.FederatedDataSourceIterator)
+    mock_iterator.select.return_value = [1, 2, 3]
+    mock_iterator.federated_type = computation_types.FederatedType(
+        tf.int32, placements.CLIENTS)
+    iterator = prefetching_data_source.PrefetchingDataSourceIterator(
+        iterator=mock_iterator,
+        context=context,
+        total_rounds=5,
+        num_rounds_to_prefetch=3,
+        num_clients_to_prefetch=3,
+        prefetch_threshold=1)
+
+    data = iterator.select(num_clients=3)
+
+    @federated_computation.federated_computation(iterator.federated_type)
+    def _identity(x):
+      return x
+
+    actual_value = await context.invoke(_identity, data)
+    self.assertEqual(actual_value, [1, 2, 3])
+
+  # pyformat: disable
+  @parameterized.named_parameters(
+      ('async_python',
+       execution_contexts.create_local_async_python_execution_context()),
+  )
+  # pyformat: enable
+  def test_select_prefetches_data(self, context):
+    mock_iterator = mock.create_autospec(
+        data_source_lib.FederatedDataSourceIterator)
+    mock_iterator.select.return_value = [1, 2, 3]
+    mock_iterator.federated_type = computation_types.FederatedType(
+        tf.int32, placements.CLIENTS)
+    iterator = prefetching_data_source.PrefetchingDataSourceIterator(
+        iterator=mock_iterator,
+        context=context,
+        total_rounds=5,
+        num_rounds_to_prefetch=3,
+        num_clients_to_prefetch=3,
+        prefetch_threshold=1)
+
+    self.assertEmpty(iterator._prefetched_data)
+    self.assertEqual(mock_iterator.select.call_count, 3)
+
+    iterator.select(num_clients=3)
+    self.assertLen(iterator._prefetched_data, 2)
+    self.assertEqual(mock_iterator.select.call_count, 3)
+
+    iterator.select(num_clients=3)
+    self.assertLen(iterator._prefetched_data, 1)
+    self.assertEqual(mock_iterator.select.call_count, 5)
+
+    iterator.select(num_clients=3)
+    self.assertLen(iterator._prefetched_data, 2)
+    self.assertEqual(mock_iterator.select.call_count, 5)
+
+    iterator.select(num_clients=3)
+    self.assertLen(iterator._prefetched_data, 1)
+    self.assertEqual(mock_iterator.select.call_count, 5)
+
+    iterator.select(num_clients=3)
+    self.assertEmpty(iterator._prefetched_data)
+    self.assertEqual(mock_iterator.select.call_count, 5)
+
   @parameterized.named_parameters(
       ('str', 'a'),
       ('list', []),
@@ -244,9 +317,29 @@ class PrefetchingDataSourceIteratorTest(parameterized.TestCase):
     with self.assertRaises(ValueError):
       iterator.select(num_clients)
 
+  def test_select_raises_runtime_error_with_to_many_rounds(self):
+    mock_iterator = mock.create_autospec(
+        data_source_lib.FederatedDataSourceIterator)
+    mock_iterator.select.return_value = [1, 2, 3]
+    mock_iterator.federated_type = computation_types.FederatedType(
+        tf.int32, placements.CLIENTS)
+    context = execution_contexts.create_local_async_python_execution_context()
+    iterator = prefetching_data_source.PrefetchingDataSourceIterator(
+        iterator=mock_iterator,
+        context=context,
+        total_rounds=5,
+        num_rounds_to_prefetch=3,
+        num_clients_to_prefetch=3,
+        prefetch_threshold=1)
 
-class PrefetchingDataSourceTest(parameterized.TestCase,
-                                unittest.IsolatedAsyncioTestCase):
+    for _ in range(5):
+      iterator.select(num_clients=3)
+
+    with self.assertRaises(RuntimeError):
+      iterator.select(num_clients=3)
+
+
+class PrefetchingDataSourceTest(parameterized.TestCase):
 
   def test_init_does_not_raise_type_error(self):
     mock_data_source = mock.create_autospec(data_source_lib.FederatedDataSource)
@@ -414,40 +507,6 @@ class PrefetchingDataSourceTest(parameterized.TestCase,
           num_rounds_to_prefetch=3,
           num_clients_to_prefetch=num_clients_to_prefetch,
           prefetch_threshold=1)
-
-  async def test_select_calls_prefetches_data(self):
-    ds = mock.create_autospec(data_source_lib.FederatedDataSource)
-    ds_iterator = mock.create_autospec(
-        data_source_lib.FederatedDataSourceIterator)
-    ds_iterator.federated_type = computation_types.FederatedType(
-        tf.int32, placements.CLIENTS)
-    ds_iterator.select.return_value = [1, 2]
-    ds.iterator.return_value = ds_iterator
-    context = execution_contexts.create_local_async_python_execution_context()
-
-    prefetching_ds = prefetching_data_source.PrefetchingDataSource(
-        data_source=ds,
-        context=context,
-        total_rounds=5,
-        num_rounds_to_prefetch=3,
-        num_clients_to_prefetch=2,
-        prefetch_threshold=0)
-
-    ds_iterator.select.assert_not_called()
-    # The first round of prefetching
-    prefetching_iter = prefetching_ds.iterator()
-    for _ in range(2):
-      prefetching_iter.select(num_clients=2)
-      self.assertEqual(ds_iterator.select.call_count, 3)
-    prefetching_iter.select(num_clients=2)
-    # The second round of prefetching
-    await asyncio.sleep(0.1)
-    self.assertEqual(ds_iterator.select.call_count, 5)
-    for _ in range(2):
-      prefetching_iter.select(num_clients=2)
-      self.assertEqual(ds_iterator.select.call_count, 5)
-    with self.assertRaises(RuntimeError):
-      prefetching_iter.select(num_clients=2)
 
 
 if __name__ == '__main__':
