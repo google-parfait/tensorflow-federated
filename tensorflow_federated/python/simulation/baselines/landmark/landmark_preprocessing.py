2bee32272dac39d6893cb6967c0e269a30670e4c
Hui Li <huilisea@google.com>
2023-01-30 19:46:39 -0800

    The first step to create the Baselines API of GLDv2: to create a preprocessing function.
    PiperOrigin-RevId: 505867303

---
+++ tensorflow_federated/python/simulation/baselines/landmark/landmark_preprocessing.py
@@ -0,0 +1,138 @@
+# Copyright 2022, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Preprocessing library for LANDMARK baseline tasks."""
+
+import collections
+from collections.abc import Callable
+from typing import Optional
+
+import tensorflow as tf
+
+from tensorflow_federated.python.simulation.baselines import client_spec
+
+
+# The largest client of LANDMARK has 3500 examples.
+_MAX_CLIENT_DATASET_SIZE = 3500
+# We use 128 as the image size (not 224 as in https://arxiv.org/abs/2003.08082)
+# for faster experiments.
+_IMAGE_SIZE = 128
+
+
+def _map_fn(
+    element: collections.OrderedDict[str, tf.Tensor], is_training: bool
+) -> tuple[tf.Tensor, tf.Tensor]:
+  """Preprocesses an image for training/eval using Keras data augmentation.
+
+  The original GLD images have various image size. We map a single image at a
+  time instead of a batch to the target image size. For training images, we
+  randomly crop the image to the target size, randomly flipped the image
+  horizontally, and rescale the input values to the range of [-1.0, 1.0]. For
+  testing images, we crop the center portion of the image at the target size,
+  and rescale the input values to the range of [-1.0, 1.0].
+
+  Args:
+    element: An OrderedDict with the keys of `image/decoded` and `class`
+      representing an image.
+    is_training: Boolean. If true it would preprocess an image for train,
+      otherwise it would preprocess it for evaluation.
+
+  Returns:
+    A tuple or two tensors. The first tensor represents the processed image.
+    The second tensor inherits the `class` of the input element.
+  """
+  preprocessing_image_for_train = tf.keras.Sequential([
+      tf.keras.layers.RandomCrop(_IMAGE_SIZE, _IMAGE_SIZE),
+      tf.keras.layers.RandomFlip('horizontal'),
+      tf.keras.layers.Rescaling(
+          scale=2.0 / 255, offset=-1.0
+      ),  # rescale the values to the range of [-1.0, 1.0]
+  ])
+  preprocessing_image_for_eval = tf.keras.Sequential([
+      tf.keras.layers.CenterCrop(_IMAGE_SIZE, _IMAGE_SIZE),
+      tf.keras.layers.Rescaling(
+          scale=2.0 / 255, offset=-1.0
+      ),  # rescale the values to the range of [-1.0, 1.0]
+  ])
+
+  if is_training:
+    image = preprocessing_image_for_train(element['image/decoded'])
+  else:
+    image = preprocessing_image_for_eval(element['image/decoded'])
+
+  label = element['class']
+  return image, label
+
+
+def create_preprocess_fn(
+    preprocess_spec: client_spec.ClientSpec,
+    is_training: bool,
+    num_parallel_calls: tf.Tensor = tf.data.experimental.AUTOTUNE,
+    debug_seed: Optional[int] = None,
+) -> Callable[[tf.data.Dataset], tf.data.Dataset]:
+  """Creates a preprocessing function for LANDMARK client datasets.
+
+  The preprocessing function crops & reshapes, shuffles, repeats, and batches
+  images, using the `map`, `shuffle`, `repeat`, `take`, and `batch` of a
+  `tf.data.Dataset`, in that order.
+
+  Args:
+    preprocess_spec: A `tff.simulation.baselines.ClientSpec` containing
+      information on how to preprocess clients.
+    is_training: Boolean. If `True`, we randomly crop the image to the target
+      size, randomly flipped the image horizontally, and rescale the input
+      values to the range of [-1.0, 1.0]. Otherwise, we crop the center portion
+      of the image at the target size, and rescale the input values to the range
+      of [-1.0, 1.0].
+    num_parallel_calls: An integer representing the number of parallel calls
+      used when performing `tf.data.Dataset.map`.
+    debug_seed: An optional integer seed for deterministic shuffling and
+      mapping. Intended for unittesting.
+
+  Returns:
+    A callable function taking `tf.data.Dataset` as an input, and returning a
+    `tf.data.Dataset` formed by preprocessing according to the input arguments.
+  """
+
+  shuffle_buffer_size = preprocess_spec.shuffle_buffer_size
+  if shuffle_buffer_size is None:
+    shuffle_buffer_size = _MAX_CLIENT_DATASET_SIZE
+
+  num_epochs = preprocess_spec.num_epochs
+  batch_size = preprocess_spec.batch_size
+  max_elements = preprocess_spec.max_elements
+
+  def preprocess_fn(dataset: tf.data.Dataset) -> tf.data.Dataset:
+    """Create a callable function to preprocess `tf.data.Dataset`.
+
+    Args:
+      dataset: `tf.data.Dataset` to be preprocessed.
+
+    Returns:
+      `tf.data.Dataset` preprocessed according to the input arguments.
+    """
+    if is_training:
+      data_map_fn = lambda element: _map_fn(element, is_training=True)
+    else:
+      data_map_fn = lambda element: _map_fn(element, is_training=False)
+
+    dataset = dataset.map(data_map_fn, num_parallel_calls=num_parallel_calls)
+    if shuffle_buffer_size > 1:
+      dataset = dataset.shuffle(shuffle_buffer_size, seed=debug_seed)
+    if num_epochs > 1:
+      dataset = dataset.repeat(num_epochs)
+    if max_elements is not None:
+      dataset = dataset.take(max_elements)
+    return dataset.batch(batch_size)
+
+  return preprocess_fn
---
+++ tensorflow_federated/python/simulation/baselines/landmark/landmark_preprocessing_test.py
@@ -0,0 +1,101 @@
+# Copyright 2022, Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import collections
+from absl.testing import parameterized
+import tensorflow as tf
+
+from tensorflow_federated.python.core.backends.native import execution_contexts
+from tensorflow_federated.python.simulation.baselines import client_spec
+from tensorflow_federated.python.simulation.baselines.landmark import landmark_preprocessing
+
+TEST_DATA = collections.OrderedDict({
+    'image/decoded': tf.zeros(shape=(200, 200, 3), dtype=tf.int32),
+    'class': tf.zeros(shape=(1), dtype=tf.int64),
+})
+_IMAGE_SIZE = landmark_preprocessing._IMAGE_SIZE
+
+
+class LandmarkPreprocessingTest(parameterized.TestCase, tf.test.TestCase):
+
+  @parameterized.named_parameters(
+      (
+          'train',
+          lambda data: landmark_preprocessing._map_fn(data, is_training=True),
+      ),
+      (
+          'test',
+          lambda data: landmark_preprocessing._map_fn(data, is_training=False),
+      ),
+  )
+  def test_map_fn_returns_correct_output_shape_and_dtype(self, map_fn):
+    ds = tf.data.Dataset.from_tensors(TEST_DATA)
+    image, label = iter(ds.map(map_fn)).next()
+    self.assertListEqual(
+        image.shape.as_list(),
+        [_IMAGE_SIZE, _IMAGE_SIZE, 3],
+    )
+    self.assertListEqual(label.shape.as_list(), [1])
+    self.assertEqual(image.dtype, tf.float32)
+    self.assertEqual(label.dtype, tf.int64)
+
+  @parameterized.named_parameters(
+      ('train1', True, 1, 20, 1),
+      ('train2', True, 1, 1, None),
+      ('train3', True, 200, 1, None),
+      ('train4', True, 200, 1, 100),
+      ('train5', True, 100, 1, 200),
+      ('test1', False, 1, 20, 1),
+      ('test2', False, 1, 1, None),
+      ('test3', False, 200, 1, None),
+      ('test4', False, 200, 1, 100),
+      ('test5', False, 100, 1, 200),
+  )
+  def test_create_preprocess_fn_returns_correct_output(
+      self, is_training, num_epochs, batch_size, max_elements
+  ):
+    ds = tf.data.Dataset.from_tensors(TEST_DATA)
+    preprocess_spec = client_spec.ClientSpec(
+        num_epochs=num_epochs, batch_size=batch_size, max_elements=max_elements
+    )
+    preprocess_fn = landmark_preprocessing.create_preprocess_fn(
+        preprocess_spec=preprocess_spec, is_training=is_training
+    )
+    preprocessed_ds = preprocess_fn(ds)
+    self.assertEqual(
+        preprocessed_ds.element_spec,
+        (
+            tf.TensorSpec(
+                shape=(None, _IMAGE_SIZE, _IMAGE_SIZE, 3), dtype=tf.float32
+            ),
+            tf.TensorSpec(shape=(None, 1), dtype=tf.int64),
+        ),
+    )
+
+    element = iter(preprocessed_ds).next()
+    if max_elements is None:
+      expected_length = num_epochs
+    else:
+      expected_length = min(num_epochs, max_elements)
+    self.assertLen(preprocessed_ds, expected_length)
+
+    expected_element = (
+        tf.convert_to_tensor([[[[-1.0] * 3] * _IMAGE_SIZE] * _IMAGE_SIZE] * 1),
+        tf.zeros(shape=(1, 1), dtype=tf.int64),
+    )
+    self.assertAllClose(element, expected_element)
+
+
+if __name__ == '__main__':
+  execution_contexts.set_sync_local_cpp_execution_context()
+  tf.test.main()
