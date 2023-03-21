cffc985e13d76d5db7902f40d0169dceae88ee74
Zachary Charles <zachcharles@google.com>
2023-03-21 14:26:15 -0700

    Remove `tff.learning.ModelWeights` from the public API. Users should instead use `tff.learning.models.ModelWeights`.
    PiperOrigin-RevId: 518381724

--- tensorflow_federated/python/learning/__init__.py
+++ tensorflow_federated/python/learning/__init__.py
@@ -63,7 +63,6 @@ from tensorflow_federated.python.learning.model_update_aggregator import secure_
 # to `tff.learning.models`.
 from tensorflow_federated.python.learning.models.keras_utils import federated_aggregate_keras_metric
 from tensorflow_federated.python.learning.models.keras_utils import from_keras_model
-from tensorflow_federated.python.learning.models.model_weights import ModelWeights
 # TODO(b/259609586): Remove tff.learning.models.variable imports once all
 # callsites have been updated to `tff.learning.models`.
 from tensorflow_federated.python.learning.models.variable import BatchOutput
