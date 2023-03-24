e00f3a111a38d0a83fa36895052b5d86af8c36a0
amangupta201 <113130485+amangupta201@users.noreply.github.com>
2023-03-24 11:52:18 +0530

    Updated _init_.py
    Added Libraries for deploying TensorFlow Federated production cloud environments by using aws lambda and creating a serverless application using python

--- tensorflow_federated/python/cloud/__init__.py
+++ tensorflow_federated/python/cloud/__init__.py
@@ -18,3 +18,38 @@ variety of production cloud environments (e.g GCP, AWS, Azure). For generic
 components that are not specific to such environments we recommend contributing
 them to an appropriate location `core`.
 """
+import tensorflow as tf
+import tensorflow_federated as tff
+
+# Load simulation data.
+source, _ = tff.simulation.datasets.emnist.load_data()
+def client_data(n):
+  return source.create_tf_dataset_for_client(source.client_ids[n]).map(
+      lambda e: (tf.reshape(e['pixels'], [-1]), e['label'])
+  ).repeat(10).batch(20)
+
+# Pick a subset of client devices to participate in training.
+train_data = [client_data(n) for n in range(3)]
+
+# Wrap a Keras model for use with TFF.
+def model_fn():
+  model = tf.keras.models.Sequential([
+      tf.keras.layers.Dense(10, tf.nn.softmax, input_shape=(784,),
+                            kernel_initializer='zeros')
+  ])
+  return tff.learning.models.from_keras_model(
+      model,
+      input_spec=train_data[0].element_spec,
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+
+# Simulate a few rounds of training with the selected client devices.
+trainer = tff.learning.algorithms.build_weighted_fed_avg(
+  model_fn,
+  client_optimizer_fn=lambda: tf.keras.optimizers.SGD(0.1))
+state = trainer.initialize()
+for _ in range(5):
+  result = trainer.next(state, train_data)
+  state = result.state
+  metrics = result.metrics
+  print(metrics['client_work']['train']['loss'])
