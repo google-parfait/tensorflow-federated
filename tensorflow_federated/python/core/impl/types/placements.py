fc1033f2865e4bdb7d5c2bfb3c78d9e217e545bf
Michael Reneer <michaelreneer@google.com>
2023-05-11 11:01:48 -0700

    Add type annotations to the `placements` module.
    This makes it easier to fix other type-relating issues in the `computation_types` module.
    
    PiperOrigin-RevId: 531251718

--- tensorflow_federated/python/core/impl/types/placements.py
+++ tensorflow_federated/python/core/impl/types/placements.py
@@ -17,50 +17,49 @@
 class PlacementLiteral:
   """A representation of one of the globally recognized placement literals."""
 
-  def __init__(self, name, uri, default_all_equal, description):
+  def __init__(
+      self, name: str, uri: str, default_all_equal: bool, description: str
+  ):
     self._name = name
     self._uri = uri
-    self._description = description
     self._default_all_equal = default_all_equal
+    self._description = description
 
   @property
-  def name(self):
+  def name(self) -> str:
     return self._name
 
   @property
-  def uri(self):
+  def uri(self) -> str:
     return self._uri
 
   @property
-  def default_all_equal(self):
+  def default_all_equal(self) -> bool:
     return self._default_all_equal
 
-  def is_server(self):
+  def is_server(self) -> bool:
     return self is SERVER
 
-  def is_clients(self):
+  def is_clients(self) -> bool:
     return self is CLIENTS
 
-  def __doc__(self):
+  def __doc__(self) -> str:
     return self._description
 
-  def __str__(self):
+  def __str__(self) -> str:
     return self._name
 
-  def __repr__(self):
+  def __repr__(self) -> str:
     return "PlacementLiteral('{}')".format(self._uri)
 
-  def __eq__(self, other):
+  def __eq__(self, other: object) -> bool:
     if self is other:
       return True
     elif not isinstance(other, PlacementLiteral):
       return NotImplemented
     return self._uri == other.uri
 
-  def __ne__(self, other):
-    return not self == other
-
-  def __hash__(self):
+  def __hash__(self) -> int:
     return hash(self._uri)
 
 
@@ -82,7 +81,7 @@ SERVER = PlacementLiteral(
 )
 
 
-def uri_to_placement_literal(uri):
+def uri_to_placement_literal(uri: str) -> PlacementLiteral:
   """Returns the placement literal that corresponds to the given URI.
 
   Args:
@@ -97,4 +96,4 @@ def uri_to_placement_literal(uri):
   for literal in [CLIENTS, SERVER]:
     if uri == literal.uri:
       return literal
-  raise ValueError("There is no known literal with uri '{}'.".format(uri))
+  raise ValueError(f'There is no known literal with uri `{uri}`.')
