6d70324f0bc5f097cce3deba7750761f14b945ae
Michael Reneer <michaelreneer@google.com>
2023-06-05 08:33:28 -0700

    Remove `OrderedTasks` API and use `asyncio` directly.
    * Removed `OrderedTasks` and the associated utilties and tests.
    * Updated the federated program example to use `asyncio` directly.
    * Removed logging from the federated program example. These logs are no longer ordered with the values being released using the LoggingReleaseManager and they establish a convention of adding a side channel to a federated program that makes it harder to reason about the privacy of that federated program.
    * Fixed some run-on sentences in federated program example documentation.
    
    PiperOrigin-RevId: 537883977

--- tensorflow_federated/python/common_libs/async_utils_test.py
+++ tensorflow_federated/python/common_libs/async_utils_test.py
@@ -11,89 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for `async_utils` library."""
 
 import asyncio
 
 from absl.testing import absltest
 
 from tensorflow_federated.python.common_libs import async_utils
-
-
-async def _sleep_then_append(entries, value):
-  await asyncio.sleep(0.01)
-  entries.append(value)
-
-
-class OrderedTasksTest(absltest.TestCase):
-
-  def test_runs_awaitable(self):
-    nums = []
-
-    async def run():
-      async with async_utils.ordered_tasks() as tasks:
-        tasks.add(_sleep_then_append(nums, 0))
-
-    asyncio.run(run())
-    self.assertEqual(nums, [0])
-
-  def test_runs_awaitable_in_separate_task_while_in_context(self):
-    async def run():
-      event = asyncio.Event()
-
-      async def awaitable():
-        await asyncio.sleep(0.01)
-        event.set()
-
-      async with async_utils.ordered_tasks() as tasks:
-        tasks.add(awaitable())
-        await asyncio.wait_for(event.wait(), timeout=10)
-
-    asyncio.run(run())
-
-  def test_runs_despite_exception_and_logs(self):
-    nums = []
-
-    async def _sleep_then_append_then_throw(entries, value):
-      await asyncio.sleep(0.01)
-      entries.append(value)
-      raise ValueError('async throwing')
-
-    def _append_then_throw():
-      nums.append(1)
-      raise ValueError('sync throwing')
-
-    async def run():
-      async with async_utils.ordered_tasks() as tasks:
-        tasks.add(_sleep_then_append_then_throw(nums, 0))
-        tasks.add_callable(_append_then_throw)
-        tasks.add(_sleep_then_append_then_throw(nums, 2))
-
-    with self.assertLogs() as logs:
-      asyncio.run(run())
-    self.assertEqual(nums, [0, 1, 2])
-    self.assertLen(logs.records, 3)
-
-  def test_runs_awaitables(self):
-    nums = []
-
-    async def run():
-      async with async_utils.ordered_tasks() as tasks:
-        tasks.add_all(*(_sleep_then_append(nums, n) for n in range(3)))
-
-    asyncio.run(run())
-    self.assertSameElements(nums, list(range(3)))
-
-  def test_runs_sync_after_awaitable(self):
-    nums = []
-
-    async def run():
-      async with async_utils.ordered_tasks() as tasks:
-        tasks.add(_sleep_then_append(nums, 0))
-        tasks.add_callable(lambda: nums.append(1))
-
-    asyncio.run(run())
-    self.assertEqual(nums, [0, 1])
 
 
 class SharedAwaitableTest(absltest.TestCase):
