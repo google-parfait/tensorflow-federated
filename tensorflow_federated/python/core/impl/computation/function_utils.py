da6e997d96e0663bbdb49968f5b4718c786b38b6
Michael Reneer <michaelreneer@google.com>
2023-09-22 15:11:02 -0700

    Remove usage of `Any` from some modules in the `impl` package.
    In most cases it's possible to simply replace `Any` with `object` but in some cases a more specific type is appropriate.
    
    This change also fixes any pytype errors caused by adding the correct Python type annotation.
    
    Note: This also meant moving the PolymorphicComputaiton class into it's own file in order to break a circular dependency.
    PiperOrigin-RevId: 567727933

--- tensorflow_federated/python/core/impl/computation/function_utils.py
+++ tensorflow_federated/python/core/impl/computation/function_utils.py
@@ -21,7 +21,6 @@ from typing import Any, Optional
 
 from tensorflow_federated.python.common_libs import py_typecheck
 from tensorflow_federated.python.common_libs import structure
-from tensorflow_federated.python.core.impl.computation import computation_base
 from tensorflow_federated.python.core.impl.types import computation_types
 from tensorflow_federated.python.core.impl.types import type_analysis
 from tensorflow_federated.python.core.impl.types import type_conversions
@@ -486,89 +485,3 @@ def create_argument_unpacking_fn(
     return functools.partial(_unpack_arg, arg_types, kwarg_types)
   else:
     return functools.partial(_ensure_arg_type, parameter_type)
-
-
-class PolymorphicComputation:
-  """A generic polymorphic function that accepts arguments of diverse types."""
-
-  def __init__(
-      self,
-      concrete_function_factory: Callable[
-          [computation_types.Type, Optional[bool]], computation_base.Computation
-      ],
-  ):
-    """Crates a polymorphic function with a given function factory.
-
-    Args:
-      concrete_function_factory: A callable that accepts a (non-None) TFF type
-        as an argument, as well as an optional boolean `unpack` argument which
-        should be treated as documented in `create_argument_unpacking_fn` above.
-        The callable must return a `Computation` instance that's been created to
-        accept a single positional argument of this TFF type (to be reused for
-        future calls with parameters of a matching type).
-    """
-    self._concrete_function_factory = concrete_function_factory
-    self._concrete_function_cache = {}
-
-  def fn_for_argument_type(
-      self, arg_type: computation_types.Type, unpack: Optional[bool] = None
-  ) -> computation_base.Computation:
-    """Concretizes this function with the provided `arg_type`.
-
-    The first time this function is called with a particular type on a
-    given `PolymorphicComputation` (or this `PolymorphicComputation` is called
-    with an argument of the given type), the underlying function will be
-    traced using the provided argument type as input. Later calls will
-    return the cached computed concrete function.
-
-    Args:
-      arg_type: The argument type to use when concretizing this function.
-      unpack: Whether to force unpacking the arguments (`True`), never unpack
-        the arguments (`False`), or infer whether or not to unpack the arguments
-        (`None`).
-
-    Returns:
-      The `computation_base.Computation` that results from tracing this
-      `PolymorphicComputation` with `arg_type.
-    """
-    key = repr(arg_type) + str(unpack)
-    concrete_fn = self._concrete_function_cache.get(key)
-    if not concrete_fn:
-      concrete_fn = (self._concrete_function_factory)(arg_type, unpack)
-      py_typecheck.check_type(
-          concrete_fn, computation_base.Computation, 'computation'
-      )
-      if concrete_fn.type_signature.parameter != arg_type:
-        raise TypeError(
-            'Expected a concrete function that takes parameter {}, got one '
-            'that takes {}.'.format(
-                arg_type, concrete_fn.type_signature.parameter
-            )
-        )
-      self._concrete_function_cache[key] = concrete_fn
-    return concrete_fn
-
-  def __call__(self, *args, **kwargs):
-    """Invokes this polymorphic function with a given set of arguments.
-
-    Args:
-      *args: Positional args.
-      **kwargs: Keyword args.
-
-    Returns:
-      The result of calling a concrete function, instantiated on demand based
-      on the argument types (and cached for future calls).
-
-    Raises:
-      TypeError: if the concrete functions created by the factory are of the
-        wrong computation_types.
-    """
-    # TODO: b/113112885 - We may need to normalize individuals args, such that
-    # the type is more predictable and uniform (e.g., if someone supplies an
-    # unordered dictionary), possibly by converting dict-like and tuple-like
-    # containers into `Struct`s.
-    packed_arg = pack_args_into_struct(args, kwargs)
-    arg_type = type_conversions.infer_type(packed_arg)
-    # We know the argument types have been packed, so force unpacking.
-    concrete_fn = self.fn_for_argument_type(arg_type, unpack=True)
-    return concrete_fn(packed_arg)
--- tensorflow_federated/python/core/impl/computation/function_utils_test.py
+++ tensorflow_federated/python/core/impl/computation/function_utils_test.py
@@ -19,14 +19,9 @@ from absl.testing import absltest
 from absl.testing import parameterized
 import tensorflow as tf
 
-from tensorflow_federated.proto.v0 import computation_pb2 as pb
 from tensorflow_federated.python.common_libs import structure
-from tensorflow_federated.python.core.impl.computation import computation_impl
 from tensorflow_federated.python.core.impl.computation import function_utils
-from tensorflow_federated.python.core.impl.context_stack import context_base
-from tensorflow_federated.python.core.impl.context_stack import context_stack_base
 from tensorflow_federated.python.core.impl.types import computation_types
-from tensorflow_federated.python.core.impl.types import type_serialization
 
 
 class FunctionUtilsTest(parameterized.TestCase):
@@ -261,95 +256,6 @@ class FunctionUtilsTest(parameterized.TestCase):
     args, kwargs = unpack_arguments(arg)
     actual_result = fn(*args, **kwargs)
     self.assertEqual(actual_result, expected_result)
-
-
-class PolymorphicComputationTest(absltest.TestCase):
-
-  def test_call_returns_result(self):
-    class TestContext(context_base.SyncContext):
-
-      def ingest(self, val, type_spec):
-        return val
-
-      def invoke(self, comp, arg):
-        return 'name={},type={},arg={},unpack={}'.format(
-            comp.name, comp.type_signature.parameter, arg, comp.unpack
-        )
-
-    class TestContextStack(context_stack_base.ContextStack):
-
-      def __init__(self):
-        super().__init__()
-        self._context = TestContext()
-
-      @property
-      def current(self):
-        return self._context
-
-      def install(self, ctx):
-        del ctx  # Unused
-        return self._context
-
-    context_stack = TestContextStack()
-
-    class TestFunction(computation_impl.ConcreteComputation):
-
-      def __init__(self, name, unpack, parameter_type):
-        self._name = name
-        self._unpack = unpack
-        type_signature = computation_types.FunctionType(
-            parameter_type, tf.string
-        )
-        test_proto = pb.Computation(
-            type=type_serialization.serialize_type(type_signature)
-        )
-        super().__init__(test_proto, context_stack, type_signature)
-
-      @property
-      def name(self):
-        return self._name
-
-      @property
-      def unpack(self):
-        return self._unpack
-
-    class TestFunctionFactory:
-
-      def __init__(self):
-        self._count = 0
-
-      def __call__(self, parameter_type, unpack):
-        self._count = self._count + 1
-        return TestFunction(str(self._count), str(unpack), parameter_type)
-
-    fn = function_utils.PolymorphicComputation(TestFunctionFactory())
-
-    self.assertEqual(fn(10), 'name=1,type=<int32>,arg=<10>,unpack=True')
-    self.assertEqual(
-        fn(20, x=True), 'name=2,type=<int32,x=bool>,arg=<20,x=True>,unpack=True'
-    )
-    fn_with_bool_arg = fn.fn_for_argument_type(
-        computation_types.to_type(tf.bool)
-    )
-    self.assertEqual(
-        fn_with_bool_arg(True), 'name=3,type=bool,arg=True,unpack=None'
-    )
-    self.assertEqual(
-        fn(30, x=40), 'name=4,type=<int32,x=int32>,arg=<30,x=40>,unpack=True'
-    )
-    self.assertEqual(fn(50), 'name=1,type=<int32>,arg=<50>,unpack=True')
-    self.assertEqual(
-        fn(0, x=False), 'name=2,type=<int32,x=bool>,arg=<0,x=False>,unpack=True'
-    )
-    fn_with_bool_arg = fn.fn_for_argument_type(
-        computation_types.to_type(tf.bool)
-    )
-    self.assertEqual(
-        fn_with_bool_arg(False), 'name=3,type=bool,arg=False,unpack=None'
-    )
-    self.assertEqual(
-        fn(60, x=70), 'name=4,type=<int32,x=int32>,arg=<60,x=70>,unpack=True'
-    )
 
 
 if __name__ == '__main__':
