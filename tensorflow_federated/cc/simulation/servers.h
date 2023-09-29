4bb3ccbc78efdbfac4280bdb35fa4c9adfc15e62
Michael Reneer <michaelreneer@google.com>
2023-09-29 09:48:28 -0700

    Automated rollback of commit 680b21027d8e75951b9628b8c26f06bcfbbe40eb
    PiperOrigin-RevId: 569516766

--- tensorflow_federated/cc/simulation/servers.h
+++ tensorflow_federated/cc/simulation/servers.h
@@ -19,7 +19,6 @@ limitations under the License
 #include <functional>
 #include <memory>
 #include <optional>
-#include <string>
 
 #include "absl/status/statusor.h"
 #include "grpcpp/grpcpp.h"
@@ -44,9 +43,7 @@ void RunServer(std::function<absl::StatusOr<std::shared_ptr<Executor>>(
 // will execute federated computations on the local machine.
 void RunWorker(int port, std::shared_ptr<grpc::ServerCredentials> credentials,
                int grpc_max_message_length_megabytes,
-               int32_t max_concurrent_computation_calls = -1,
-               std::string serialized_server_mesh = "",
-               std::string serialized_client_mesh = "");
+               int32_t max_concurrent_computation_calls = -1);
 
 }  // namespace tensorflow_federated
 #endif  // THIRD_PARTY_TENSORFLOW_FEDERATED_CC_SIMULATION_SERVERS_H_
