e8c92d1316d90f788dd9964836ddc8ea39edd81c
Michael Reneer <michaelreneer@google.com>
2023-11-08 15:17:56 -0800

    Reduce the tensorflow dependency from the `value_serialization` module.
    PiperOrigin-RevId: 580676595

--- tensorflow_federated/python/core/impl/types/array_shape_test.py
+++ tensorflow_federated/python/core/impl/types/array_shape_test.py
@@ -21,32 +21,74 @@ from tensorflow_federated.python.core.impl.types import array_shape
 class ArrayShapeTest(parameterized.TestCase):
 
   @parameterized.named_parameters(
-      ('fully_defined', (2, 3), True),
-      ('partially_defined', (1, None), False),
-      ('unknown', None, False),
-      ('scalar', (), True),
+      ('fully_defined', (2, 3)),
+      ('scalar', ()),
   )
-  def test_is_shape_fully_defined(self, shape, expected_result):
-    actual_result = array_shape.is_shape_fully_defined(shape)
-    self.assertEqual(actual_result, expected_result)
+  def test_is_shape_fully_defined_returns_true(self, shape):
+    result = array_shape.is_shape_fully_defined(shape)
+    self.assertTrue(result)
 
   @parameterized.named_parameters(
-      ('fully_defined', (2, 3), False),
-      ('partially_defined', (1, None), False),
-      ('unknown', None, False),
-      ('scalar', (), True),
+      ('partially_defined', (2, None)),
+      ('unknown', None),
   )
-  def test_is_shape_scalar(self, shape, expected_result):
-    actual_result = array_shape.is_shape_scalar(shape)
-    self.assertEqual(actual_result, expected_result)
+  def test_is_shape_fully_defined_returns_false(self, shape):
+    result = array_shape.is_shape_fully_defined(shape)
+    self.assertFalse(result)
+
+  def test_is_shape_scalar_returns_true(self):
+    shape = ()
+    result = array_shape.is_shape_scalar(shape)
+    self.assertTrue(result)
+
+  @parameterized.named_parameters(
+      ('fully_defined', (2, 3)),
+      ('partially_defined', (2, None)),
+      ('unknown', None),
+  )
+  def test_is_shape_scalar_returns_false(self, shape):
+    result = array_shape.is_shape_scalar(shape)
+    self.assertFalse(result)
+
+  @parameterized.named_parameters(
+      ('unknown_and_fully_defined', None, (2, 3)),
+      ('unknown_and_partially_defined', None, (2, None)),
+      ('unknown_and_unknown', None, None),
+      ('unknown_and_scalar', None, ()),
+      ('known_dimensions_and_fully_defined', (None, None), (2, 3)),
+      ('known_dimensions_and_partially_defined', (None, None), (2, None)),
+      ('known_dimensions_and_unknown', (None, None), None),
+      ('partially_defined_and_fully_defined', (2, None), (2, 3)),
+      ('partially_defined_and_partially_defined', (2, None), (2, None)),
+      ('partially_defined_and_unknown', (2, None), None),
+      ('fully_defined_and_fully_defined', (2, 3), (2, 3)),
+      ('fully_defined_and_partially_defined', (2, 3), (2, None)),
+      ('fully_defined_and_unknown', (2, 3), None),
+  )
+  def test_is_compatible_with_returns_true(self, target, other):
+    result = array_shape.is_compatible_with(target, other)
+    self.assertTrue(result)
+
+  @parameterized.named_parameters(
+      ('known_dimensions_and_scalar', (None, None), ()),
+      ('known_dimensions_wrong_length_short', (None, None), (None,)),
+      ('known_dimensions_wrong_length_long', (None, None), (None, None, None)),
+      ('partially_defined_and_scalar', (2, None), ()),
+      ('partially_defined_wrong_size', (2, None), (20, None)),
+      ('fully_defined_and_scalar', (2, 3), ()),
+      ('fully_defined_wrong_size', (2, 3), (20, 3)),
+  )
+  def test_is_compatible_with_returns_false(self, target, other):
+    result = array_shape.is_compatible_with(target, other)
+    self.assertFalse(result)
 
   @parameterized.named_parameters(
       ('fully_defined', (2, 3), 6),
-      ('partially_defined', (1, None), None),
+      ('partially_defined', (2, None), None),
       ('unknown', None, None),
       ('scalar', (), 1),
   )
-  def test_num_elements_in_shape(self, shape, expected_result):
+  def test_num_elements_in_shape_returns_result(self, shape, expected_result):
     actual_result = array_shape.num_elements_in_shape(shape)
     self.assertEqual(actual_result, expected_result)
 
