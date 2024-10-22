fefd2d1badf2fa83ed693244d3c489f97de868ec
Zachary Garrett <zachgarrett@google.com>
2024-10-22 11:51:33 -0700

    Update xla_serialization_test to use jax.jit instead of xla_client.
    PiperOrigin-RevId: 688634385

--- tensorflow_federated/python/core/environments/xla_backend/xla_serialization_test.py
+++ tensorflow_federated/python/core/environments/xla_backend/xla_serialization_test.py
@@ -13,7 +13,8 @@
 # limitations under the License.
 
 from absl.testing import absltest
-from jax.lib import xla_client
+import jax
+import jax.numpy as jnp
 import numpy as np
 
 from google.protobuf import any_pb2
@@ -23,21 +24,32 @@ from tensorflow_federated.python.core.impl.types import computation_types
 from tensorflow_federated.python.core.impl.types import type_serialization
 
 
+def _make_xla_shape(shapes_and_dtypes_pytree):
+  xla_computation = (
+      jax.jit(
+          lambda: jax.tree_util.tree_map(
+              lambda shape_dtype: jnp.zeros(
+                  shape_dtype.shape, shape_dtype.dtype
+              ),
+              shapes_and_dtypes_pytree,
+          )
+      )
+      .lower()
+      .compiler_ir('hlo')
+  )
+  return xla_computation.program_shape().result_shape()
+
+
 def _make_test_xla_comp_noarg_to_int32():
-  builder = xla_client.XlaBuilder('comp')
-  xla_client.ops.Constant(builder, np.int32(10))
-  return builder.build()
+  return jax.jit(lambda: jnp.int32(10)).lower().compiler_ir('hlo')
 
 
 def _make_test_xla_comp_int32x10_to_int32x10():
-  builder = xla_client.XlaBuilder('comp')
-  tensor_shape = xla_client.Shape.array_shape(
-      xla_client.dtype_to_etype(np.int32), (10,)
+  return (
+      jax.jit(lambda x: jnp.zeros((10,), dtype=jnp.int32), keep_unused=True)
+      .lower(jnp.zeros((10,), jnp.int32))
+      .compiler_ir('hlo')
   )
-  param = xla_client.ops.Parameter(builder, 0, tensor_shape)
-  constant = xla_client.ops.Constant(builder, np.zeros((10,), dtype=np.int32))
-  xla_client.ops.Add(param, constant)
-  return builder.build()
 
 
 class XlaUtilsTest(absltest.TestCase):
@@ -114,29 +126,21 @@ class XlaUtilsTest(absltest.TestCase):
     self.assertEqual(str(type_spec), '(int32[10] -> int32[10])')
 
   def test_create_xla_tff_computation_with_reordered_tensor_indexes(self):
-    builder = xla_client.XlaBuilder('comp')
-    tensor_shape_1 = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (10, 1)
+
+    def dot(x, y):
+      return jnp.dot(x, y)
+
+    xla_comp = (
+        jax.jit(dot)
+        .lower(jnp.zeros((10, 1), jnp.int32), jnp.zeros((1, 20), jnp.int32))
+        .compiler_ir('hlo')
     )
-    param_1 = xla_client.ops.Parameter(builder, 0, tensor_shape_1)
-    tensor_shape_2 = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (1, 20)
-    )
-    param_2 = xla_client.ops.Parameter(builder, 1, tensor_shape_2)
-    xla_client.ops.Dot(param_1, param_2)
-    xla_comp = builder.build()
     comp_pb_1 = xla_serialization.create_xla_tff_computation(
         xla_comp,
         [0, 1],
         computation_types.FunctionType(
             ((np.int32, (10, 1)), (np.int32, (1, 20))),
-            (
-                np.int32,
-                (
-                    10,
-                    20,
-                ),
-            ),
+            (np.int32, (10, 20)),
         ),
     )
     self.assertIsInstance(comp_pb_1, pb.Computation)
@@ -150,13 +154,7 @@ class XlaUtilsTest(absltest.TestCase):
         [1, 0],
         computation_types.FunctionType(
             ((np.int32, (1, 20)), (np.int32, (10, 1))),
-            (
-                np.int32,
-                (
-                    10,
-                    20,
-                ),
-            ),
+            (np.int32, (10, 20)),
         ),
     )
     self.assertIsInstance(comp_pb_2, pb.Computation)
@@ -167,30 +165,27 @@ class XlaUtilsTest(absltest.TestCase):
     )
 
   def test_flatten_xla_tensor_shape(self):
-    tensor_shape = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (10,)
+    tensor_shape = _make_xla_shape(
+        jax.ShapeDtypeStruct(shape=(10,), dtype=jnp.int32)
     )
     flattened = xla_serialization.flatten_xla_shape(tensor_shape)
     self.assertIsInstance(flattened, list)
-    self.assertListEqual(flattened, [tensor_shape])
+    self.assertEqual(flattened, [tensor_shape])
 
   def test_flatten_xla_tuple_shape(self):
-    tensor_shape_1 = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (10,)
-    )
-    tensor_shape_2 = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.float32), (20,)
-    )
-    tuple_shape = xla_client.Shape.tuple_shape([tensor_shape_1, tensor_shape_2])
+    tensor_shape_1 = jax.ShapeDtypeStruct(shape=(10,), dtype=np.int32)
+    tensor_shape_2 = jax.ShapeDtypeStruct(shape=(20,), dtype=np.float32)
+    tuple_shape = _make_xla_shape((tensor_shape_1, tensor_shape_2))
     flattened = xla_serialization.flatten_xla_shape(tuple_shape)
     self.assertIsInstance(flattened, list)
-    self.assertListEqual(flattened, [tensor_shape_1, tensor_shape_2])
+    self.assertEqual(
+        flattened,
+        [_make_xla_shape(tensor_shape_1), _make_xla_shape(tensor_shape_2)],
+    )
 
   def test_xla_shapes_and_binding_to_tff_type_with_tensor(self):
-    tensor_shape = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (10,)
-    )
-    xla_shapes = [tensor_shape]
+    tensor_shape = jax.ShapeDtypeStruct(shape=(10,), dtype=np.int32)
+    xla_shapes = [_make_xla_shape(tensor_shape)]
     binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
     tff_type = xla_serialization.xla_shapes_and_binding_to_tff_type(
         xla_shapes, binding
@@ -198,13 +193,12 @@ class XlaUtilsTest(absltest.TestCase):
     self.assertEqual(str(tff_type), 'int32[10]')
 
   def test_xla_shapes_and_binding_to_tff_type_with_tuple(self):
-    tensor_shape_1 = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (10,)
-    )
-    tensor_shape_2 = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.float32), (20,)
-    )
-    xla_shapes = [tensor_shape_1, tensor_shape_2]
+    tensor_shape_1 = jax.ShapeDtypeStruct(shape=(10,), dtype=np.int32)
+    tensor_shape_2 = jax.ShapeDtypeStruct(shape=(20,), dtype=np.float32)
+    xla_shapes = [
+        _make_xla_shape(tensor_shape_1),
+        _make_xla_shape(tensor_shape_2),
+    ]
     binding = pb.Xla.Binding(
         struct=pb.Xla.StructBinding(
             element=[
@@ -219,30 +213,25 @@ class XlaUtilsTest(absltest.TestCase):
     self.assertEqual(str(tff_type), '<float32[20],int32[10]>')
 
   def test_xla_shapes_and_binding_to_tff_type_raises_unused_tensor(self):
-    tensor_shape = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (10,)
-    )
-    xla_shapes = [tensor_shape]
+    tensor_shape = jax.ShapeDtypeStruct(shape=(10,), dtype=np.int32)
+    xla_shapes = [_make_xla_shape(tensor_shape)]
     binding = None
     with self.assertRaises(ValueError):
       xla_serialization.xla_shapes_and_binding_to_tff_type(xla_shapes, binding)
 
   def test_xla_shapes_and_binding_to_tff_type_raises_unused_element(self):
-    tensor_shape_1 = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (10,)
-    )
-    tensor_shape_2 = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.float32), (20,)
-    )
-    xla_shapes = [tensor_shape_1, tensor_shape_2]
+    tensor_shape_1 = jax.ShapeDtypeStruct(shape=(10,), dtype=np.int32)
+    tensor_shape_2 = jax.ShapeDtypeStruct(shape=(20,), dtype=np.float32)
+    xla_shapes = [
+        _make_xla_shape(tensor_shape_1),
+        _make_xla_shape(tensor_shape_2),
+    ]
     binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=1))
     with self.assertRaises(ValueError):
       xla_serialization.xla_shapes_and_binding_to_tff_type(xla_shapes, binding)
 
   def test_xla_computation_and_bindings_to_tff_type_none_binding_to_int32(self):
-    builder = xla_client.XlaBuilder('comp')
-    xla_client.ops.Constant(builder, np.int32(10))
-    xla_computation = builder.build()
+    xla_computation = _make_test_xla_comp_noarg_to_int32()
     parameter_binding = None
     result_binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
     tff_type = xla_serialization.xla_computation_and_bindings_to_tff_type(
@@ -260,10 +249,16 @@ class XlaUtilsTest(absltest.TestCase):
     self.assertEqual(str(tff_type), '( -> int32)')
 
   def test_xla_computation_and_bindings_to_tff_type_empty_tuple_to_int32(self):
-    builder = xla_client.XlaBuilder('comp')
-    xla_client.ops.Parameter(builder, 0, xla_client.Shape.tuple_shape([]))
-    xla_client.ops.Constant(builder, np.int32(10))
-    xla_computation = builder.build()
+
+    def empty_tuple_to_int32(x):
+      del x  # Unused.
+      return jnp.int32(10)
+
+    xla_computation = (
+        jax.jit(empty_tuple_to_int32, keep_unused=True)
+        .lower(())
+        .compiler_ir('hlo')
+    )
     parameter_binding = pb.Xla.Binding()
     result_binding = pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))
     tff_type = xla_serialization.xla_computation_and_bindings_to_tff_type(
@@ -272,15 +267,15 @@ class XlaUtilsTest(absltest.TestCase):
     self.assertEqual(str(tff_type), '( -> int32)')
 
   def test_xla_computation_and_bindings_to_tff_type_int32_tuple_to_int32(self):
-    builder = xla_client.XlaBuilder('comp')
-    tensor_shape = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (10,)
+
+    def tuple_int_to_int(x):
+      return x[0]
+
+    xla_computation = (
+        jax.jit(tuple_int_to_int)
+        .lower((jax.ShapeDtypeStruct(shape=(10,), dtype=np.int32),))
+        .compiler_ir('hlo')
     )
-    tuple_shape = xla_client.Shape.tuple_shape([tensor_shape])
-    param = xla_client.ops.Parameter(builder, 0, tuple_shape)
-    constant = xla_client.ops.Constant(builder, np.zeros((10,), dtype=np.int32))
-    xla_client.ops.Add(xla_client.ops.GetTupleElement(param, 0), constant)
-    xla_computation = builder.build()
     parameter_binding = pb.Xla.Binding(
         struct=pb.Xla.StructBinding(
             element=[pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))]
@@ -302,15 +297,18 @@ class XlaUtilsTest(absltest.TestCase):
       )
 
   def test_xla_computation_and_bindings_to_tff_type_raises_unused_element(self):
-    builder = xla_client.XlaBuilder('comp')
-    tensor_shape = xla_client.Shape.array_shape(
-        xla_client.dtype_to_etype(np.int32), (10,)
+
+    def unused_element(x):
+      return x[0] + jnp.int32(10)
+
+    xla_computation = (
+        jax.jit(unused_element, keep_unused=True)
+        .lower((
+            jax.ShapeDtypeStruct(shape=(10,), dtype=jnp.int32),
+            jax.ShapeDtypeStruct(shape=(10,), dtype=jnp.int32),
+        ))
+        .compiler_ir('hlo')
     )
-    tuple_shape = xla_client.Shape.tuple_shape([tensor_shape, tensor_shape])
-    param = xla_client.ops.Parameter(builder, 0, tuple_shape)
-    constant = xla_client.ops.Constant(builder, np.zeros((10,), dtype=np.int32))
-    xla_client.ops.Add(xla_client.ops.GetTupleElement(param, 0), constant)
-    xla_computation = builder.build()
     parameter_binding = pb.Xla.Binding(
         struct=pb.Xla.StructBinding(
             element=[pb.Xla.Binding(tensor=pb.Xla.TensorBinding(index=0))]
