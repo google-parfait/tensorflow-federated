5e8d352ce119dde61eb97f6f49341d2056fae6eb
Galen Andrew <galenandrew@google.com>
2025-11-12 15:52:48 -0800

    Improved numerical stability, speed, and code clarity in Gaussian mechanism calibration.
    PiperOrigin-RevId: 831572175

--- tensorflow_federated/python/analytics/differential_privacy.py
+++ tensorflow_federated/python/analytics/differential_privacy.py
@@ -13,94 +13,135 @@
 # limitations under the License.
 """Differential privacy methods for federated analytics."""
 
-import math
+import numpy as np
+from scipy import optimize
+from scipy import stats
 
 
-def analytic_gauss_stddev(epsilon, delta, norm_bound, tol=1.0e-12):
+def _log_sub(x, y):
+  """Stable computation of log(exp(x) - exp(y))."""
+  return x + np.log1p(-np.exp(y - x)) if y <= x else -np.inf
+
+
+def _get_log_delta(noise, eps):
+  # See https://arxiv.org/pdf/1805.06530, Eq. (6).
+  t_star = eps * noise + 1 / (2 * noise)
+  return _log_sub(
+      stats.norm.logcdf(1 / noise - t_star),
+      eps + stats.norm.logcdf(-t_star),
+  )
+
+
+def get_epsilon_gaussian(
+    noise: float, delta: float, tol: float = 1.0e-12
+) -> float:
+  """Compute the epsilon for the Gaussian mechanism with the given DP params.
+
+  Args:
+    noise: The standard deviation of the Gaussian noise.
+    delta: The target delta (0 < delta < 1).
+    tol: Error tolerance for search.
+
+  Returns:
+    The epsilon for the Gaussian mechanism with the given DP params.
+  """
+  if noise < 0:
+    raise ValueError(f'noise must be non-negative, got noise={noise}.')
+  if not 0 <= delta <= 1:
+    raise ValueError(f'delta must be in [0, 1], got delta={delta}.')
+
+  if delta == 1:
+    return 0
+
+  if noise == 0:
+    return np.inf
+  elif noise == np.inf:
+    return 0
+
+  if delta == 0:
+    return np.inf
+
+  log_delta = np.log(delta)
+  if _get_log_delta(noise, 0) < log_delta:
+    # We have (0, delta)-DP.
+    return 0
+
+  eps_lo, eps_hi = 0, 1
+  while _get_log_delta(noise, eps_hi) > log_delta:
+    eps_lo, eps_hi = eps_hi, eps_hi * 2
+
+  return optimize.brentq(
+      lambda eps: _get_log_delta(noise, eps) - log_delta,
+      eps_lo,
+      eps_hi,
+      xtol=tol,
+  )
+
+
+def get_noise_gaussian(
+    epsilon: float, delta: float, tol: float = 1.0e-12
+) -> float:
+  """Compute the noise for the Gaussian mechanism with the given DP params.
+
+  Args:
+    epsilon: The target epsilon.
+    delta: The target delta.
+    tol: Error tolerance for search.
+
+  Returns:
+    The noise for the Gaussian mechanism with the given DP params.
+  """
+  if epsilon < 0:
+    raise ValueError(f'epsilon must be non-negative, got epsilon={epsilon}.')
+  if not 0 <= delta <= 1:
+    raise ValueError(f'delta must be in [0, 1], got delta={delta}.')
+
+  if delta == 1:
+    return 0
+
+  if epsilon == 0:
+    return np.inf
+  elif epsilon == np.inf:
+    return 0
+
+  if delta == 0:
+    return np.inf
+
+  log_delta = np.log(delta)
+  noise_lo, noise_hi = 1e-1, 1
+  while _get_log_delta(noise_lo, epsilon) < log_delta:
+    noise_hi, noise_lo = noise_lo, noise_lo / 10
+  while _get_log_delta(noise_hi, epsilon) > log_delta:
+    noise_lo, noise_hi = noise_hi, noise_hi * 10
+
+  return optimize.brentq(
+      lambda noise: _get_log_delta(noise, epsilon) - log_delta,
+      noise_lo,
+      noise_hi,
+      xtol=tol,
+  )
+
+
+def analytic_gauss_stddev(
+    epsilon: float, delta: float, norm_bound: float, tol: float = 1.0e-12
+):
   """Compute the stddev for the Gaussian mechanism with the given DP params.
 
-  Calibrate a Gaussian perturbation for differential privacy using the
-  analytic Gaussian mechanism of [Balle and Wang, ICML'18].
-
-  Reference: http://proceedings.mlr.press/v80/balle18a/balle18a.pdf.
-
   Arguments:
-    epsilon: Target epsilon (0 < epsilon <= 500). The epsilon value is limited
-      to at most 500 in this implementation because large epsilons causes
-      overflow in python.
-    delta: Target delta (0 < delta < 1).
-    norm_bound: Upper bound on L2 global sensitivity (norm_bound >= 0).
-    tol: Error tolerance for binary search (tol > 0).
+    epsilon: The target epsilon.
+    delta: The target delta.
+    norm_bound: Upper bound on L2 global sensitivity.
+    tol: Error tolerance for search.
 
   Returns:
     sigma: Standard deviation of Gaussian noise needed to achieve
       (epsilon,delta)-DP under the given norm_bound.
   """
+  if norm_bound < 0:
+    raise ValueError(f'norm_bound must be non-negative, got {norm_bound}.')
 
-  if epsilon <= 0 or epsilon >= 500:
-    raise ValueError(f'epsilon must be in (0, 500], got epsilon={epsilon}.')
-  if delta <= 0 or delta >= 1:
-    raise ValueError(f'delta must be in (0, 1), got delta={delta}.')
-  if norm_bound <= 0:
-    raise ValueError(
-        f'norm_bound must be positive, got norm_bound={norm_bound}.'
-    )
-  if tol <= 0:
-    raise ValueError(f'tol must be positive, got tol={tol}.')
+  if norm_bound == 0:
+    return 0
 
-  exp = math.exp
-  sqrt = math.sqrt
-
-  def phi(t):
-    return 0.5 * (1.0 + math.erf(float(t) / sqrt(2.0)))
-
-  # $B_{\epsilon}^+(v)$ in Alg 1 of [Balle and Wang, ICML'18].
-  def case_one(eps, s):
-    return phi(sqrt(eps * s)) - exp(eps) * phi(-sqrt(eps * (s + 2.0)))
-
-  # $B_{\epsilon}^-(u)$ in Alg 1 of [Balle and Wang, ICML'18].
-  def case_two(eps, s):
-    return phi(-sqrt(eps * s)) - exp(eps) * phi(-sqrt(eps * (s + 2.0)))
-
-  def search_s_bounds(predicate_stop, s_inf, s_sup):
-    while not predicate_stop(s_sup):
-      s_inf = s_sup
-      s_sup = 2.0 * s_inf
-    return s_inf, s_sup
-
-  def binary_search(predicate_stop, predicate_left, s_inf, s_sup):
-    s_mid = s_inf + (s_sup - s_inf) / 2.0
-    while not predicate_stop(s_mid):
-      if predicate_left(s_mid):
-        s_sup = s_mid
-      else:
-        s_inf = s_mid
-      s_mid = s_inf + (s_sup - s_inf) / 2.0
-    return s_mid
-
-  delta_thr = case_one(epsilon, 0.0)
-
-  if delta == delta_thr:
-    alpha = 1.0
-
-  else:
-    if delta > delta_thr:
-      predicate_stop_dt = lambda s: case_one(epsilon, s) >= delta
-      function_s_to_delta = lambda s: case_one(epsilon, s)
-      predicate_left_bs = lambda s: function_s_to_delta(s) > delta
-      function_s_to_alpha = lambda s: sqrt(1.0 + s / 2.0) - sqrt(s / 2.0)
-
-    else:
-      predicate_stop_dt = lambda s: case_two(epsilon, s) <= delta
-      function_s_to_delta = lambda s: case_two(epsilon, s)
-      predicate_left_bs = lambda s: function_s_to_delta(s) < delta
-      function_s_to_alpha = lambda s: sqrt(1.0 + s / 2.0) + sqrt(s / 2.0)
-
-    predicate_stop_bs = lambda s: abs(function_s_to_delta(s) - delta) <= tol
-
-    s_inf, s_sup = search_s_bounds(predicate_stop_dt, 0.0, 1.0)
-    s_final = binary_search(predicate_stop_bs, predicate_left_bs, s_inf, s_sup)
-    alpha = function_s_to_alpha(s_final)
-
-  sigma = alpha * norm_bound / sqrt(2.0 * epsilon)
-  return sigma
+  scaled_tol = tol / norm_bound
+  return get_noise_gaussian(epsilon, delta, scaled_tol) * norm_bound
--- tensorflow_federated/python/analytics/differential_privacy_test.py
+++ tensorflow_federated/python/analytics/differential_privacy_test.py
@@ -11,38 +11,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for differential_privacy."""
 import math
+
 from absl.testing import absltest
 from absl.testing import parameterized
+import numpy as np
+
 from tensorflow_federated.python.analytics import differential_privacy
 
 
 class DifferentialPrivacyTest(parameterized.TestCase):
 
   @parameterized.named_parameters(
-      {'testcase_name': 'epsilon_0', 'epsilon': 0.0, 'regex': 'epsilon'},
-      {'testcase_name': 'epsilon_neg', 'epsilon': -1.0, 'regex': 'epsilon'},
-      {'testcase_name': 'epsilon_large', 'epsilon': 600, 'regex': 'epsilon'},
-      {'testcase_name': 'delta_0', 'delta': 0.0, 'regex': 'delta'},
-      {'testcase_name': 'delta_1', 'delta': 1.0, 'regex': 'delta'},
-      {'testcase_name': 'delta_neg', 'delta': -1.0, 'regex': 'delta'},
-      {'testcase_name': 'delta_large', 'delta': 10.0, 'regex': 'delta'},
-      {
-          'testcase_name': 'norm_bound_neg',
-          'norm_bound': -1.0,
-          'regex': 'norm_bound',
-      },
-      {'testcase_name': 'tol_0', 'tol': 0.0, 'regex': 'tol'},
-      {'testcase_name': 'tol_neg', 'tol': -1.0, 'regex': 'tol'},
+      ('epsilon_neg', {'epsilon': -1.0}, 'epsilon'),
+      ('delta_neg', {'delta': -1.0}, 'delta'),
+      ('delta_gt_1', {'delta': 2.0}, 'delta'),
+      ('norm_bound_neg', {'norm_bound': -1.0}, 'norm_bound'),
   )
-  def test_analytic_gauss_stddev_input_validation(
-      self, epsilon=1.0, delta=0.1, norm_bound=1.0, tol=1.0e-12, regex=''
-  ):
+  def test_analytic_gauss_stddev_input_validation(self, args_update, regex):
+    args = dict(epsilon=1.0, delta=0.1, norm_bound=1.0, tol=1.0e-12)
+    args.update(args_update)
     with self.assertRaisesRegex(ValueError, regex):
-      differential_privacy.analytic_gauss_stddev(
-          epsilon=epsilon, delta=delta, norm_bound=norm_bound, tol=tol
-      )
+      differential_privacy.analytic_gauss_stddev(**args)
 
   @parameterized.named_parameters(
       ('eps_1_delta_0.5', 1.0, 0.5, 1.0, 0.50706503),
@@ -55,6 +45,23 @@ class DifferentialPrivacyTest(parameterized.TestCase):
         epsilon=epsilon, delta=delta, norm_bound=norm_bound
     )
     self.assertAlmostEqual(sigma, expected_stddev, places=7)
+
+  def test_get_epsilon_gaussian_inverts_get_noise_gaussian(self):
+    for eps in np.logspace(-5, 5, 11, 10):
+      for delta in [0] + np.logspace(-10, 0, 11, 10):
+        noise = differential_privacy.get_noise_gaussian(eps, delta)
+        recovered_eps = differential_privacy.get_epsilon_gaussian(noise, delta)
+        np.testing.assert_allclose(recovered_eps, 0 if delta == 1 else eps)
+
+  def test_get_noise_gaussian_inverts_get_epsilon_gaussian(self):
+    for noise in np.logspace(-5, 5, 11, 10):
+      for delta in [0] + np.logspace(-10, 0, 11, 10):
+        eps = differential_privacy.get_epsilon_gaussian(noise, delta)
+        recovered_noise = differential_privacy.get_noise_gaussian(eps, delta)
+        if eps == 0:
+          self.assertEqual(recovered_noise, 0 if delta == 1 else np.inf)
+        else:
+          np.testing.assert_allclose(recovered_noise, noise)
 
 
 if __name__ == '__main__':
